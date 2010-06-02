7929501e116e1377fff23f971bf7a2279f9c0f92
dr-jts <mtnclimb@gmail.com>
2010-06-02 16:49:16 +0000

    Added MathUtil

---
+++ jts/java/src/com/vividsolutions/jts/util/MathUtil.java
@@ -0,0 +1,26 @@
+package com.vividsolutions.jts.util;
+
+/**
+ * Various utility functions for mathematical and numerical operations.
+ * 
+ * @author mbdavis
+ *
+ */
+public class MathUtil 
+{
+	/**
+	 * Clamps a value to a given range.
+	 * @param x the value to clamp
+	 * @param min the minimum value of the range
+	 * @param max the maximum value of the range
+	 * @return the clamped value
+	 */
+  public static double clamp(double x, double min, double max)
+  {
+  	if (x < min) return min;
+  	if (x > max) return max;
+  	return x;
+  }
+  
+
+}
