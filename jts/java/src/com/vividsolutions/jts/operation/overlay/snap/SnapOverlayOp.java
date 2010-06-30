b1714d8f449d8683f5d1e2fc4a5593cba2eddff5
dr-jts <mtnclimb@gmail.com>
2010-06-30 21:54:49 +0000

    Added some test code

--- jts/java/src/com/vividsolutions/jts/operation/overlay/snap/SnapOverlayOp.java
+++ jts/java/src/com/vividsolutions/jts/operation/overlay/snap/SnapOverlayOp.java
@@ -63,8 +63,8 @@ public class SnapOverlayOp
 
   public Geometry getResultGeometry(int opCode)
   {
-  	Geometry[] selfSnapGeom = new Geometry[] { selfSnap(geom[0]), selfSnap(geom[1])};
-    Geometry[] prepGeom = snap(selfSnapGeom);
+//  	Geometry[] selfSnapGeom = new Geometry[] { selfSnap(geom[0]), selfSnap(geom[1])};
+    Geometry[] prepGeom = snap(geom);
     Geometry result = OverlayOp.overlayOp(prepGeom[0], prepGeom[1], opCode);
     return prepareResult(result);	
   }
@@ -73,8 +73,8 @@ public class SnapOverlayOp
   {
     GeometrySnapper snapper0 = new GeometrySnapper(geom);
     Geometry snapGeom = snapper0.snapTo(geom, snapTolerance);
-    System.out.println("Self-snapped: " + snapGeom);
-    System.out.println();
+    //System.out.println("Self-snapped: " + snapGeom);
+    //System.out.println();
     return snapGeom;
   }
   
