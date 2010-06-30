73948e449cb836312cc8552aca18439449ce4d8f
dr-jts <mtnclimb@gmail.com>
2010-06-30 21:56:48 +0000

    Reset snapping tolerance to standard value

--- jts/java/src/com/vividsolutions/jts/operation/overlay/snap/GeometrySnapper.java
+++ jts/java/src/com/vividsolutions/jts/operation/overlay/snap/GeometrySnapper.java
@@ -14,7 +14,7 @@ import com.vividsolutions.jts.geom.util.GeometryTransformer;
  */
 public class GeometrySnapper
 {
-  private static final double SNAP_PRECISION_FACTOR = 1e-4;
+  private static final double SNAP_PRECISION_FACTOR = 1e-9;
 
   /**
    * Estimates the snap tolerance for a Geometry, taking into account its precision model.
