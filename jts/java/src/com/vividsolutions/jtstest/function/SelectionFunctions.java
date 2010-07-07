38b6e8f0d5e97df27d46e95afedd7f5a9ab2fd90
dr-jts <mtnclimb@gmail.com>
2010-07-07 05:38:27 +0000

    Added selection functions

--- jts/java/src/com/vividsolutions/jtstest/function/SelectionFunctions.java
+++ jts/java/src/com/vividsolutions/jtstest/function/SelectionFunctions.java
@@ -6,15 +6,106 @@ import com.vividsolutions.jts.geom.*;
 
 public class SelectionFunctions 
 {
-	public static Geometry intersects(Geometry a, Geometry mask)
-	{
-		List selected = new ArrayList();
-		for (int i = 0; i < a.getNumGeometries(); i++ ) {
-			Geometry g = a.getGeometryN(i);
-			if (mask.intersects(g)) {
-				selected.add(g);
-			}
-		}
-		return a.getFactory().buildGeometry(selected);
-	}
+  public static Geometry intersects(Geometry a, final Geometry mask)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return mask.intersects(g);
+      }
+    });
+  }
+  
+  public static Geometry covers(Geometry a, final Geometry mask)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return g.covers(mask);
+      }
+    });
+  }
+  
+  public static Geometry coveredBy(Geometry a, final Geometry mask)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return g.coveredBy(mask);
+      }
+    });
+  }
+  
+  public static Geometry disjoint(Geometry a, Geometry mask)
+  {
+    List selected = new ArrayList();
+    for (int i = 0; i < a.getNumGeometries(); i++ ) {
+      Geometry g = a.getGeometryN(i);
+      if (mask.disjoint(g)) {
+        selected.add(g);
+      }
+    }
+    return a.getFactory().buildGeometry(selected);
+  }
+  public static Geometry valid(Geometry a)
+  {
+    List selected = new ArrayList();
+    for (int i = 0; i < a.getNumGeometries(); i++ ) {
+      Geometry g = a.getGeometryN(i);
+      if (g.isValid()) {
+        selected.add(g);
+      }
+    }
+    return a.getFactory().buildGeometry(selected);
+  }
+  public static Geometry invalid(Geometry a)
+  {
+    List selected = new ArrayList();
+    for (int i = 0; i < a.getNumGeometries(); i++ ) {
+      Geometry g = a.getGeometryN(i);
+      if (! g.isValid()) {
+        selected.add(g);
+      }
+    }
+    return a.getFactory().buildGeometry(selected);
+  }
+  public static Geometry areaGreater(Geometry a, final double minArea)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return g.getArea() > minArea;
+      }
+    });
+  }
+  public static Geometry areaZero(Geometry a)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return g.getArea() == 0.0;
+      }
+    });
+  }
+  public static Geometry within(Geometry a, final Geometry mask)
+  {
+    return select(a, new GeometryPredicate() {
+      public boolean isTrue(Geometry g) {
+        return g.within(mask);
+      }
+    });
+  }
+  
+  private static Geometry select(Geometry geom, GeometryPredicate pred)
+  {
+    List selected = new ArrayList();
+    for (int i = 0; i < geom.getNumGeometries(); i++ ) {
+      Geometry g = geom.getGeometryN(i);
+      if (pred.isTrue(g)) {
+        selected.add(g);
+      }
+    }
+    return geom.getFactory().buildGeometry(selected);
+
+  }
+}
+
+interface GeometryPredicate
+{
+  boolean isTrue(Geometry geom);
 }
