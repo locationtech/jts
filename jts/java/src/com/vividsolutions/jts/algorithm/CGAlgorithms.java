5d510b6c1a302b573a23969efe590766050c0a66
dr-jts <mtnclimb@gmail.com>
2010-08-09 20:57:06 +0000

    Added potential fix for orientation sensitivity of point-line orientation index algorithm

--- jts/java/src/com/vividsolutions/jts/algorithm/CGAlgorithms.java
+++ jts/java/src/com/vividsolutions/jts/algorithm/CGAlgorithms.java
@@ -74,10 +74,36 @@ public class CGAlgorithms
    * @return -1 if q is clockwise (right) from p1-p2
    * @return 0 if q is collinear with p1-p2
    */
-  public static int orientationIndex(Coordinate p1, Coordinate p2, Coordinate q) {
-    // travelling along p1->p2, turn counter clockwise to get to q return 1,
-    // travelling along p1->p2, turn clockwise to get to q return -1,
-    // p1, p2 and q are colinear return 0.
+  public static int orientationIndex(Coordinate p1, Coordinate p2, Coordinate q) 
+  {
+  	/**
+  	 * MD - 9 Aug 2010
+  	 * It seems that the basic algorithm is slightly orientation dependent,
+  	 * when computing the orientation of a point very close to a line.
+  	 * This is possibly due to the arithmetic in the translation to the origin.
+  	 * 
+  	 * For instance, the following situation produces identical results 
+  	 * in spite of the inverse orientation of the line segment:
+  	 * 
+  	 * Coordinate p0 = new Coordinate(219.3649559090992, 140.84159161824724);
+  	 * Coordinate p1 = new Coordinate(168.9018919682399, -5.713787599646864);
+  	 * 
+  	 * Coordinate p = new Coordinate(186.80814046338352, 46.28973405831556);
+  	 * int orient = orientationIndex(p0, p1, p);
+  	 * int orientInv = orientationIndex(p1, p0, p);
+
+  	 * A way to force consistent results is to normalize the orientation of the vector
+  	 * using the following code.
+  	 * However, this may make the results of orientationIndex inconsistent
+  	 * through the triangle of points, so it's not clear this is 
+  	 * an appropriate patch.
+  	 * 
+  	 */
+  	/*
+  	 // normalize orientation of vector to ensure consistent result
+ 	   if (p2.x < p1.x || (p2.x == p1.x && p2.y < p1.y))
+			return -orientationIndex(p2, p1, q);
+	*/
     double dx1 = p2.x - p1.x;
     double dy1 = p2.y - p1.y;
     double dx2 = q.x - p2.x;
