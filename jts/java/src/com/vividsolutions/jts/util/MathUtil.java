a044ab7d81831fb97624f12cf3f1ab3ec99e881f
dr-jts <mtnclimb@gmail.com>
2010-11-17 04:19:04 +0000

    Improved error checking for log10

--- jts/java/src/com/vividsolutions/jts/util/MathUtil.java
+++ jts/java/src/com/vividsolutions/jts/util/MathUtil.java
@@ -22,12 +22,25 @@ public class MathUtil
   	return x;
   }
   
-
   private static final double LOG_10 = Math.log(10);
   
+  /**
+   * Computes the base-10 logarithm of a <tt>double</tt> value.
+   * <ul>
+   * <li>If the argument is NaN or less than zero, then the result is NaN.
+   * <li>If the argument is positive infinity, then the result is positive infinity.
+   * <li>If the argument is positive zero or negative zero, then the result is negative infinity.
+   * </ul>
+   *   
+   * @param x a positive number
+   * @return the value log a, the base-10 logarithm of the input value
+   */
   public static double log10(double x)
   {
-    return Math.log(x)/ LOG_10;
+    double ln = Math.log(x);
+    if (Double.isInfinite(ln)) return ln;
+    if (Double.isNaN(ln)) return ln;
+    return ln / LOG_10;
   }
   
 
