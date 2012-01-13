e0ef6855a7c6ce5d9fce0accbd7069908560b474
dr-jts <mtnclimb@gmail.com>
2012-01-13 05:02:15 +0000

    Changed visibility of TaggedLineStringSimplifier back to public

--- jts/java/src/com/vividsolutions/jts/simplify/TaggedLineStringSimplifier.java
+++ jts/java/src/com/vividsolutions/jts/simplify/TaggedLineStringSimplifier.java
@@ -13,7 +13,7 @@ import com.vividsolutions.jts.util.Debug;
  * @author Martin Davis
  * @version 1.7
  */
-class TaggedLineStringSimplifier
+public class TaggedLineStringSimplifier
 {
   private LineIntersector li = new RobustLineIntersector();
   private LineSegmentIndex inputIndex = new LineSegmentIndex();
