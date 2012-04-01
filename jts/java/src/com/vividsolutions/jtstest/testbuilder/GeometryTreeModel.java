af757794d1bdfe313e2f0e22241aac5de6a6cf1e
dr-jts <mtnclimb@gmail.com>
2012-04-01 05:31:13 +0000

    Improved GeometryNode hierarchy

--- jts/java/src/com/vividsolutions/jtstest/testbuilder/GeometryTreeModel.java
+++ jts/java/src/com/vividsolutions/jtstest/testbuilder/GeometryTreeModel.java
@@ -17,316 +17,363 @@ import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 
-public class GeometryTreeModel implements TreeModel {
-    private Vector<TreeModelListener> treeModelListeners =
-        new Vector<TreeModelListener>();
-    private GeometryNode rootGeom;
+public class GeometryTreeModel implements TreeModel
+{
+  private Vector<TreeModelListener> treeModelListeners = new Vector<TreeModelListener>();
 
-    public GeometryTreeModel(Geometry geom) {
-        rootGeom = GeometryNode.create(geom);
-    }
+  private GeometryNode rootGeom;
 
+  public GeometryTreeModel(Geometry geom)
+  {
+    rootGeom = GeometryNode.create(geom);
+  }
 
-//////////////// TreeModel interface implementation ///////////////////////
+  // ////////////// TreeModel interface implementation ///////////////////////
 
-    /**
-     * Adds a listener for the TreeModelEvent posted after the tree changes.
-     */
-    public void addTreeModelListener(TreeModelListener l) {
-        treeModelListeners.addElement(l);
-    }
+  /**
+   * Adds a listener for the TreeModelEvent posted after the tree changes.
+   */
+  public void addTreeModelListener(TreeModelListener l)
+  {
+    treeModelListeners.addElement(l);
+  }
 
-    /**
-     * Returns the child of parent at index index in the parent's child array.
-     */
-    public Object getChild(Object parent, int index) {
-    	GeometryNode gn = (GeometryNode)parent;
-      return gn.getChildAt(index);
-    }
+  /**
+   * Returns the child of parent at index index in the parent's child array.
+   */
+  public Object getChild(Object parent, int index)
+  {
+    GeometryNode gn = (GeometryNode) parent;
+    return gn.getChildAt(index);
+  }
 
-    /**
-     * Returns the number of children of parent.
-     */
-    public int getChildCount(Object parent) {
-    	GeometryNode gn = (GeometryNode)parent;
-        return gn.getChildCount();
-    }
+  /**
+   * Returns the number of children of parent.
+   */
+  public int getChildCount(Object parent)
+  {
+    GeometryNode gn = (GeometryNode) parent;
+    return gn.getChildCount();
+  }
 
-    /**
-     * Returns the index of child in parent.
-     */
-    public int getIndexOfChild(Object parent, Object child) {
-    	GeometryNode gn = (GeometryNode)parent;
-        return gn.getIndexOfChild((GeometryNode)child);
-    }
+  /**
+   * Returns the index of child in parent.
+   */
+  public int getIndexOfChild(Object parent, Object child)
+  {
+    GeometryNode gn = (GeometryNode) parent;
+    return gn.getIndexOfChild((GeometryNode) child);
+  }
 
-    /**
-     * Returns the root of the tree.
-     */
-    public Object getRoot() {
-        return rootGeom;
-    }
+  /**
+   * Returns the root of the tree.
+   */
+  public Object getRoot()
+  {
+    return rootGeom;
+  }
 
-    /**
-     * Returns true if node is a leaf.
-     */
-    public boolean isLeaf(Object node) {
-    	GeometryNode gn = (GeometryNode)node;
-        return gn.isLeaf();
-    }
+  /**
+   * Returns true if node is a leaf.
+   */
+  public boolean isLeaf(Object node)
+  {
+    GeometryNode gn = (GeometryNode) node;
+    return gn.isLeaf();
+  }
 
-    /**
-     * Removes a listener previously added with addTreeModelListener().
-     */
-    public void removeTreeModelListener(TreeModelListener l) {
-        treeModelListeners.removeElement(l);
-    }
+  /**
+   * Removes a listener previously added with addTreeModelListener().
+   */
+  public void removeTreeModelListener(TreeModelListener l)
+  {
+    treeModelListeners.removeElement(l);
+  }
 
-    /**
-     * Messaged when the user has altered the value for the item
-     * identified by path to newValue.  Not used by this model.
-     */
-    public void valueForPathChanged(TreePath path, Object newValue) {
-        System.out.println("*** valueForPathChanged : "
-                           + path + " --> " + newValue);
-    }
+  /**
+   * Messaged when the user has altered the value for the item identified by
+   * path to newValue. Not used by this model.
+   */
+  public void valueForPathChanged(TreePath path, Object newValue)
+  {
+    System.out
+        .println("*** valueForPathChanged : " + path + " --> " + newValue);
+  }
 }
 
-abstract class GeometryNode 
+abstract class GeometryNode
 {
-	public static GeometryNode create(Coordinate p)
-	{
-		return new CoordinateNode(p);
-	}
-	public static GeometryNode create(Geometry geom)
-	{
-		if (geom instanceof GeometryCollection) return new GeometryCollectionNode((GeometryCollection) geom);
-		if (geom instanceof Polygon) return new PolygonNode((Polygon) geom);
-		if (geom instanceof LineString) return new LineStringNode((LineString) geom);
-    if (geom instanceof LinearRing) return new LinearRingNode((LinearRing) geom);
-    if (geom instanceof Point) return new PointNode((Point) geom);
-		return null;
-	}
-	
+  public static GeometryNode create(Coordinate p)
+  {
+    return new CoordinateNode(p);
+  }
+
+  public static GeometryNode create(Geometry geom)
+  {
+    if (geom instanceof GeometryCollection)
+      return new GeometryCollectionNode((GeometryCollection) geom);
+    if (geom instanceof Polygon)
+      return new PolygonNode((Polygon) geom);
+    if (geom instanceof LineString)
+      return new LineStringNode((LineString) geom);
+    if (geom instanceof LinearRing)
+      return new LinearRingNode((LinearRing) geom);
+    if (geom instanceof Point)
+      return new PointNode((Point) geom);
+    return null;
+  }
+
   protected static String indexString(int index)
   {
     return "[" + index + "]";
   }
-  
+
   protected static String sizeString(int size)
   {
     return "(" + size + ")";
   }
-  
-	protected boolean isLeaf = false;
-	protected int index = -1;
-	protected String text = "";;
-	protected List<GeometryNode> children = null;
-	
-	public GeometryNode(String text)
-	{
-		this.text = text;
-	}
-	
-	public GeometryNode(Geometry geom)
-	{
-		this(geom, 0, null);
-	}
-	
-	public GeometryNode(Geometry geom, int size, String tag)
-	{
+
+  protected boolean isLeaf = false;
+
+  protected int index = -1;
+
+  protected String text = "";;
+
+  protected List<GeometryNode> children = null;
+
+  public GeometryNode(String text)
+  {
+    this.text = text;
+  }
+
+  public GeometryNode(Geometry geom)
+  {
+    this(geom, 0, null);
+  }
+
+  public GeometryNode(Geometry geom, int size, String tag)
+  {
     StringBuilder buf = new StringBuilder();
-		if (tag != null && tag.length() > 0) {
+    if (tag != null && tag.length() > 0) {
       buf.append(tag + " : ");
-		}
+    }
     buf.append(geom.getGeometryType());
-		if (geom.isEmpty()) {
-			isLeaf = true;
+    if (geom.isEmpty()) {
+      isLeaf = true;
       buf.append(" EMPTY");
-		}
-		else {
-			if (size > 0) {
+    }
+    else {
+      if (size > 0) {
         buf.append(sizeString(size));
-			}
-		}
-    text = buf.toString();
-	}
-	
-	public GeometryNode(Geometry geom, int size, int index)
-	{
-		this(geom, size, indexString(index));
-	}
-	
-	public abstract Geometry getGeometry();
-	
-	public void setIndex(int index)
-	{
-		this.index = index;
-	}
-	public boolean isLeaf() { return isLeaf; }
-	
-	protected void createChildren()
-	{
-    children = new ArrayList<GeometryNode>();	  
-	}
-	
-	public GeometryNode getChildAt(int index) 
-	{
-		if (isLeaf) return null;
-		populateChildren();
-		return (GeometryNode) children.get(index);
-	}
-	public int getChildCount()
-	{
-		if (isLeaf) return 0;
-		populateChildren();
-		return children.size();
-	}
-	public int getIndexOfChild(GeometryNode child)
-	{
-		if (isLeaf) return -1;
-		populateChildren();
-		return children.indexOf(child);
-	}
-	
-	public String getText()
-	{
-		if (index >= 0) {
-			return indexString(index) + " : " + text; 
-		}
-		return text;
-	}
-  
-	protected abstract void populateChildren();
-	
-}
-
-class PolygonNode extends GeometryNode 
-{
-	Polygon poly;
-	
-	PolygonNode(Polygon poly)
-	{
-		super(poly, poly.getNumPoints(), null);
-		this.poly = poly;
-	}
-	
-	public Geometry getGeometry() { return poly; }
-	
-	protected void populateChildren()
-	{
-	  createChildren();
-		children.add(new LinearRingNode((LinearRing) poly.getExteriorRing(), "Shell"));
-		for (int i = 0; i < poly.getNumInteriorRing(); i++) {
-			children.add(new LinearRingNode((LinearRing) poly.getInteriorRingN(i), "Hole " + i ));
-		}
-	}
-
-}
-class LineStringNode extends GeometryNode 
-{
-	LineString line;
-	
-	public LineStringNode(LineString line)
-	{
-		this(line, null);
-	}
-	public LineStringNode(LineString line, String tag)
-	{
-		super(line, line.getNumPoints(), tag);
-		this.line = line;
-	}
-  public Geometry getGeometry() { return line; }
-  
-	protected void populateChildren()
-	{
-		populateChildren(line.getCoordinates());
-	}
-	
-   protected void populateChildren(Coordinate[] pt)
-    {
-      // already initialized
-      if (children != null) return;
-      
-      createChildren();
-      for (int i = 0; i < pt.length; i++) {
-        GeometryNode node = create(pt[i]);
-        node.setIndex(i);
-        children.add(node);
       }
     }
+    text = buf.toString();
+  }
+
+  public GeometryNode(Geometry geom, int size, int index)
+  {
+    this(geom, size, indexString(index));
+  }
+
+  public abstract Geometry getGeometry();
+
+  public void setIndex(int index)
+  {
+    this.index = index;
+  }
+
+  public boolean isLeaf()
+  {
+    return isLeaf;
+  }
+
+  public GeometryNode getChildAt(int index)
+  {
+    if (isLeaf)
+      return null;
+    populateChildren();
+    return (GeometryNode) children.get(index);
+  }
+
+  public int getChildCount()
+  {
+    if (isLeaf)
+      return 0;
+    populateChildren();
+    return children.size();
+  }
+
+  public int getIndexOfChild(GeometryNode child)
+  {
+    if (isLeaf)
+      return -1;
+    populateChildren();
+    return children.indexOf(child);
+  }
+
+  public String getText()
+  {
+    if (index >= 0) {
+      return indexString(index) + " : " + text;
+    }
+    return text;
+  }
+
+  private void populateChildren()
+  {
+    // already initialized
+    if (children != null)
+      return;
+
+    children = new ArrayList<GeometryNode>();
+    fillChildren();
+  }
+
+  protected abstract void fillChildren();
 }
 
-class LinearRingNode extends LineStringNode 
+class PolygonNode extends GeometryNode
 {
-	public LinearRingNode(LinearRing ring)
-	{
-		super(ring);
-	}
-	public LinearRingNode(LinearRing ring, String tag)
-	{
-		super(ring, tag);
-	}
+  Polygon poly;
+
+  PolygonNode(Polygon poly)
+  {
+    super(poly, poly.getNumPoints(), null);
+    this.poly = poly;
+  }
+
+  public Geometry getGeometry()
+  {
+    return poly;
+  }
+
+  protected void fillChildren()
+  {
+    children.add(new LinearRingNode((LinearRing) poly.getExteriorRing(),
+        "Shell"));
+    for (int i = 0; i < poly.getNumInteriorRing(); i++) {
+      children.add(new LinearRingNode((LinearRing) poly.getInteriorRingN(i),
+          "Hole " + i));
+    }
+  }
+
 }
-class PointNode extends GeometryNode 
+
+class LineStringNode extends GeometryNode
 {
-	Point pt;
-	
-	public PointNode(Point p)
-	{
-		super(p);
-		pt = p;
-	}
-  public Geometry getGeometry() { return pt; }
-	protected void populateChildren()
-	{
-    createChildren();
-		children.add(create(pt.getCoordinate()));
-	}
+  LineString line;
 
+  public LineStringNode(LineString line)
+  {
+    this(line, null);
+  }
+
+  public LineStringNode(LineString line, String tag)
+  {
+    super(line, line.getNumPoints(), tag);
+    this.line = line;
+  }
+
+  public Geometry getGeometry()
+  {
+    return line;
+  }
+
+  protected void fillChildren()
+  {
+    populateChildren(line.getCoordinates());
+  }
+
+  private void populateChildren(Coordinate[] pt)
+  {
+    for (int i = 0; i < pt.length; i++) {
+      GeometryNode node = create(pt[i]);
+      node.setIndex(i);
+      children.add(node);
+    }
+  }
 }
 
-class GeometryCollectionNode extends GeometryNode 
+class LinearRingNode extends LineStringNode
 {
-	GeometryCollection coll;
-	
-	GeometryCollectionNode(GeometryCollection coll)
-	{
-		super(coll, coll.getNumGeometries(), null);
-		this.coll = coll;
-	}
-	
-  public Geometry getGeometry() { return coll; }
-  
-	protected void populateChildren()
-	{
-    createChildren();
-		for (int i = 0; i < coll.getNumGeometries(); i++) {
-			GeometryNode node = create(coll.getGeometryN(i));
-			node.setIndex(i);
-			children.add(node);
-		}
-	}
+  public LinearRingNode(LinearRing ring)
+  {
+    super(ring);
+  }
+
+  public LinearRingNode(LinearRing ring, String tag)
+  {
+    super(ring, tag);
+  }
 }
 
-class CoordinateNode extends GeometryNode 
+class PointNode extends GeometryNode
+{
+  Point pt;
+
+  public PointNode(Point p)
+  {
+    super(p);
+    pt = p;
+  }
+
+  public Geometry getGeometry()
+  {
+    return pt;
+  }
+
+  protected void fillChildren()
+  {
+    children.add(create(pt.getCoordinate()));
+  }
+
+}
+
+class GeometryCollectionNode extends GeometryNode
+{
+  GeometryCollection coll;
+
+  GeometryCollectionNode(GeometryCollection coll)
+  {
+    super(coll, coll.getNumGeometries(), null);
+    this.coll = coll;
+  }
+
+  public Geometry getGeometry()
+  {
+    return coll;
+  }
+
+  protected void fillChildren()
+  {
+    for (int i = 0; i < coll.getNumGeometries(); i++) {
+      GeometryNode node = create(coll.getGeometryN(i));
+      node.setIndex(i);
+      children.add(node);
+    }
+  }
+}
+
+class CoordinateNode extends GeometryNode
 {
   Coordinate coord;
-	public CoordinateNode(Coordinate coord)
-	{
-		super(coord.x + ", " + coord.y);
-		isLeaf = true;
-		this.coord = coord;
-	}
-  public Geometry getGeometry() 
-  { 
+
+  public CoordinateNode(Coordinate coord)
+  {
+    super(coord.x + ", " + coord.y);
+    isLeaf = true;
+    this.coord = coord;
+  }
+
+  public Geometry getGeometry()
+  {
     GeometryFactory geomFact = new GeometryFactory();
     return geomFact.createPoint(coord);
   }
-  protected void populateChildren()
+
+  protected void fillChildren()
   {
     // do nothing here, since this is a leaf node
     // no need to create children array, since it will never be accessed
   }
 }
-
