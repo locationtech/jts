fef99dea961d520e42f5bca0692658ec5dba6524
dr-jts <mtnclimb@gmail.com>
2012-06-26 15:13:11 +0000

    Performance improvement for DD orientationIndex Added fast filter for orientationIndex

--- jts/java/src/com/vividsolutions/jts/algorithm/CGAlgorithmsDD.java
+++ jts/java/src/com/vividsolutions/jts/algorithm/CGAlgorithmsDD.java
@@ -43,25 +43,116 @@ import com.vividsolutions.jts.math.DD;
  */
 public class CGAlgorithmsDD
 {
+  /**
+   * Returns the index of the direction of the point <code>q</code> relative to
+   * a vector specified by <code>p1-p2</code>.
+   * 
+   * @param p1 the origin point of the vector
+   * @param p2 the final point of the vector
+   * @param q the point to compute the direction to
+   * 
+   * @return 1 if q is counter-clockwise (left) from p1-p2
+   * @return -1 if q is clockwise (right) from p1-p2
+   * @return 0 if q is collinear with p1-p2
+   */
   public static int orientationIndex(Coordinate p1, Coordinate p2, Coordinate q)
   {
-    DD dx1 = DD.valueOf(p2.x).selfSubtract(p1.x);
-    DD dy1 = DD.valueOf(p2.y).selfSubtract(p1.y);
-    DD dx2 = DD.valueOf(q.x).selfSubtract(p2.x);
-    DD dy2 = DD.valueOf(q.y).selfSubtract(p2.y);
+    // fast filter for orientation index
+    // avoids use of slow extended-precision arithmetic in many cases
+    int index = orientationIndexFilter(p1, p2, q);
+    if (index <= 1) return index;
+    
+    // normalize coordinates
+    DD dx1 = DD.valueOf(p2.x).selfAdd(-p1.x);
+    DD dy1 = DD.valueOf(p2.y).selfAdd(-p1.y);
+    DD dx2 = DD.valueOf(q.x).selfAdd(-p2.x);
+    DD dy2 = DD.valueOf(q.y).selfAdd(-p2.y);
 
-    return signOfDet2x2(dx1, dy1, dx2, dy2);
+    // sign of determinant - unrolled for performance
+    return dx1.selfMultiply(dy2).selfSubtract(dy1.selfMultiply(dx2)).signum();
   }
   
+  /**
+   * Computes the sign of the determinant of the 2x2 matrix
+   * with the given entries.
+   * 
+   * @return -1 if the determinant is negative,
+   * @return  1 if the determinant is positive,
+   * @return  0 if the determinant is 0.
+   */
   public static int signOfDet2x2(DD x1, DD y1, DD x2, DD y2)
   {
-    DD det = x1.multiply(y2).subtract(y1.multiply(x2));
-    if (det.isZero())
-      return 0;
-    if (det.isNegative())
-      return -1;
-    return 1;
+    DD det = x1.multiply(y2).selfSubtract(y1.multiply(x2));
+    return det.signum();
+  }
 
+  /**
+   * A value which is safely greater than the
+   * relative round-off error in double-precision numbers
+   */
+  private static final double DP_SAFE_EPSILON = 1e-15;
+
+  /**
+   * A filter for computing the orientation index of three coordinates.
+   * <p>
+   * If the orientation can be computed safely using standard DP
+   * arithmetic, this routine returns the orientation index.
+   * Otherwise, a value i > 1 is returned.
+   * In this case the orientation index must 
+   * be computed using some other more robust method.
+   * The filter is fast to compute, so can be used to 
+   * avoid the use of slower robust methods except when they are really needed,
+   * thus providing better average performance.
+   * <p>
+   * Uses an approach due to Jonathan Shewchuk, which is in the public domain.
+   * 
+   * @param pa a coordinate
+   * @param pb a coordinate
+   * @param pc a coordinate
+   * @return the orientation index if it can be computed safely
+   * @return i > 1 if the orientation index cannot be computed safely
+   */
+  private static int orientationIndexFilter(Coordinate pa, Coordinate pb, Coordinate pc)
+  {
+    double detsum;
+
+    double detleft = (pa.x - pc.x) * (pb.y - pc.y);
+    double detright = (pa.y - pc.y) * (pb.x - pc.x);
+    double det = detleft - detright;
+
+    if (detleft > 0.0) {
+      if (detright <= 0.0) {
+        return signum(det);
+      }
+      else {
+        detsum = detleft + detright;
+      }
+    }
+    else if (detleft < 0.0) {
+      if (detright >= 0.0) {
+        return signum(det);
+      }
+      else {
+        detsum = -detleft - detright;
+      }
+    }
+    else {
+      return signum(det);
+    }
+
+    double errbound = DP_SAFE_EPSILON * detsum;
+    if ((det >= errbound) || (-det >= errbound)) {
+      return signum(det);
+    }
+
+    return 2;
+  }
+
+  private static int signum(double x)
+  {
+    if (x > 0) return 1;
+    if (x < 0) return -1;
+    return 0;
   }
 
 }
