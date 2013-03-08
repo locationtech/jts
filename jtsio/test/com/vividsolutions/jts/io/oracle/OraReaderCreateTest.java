485d422d22abb3cd866d29bac5265ca95b0c7729
dr-jts <mtnclimb@gmail.com>
2013-03-08 17:52:53 +0000

    Added more tests

--- jtsio/test/com/vividsolutions/jts/io/oracle/OraReaderCreateTest.java
+++ jtsio/test/com/vividsolutions/jts/io/oracle/OraReaderCreateTest.java
@@ -71,6 +71,12 @@ public class OraReaderCreateTest extends BaseOraTestCase
     checkValue(oraGeom, "POINT (50 50)");
   }
 
+  public void testXYM_Point() throws Exception {
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3301,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1),
+	    		MDSYS.SDO_ORDINATE_ARRAY(50,50,100));
+	    checkValue(oraGeom, 3, "POINT (50 50 100)");
+  }
+
   public void testXYZM_Point() throws Exception {
 	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(4001,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1),
 	    		MDSYS.SDO_ORDINATE_ARRAY(50,50,100,200));
@@ -86,7 +92,7 @@ public class OraReaderCreateTest extends BaseOraTestCase
   public void testXY_OrientedPoint() throws Exception {
 	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(2001,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1, 3,1,0),MDSYS.SDO_ORDINATE_ARRAY(12,14, 0.3,0.2));
 	    checkValue(oraGeom, "POINT (12 14)");
-	  }
+  }
 
   public void testXYZ_OrientedPoint() throws Exception {
 	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(2001,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1, 3,1,0),MDSYS.SDO_ORDINATE_ARRAY(12,14, 0.3,0.2));
@@ -94,10 +100,16 @@ public class OraReaderCreateTest extends BaseOraTestCase
   }
   
   public void testXY_MultiPoint_MultiElem() throws Exception {
-    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(2005,32639,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1,3,1,1),MDSYS.SDO_ORDINATE_ARRAY(548810.5,3956383.4, 548766.8,3956415.9));
-    checkValue(oraGeom, "MULTIPOINT ((548810.5 3956383.4), (548766.8 3956415.9))");
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(2005,32639,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1,3,1,1),MDSYS.SDO_ORDINATE_ARRAY(548810.5,3956383.4, 548766.8,3956415.9));
+	    checkValue(oraGeom, "MULTIPOINT ((548810.5 3956383.4), (548766.8 3956415.9))");
   }
-  
+	  
+  public void testXYM_MultiPoint_MultiElem() throws Exception {
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3005,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1,4,1,1),
+	    		MDSYS.SDO_ORDINATE_ARRAY(1,1,99, 2,2,99));
+	    checkValue(oraGeom, "MULTIPOINT ((1 1), (2 2))");
+  }
+	  
   public void testXYZ_MultiPoint() throws Exception {
     OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3005,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1,2),
     		MDSYS.SDO_ORDINATE_ARRAY(50,50,5, 100,200,300));
@@ -122,11 +134,29 @@ public class OraReaderCreateTest extends BaseOraTestCase
 	    checkValue(oraGeom, "LINESTRING (1 1, 2 2)");
   }
 
+  public void testXYZM_LineString() throws Exception {
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(4402,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),
+	    		MDSYS.SDO_ORDINATE_ARRAY(0,0,2,3,50,50,100,200));
+	    checkValue(oraGeom, "LINESTRING (0 0, 50 50)");
+  }
+
   public void testXYMZ_LineString() throws Exception {
 	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(4302,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1),
 	    		MDSYS.SDO_ORDINATE_ARRAY(0,0,2,3,50,50,100,200));
 	    checkValue(oraGeom, "LINESTRING (0 0, 50 50)");
   }
+
+  public void testXYZ_MultiLineString() throws Exception {
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3006,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1,7,2,1),
+	    		MDSYS.SDO_ORDINATE_ARRAY(0,0,2,50,50,100,10,10,12,150,150,110));
+	    checkValue(oraGeom, "MULTILINESTRING ((0 0, 50 50), (10 10, 150 150))");
+  }
+
+  public void testXYZM_MultiLineString() throws Exception {
+	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(4406,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1,9,2,1),
+	    		MDSYS.SDO_ORDINATE_ARRAY(0,0,2,3,50,50,100,200,10,10,12,13,150,150,110,210));
+	    checkValue(oraGeom, "MULTILINESTRING ((0 0, 50 50), (10 10, 150 150))");
+  	}
 
   public void testXYMZ_MultiLineString() throws Exception {
 	    OraGeom oraGeom = MDSYS.SDO_GEOMETRY(4306,8307,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,2,1,9,2,1),
@@ -138,6 +168,34 @@ public class OraReaderCreateTest extends BaseOraTestCase
 	  OraGeom oraGeom = MDSYS.SDO_GEOMETRY(2003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1),
 			  MDSYS.SDO_ORDINATE_ARRAY(0,0,50,0,50,50,0,50,0,0));
 	    checkValue(oraGeom, "POLYGON ((0 0, 50 0, 50 50, 0 50, 0 0))");
+  }
+
+  public void testXYZ_Polygon() throws Exception {
+	  OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3003,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1),
+			  MDSYS.SDO_ORDINATE_ARRAY(0,0,99,50,0,99,50,50,99,0,50,99,0,0,99));
+	    checkValue(oraGeom, "POLYGON ((0 0, 50 0, 50 50, 0 50, 0 0))");
+  }
+
+  public void testXYM_Polygon() throws Exception {
+	  OraGeom oraGeom = MDSYS.SDO_GEOMETRY(3303,NULL,NULL,MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1),
+			  MDSYS.SDO_ORDINATE_ARRAY(0,0,99,50,0,99,50,50,99,0,50,99,0,0,99));
+	    checkValue(oraGeom, "POLYGON ((0 0, 50 0, 50 50, 0 50, 0 0))");
+  }
+
+  public void testXYZ_PolygonWithHole() throws Exception {
+	  checkValue( MDSYS.SDO_GEOMETRY(3003,  NULL,  NULL,
+				    MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 28,2003,1), 
+				    MDSYS.SDO_ORDINATE_ARRAY(2,4,99, 4,3,99, 10,3,99, 13,5,99, 13,9,99, 11,13,99, 5,13,99, 2,11,99, 2,4,99,
+				        7,5,99, 7,10,99, 10,10,99, 10,5,99, 7,5,99 ) ),
+				        "POLYGON ((2 4, 4 3, 10 3, 13 5, 13 9, 11 13, 5 13, 2 11, 2 4), (7 5, 7 10, 10 10, 10 5, 7 5))");
+  }
+
+  public void testXYM_PolygonWithHole() throws Exception {
+	  checkValue( MDSYS.SDO_GEOMETRY(3303,  NULL,  NULL,
+				    MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 28,2003,1), 
+				    MDSYS.SDO_ORDINATE_ARRAY(2,4,99, 4,3,99, 10,3,99, 13,5,99, 13,9,99, 11,13,99, 5,13,99, 2,11,99, 2,4,99,
+				        7,5,99, 7,10,99, 10,10,99, 10,5,99, 7,5,99 ) ),
+				        "POLYGON ((2 4, 4 3, 10 3, 13 5, 13 9, 11 13, 5 13, 2 11, 2 4), (7 5, 7 10, 10 10, 10 5, 7 5))");
   }
 
   public void testXY_RectangleMultiPolygon() throws Exception {
@@ -189,6 +247,17 @@ public class OraReaderCreateTest extends BaseOraTestCase
 			              5,3, 6,4, 6,3, 5,3 ) ), 
 	    		"GEOMETRYCOLLECTION (POINT (1 1), LINESTRING (1 2, 2 1), POLYGON ((2 2, 3 2, 3 3, 2 3, 2 2)), POLYGON ((5 1, 9 5, 5 5, 5 1), (5 3, 6 4, 6 3, 5 3)))");
   }
+  public void testXYM_GeometryCollection_Doc() throws Exception {
+	  checkValue(  MDSYS.SDO_GEOMETRY(3304, NULL, NULL,
+			        MDSYS.SDO_ELEM_INFO_ARRAY(1,1,1, 4,2,1, 10,1003,1, 25,1003,1, 37,2003,1),
+			        MDSYS.SDO_ORDINATE_ARRAY(
+			              1,1,99,
+			              1,2,99, 2,1,99,
+			              2,2,99, 3,2,99, 3,3,99, 2,3,99, 2,2,99,
+			              5,1,99, 9,5,99, 5,5,99, 5,1,99,
+			              5,3,99, 6,4,99, 6,3,99, 5,3,99 ) ), 
+	    		"GEOMETRYCOLLECTION (POINT (1 1), LINESTRING (1 2, 2 1), POLYGON ((2 2, 3 2, 3 3, 2 3, 2 2)), POLYGON ((5 1, 9 5, 5 5, 5 1), (5 3, 6 4, 6 3, 5 3)))");
+  }
   
   //====================================================================================
   // Cases from GeoTools
@@ -219,6 +288,30 @@ public class OraReaderCreateTest extends BaseOraTestCase
 			       MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 11,1003,1),
 			       MDSYS.SDO_ORDINATE_ARRAY(2,3, 7,3, 7,9, 2,9, 2,3,
 			             9,5, 13,5, 11,5, 9,5)			      ), 
+	    		"MULTIPOLYGON (((2 3, 7 3, 7 9, 2 9, 2 3)), ((9 5, 13 5, 11 5, 9 5)))");
+  }
+
+  public void testXYZ_MultiPolygon() throws Exception {
+	  checkValue(  MDSYS.SDO_GEOMETRY(3007, NULL,  NULL,
+			       MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 16,1003,1),
+			       MDSYS.SDO_ORDINATE_ARRAY(2,3,99, 7,3,99, 7,9,99, 2,9,99, 2,3,99,
+			             9,5,99, 13,5,99, 11,5,99, 9,5,99 )			      ), 
+	    		"MULTIPOLYGON (((2 3, 7 3, 7 9, 2 9, 2 3)), ((9 5, 13 5, 11 5, 9 5)))");
+  }
+
+  public void testXYM_MultiPolygon() throws Exception {
+	  checkValue(  MDSYS.SDO_GEOMETRY(3307, NULL,  NULL,
+			       MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 16,1003,1),
+			       MDSYS.SDO_ORDINATE_ARRAY(2,3,99, 7,3,99, 7,9,99, 2,9,99, 2,3,99,
+			             9,5,99, 13,5,99, 11,5,99, 9,5,99 )			      ), 
+	    		"MULTIPOLYGON (((2 3, 7 3, 7 9, 2 9, 2 3)), ((9 5, 13 5, 11 5, 9 5)))");
+  }
+
+  public void testXYZM_MultiPolygon() throws Exception {
+	  checkValue(  MDSYS.SDO_GEOMETRY(4307, NULL,  NULL,
+			       MDSYS.SDO_ELEM_INFO_ARRAY(1,1003,1, 21,1003,1),
+			       MDSYS.SDO_ORDINATE_ARRAY(2,3,99,88, 7,3,99,88, 7,9,99,88, 2,9,99,88, 2,3,99,88,
+			             9,5,99,88, 13,5,99,88, 11,5,99,88, 9,5,99,88 )			      ), 
 	    		"MULTIPOLYGON (((2 3, 7 3, 7 9, 2 9, 2 3)), ((9 5, 13 5, 11 5, 9 5)))");
   }
 
