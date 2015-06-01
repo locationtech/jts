f382a454b903a8a5aa329bc17d2a4c1cbd351300
dr-jts <mtnclimb@gmail.com>
2015-06-01 15:48:49 +0000

    Add Compute New button to Geometry Functions panel (II)

---
+++ jtsapp/src/main/java/com/vividsolutions/jtstest/testbuilder/controller/FunctionInvocation.java
@@ -0,0 +1,31 @@
+package com.vividsolutions.jtstest.testbuilder.controller;
+
+import com.vividsolutions.jtstest.function.GeometryFunction;
+import com.vividsolutions.jtstest.testbuilder.model.FunctionParameters;
+
+public class FunctionInvocation {
+
+  private GeometryFunction function;
+  private Object[] param;
+
+  public FunctionInvocation(GeometryFunction function, Object[] functionParams) {
+    this.function = function;
+    this.param = functionParams;
+  }
+
+  public String getFunctionCall() {
+    if (function == null)
+      return null;
+    return function.getCategory() 
+        + "." + function.getName()
+        + "(" + FunctionParameters.toString(param) + ")";
+  }
+
+  public GeometryFunction getFunction() {
+    return function;
+  }
+
+  public Object[] getParameters() {
+    return param;
+  }
+}
