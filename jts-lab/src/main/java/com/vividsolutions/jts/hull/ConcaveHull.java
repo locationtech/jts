21c9058c05d427365c77d8d152ba28248781cb38
dr-jts <mtnclimb@gmail.com>
2015-08-13 15:59:06 +0000

    Add Concave Hull experimental code

---
+++ jts-lab/src/main/java/com/vividsolutions/jts/hull/ConcaveHull.java
@@ -0,0 +1,41 @@
+package com.vividsolutions.jts.hull;
+
+import java.util.List;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.triangulate.DelaunayTriangulationBuilder;
+import com.vividsolutions.jts.triangulate.quadedge.QuadEdgeSubdivision;
+import com.vividsolutions.jts.triangulate.quadedge.QuadEdgeTriangle;
+
+public class ConcaveHull {
+  private Geometry geom;
+  private double tolerance;
+
+  public ConcaveHull(Geometry geom, double tolerance) {
+    this.geom = geom;
+    this.tolerance = tolerance;
+  }
+  
+  public Geometry getResult() {
+    QuadEdgeSubdivision subdiv = buildDelaunay();
+    List tris = extractTriangles(subdiv);
+    Geometry hull = computeHull(tris);
+    return hull;
+  }
+
+  private List extractTriangles(QuadEdgeSubdivision subdiv) {
+    return QuadEdgeTriangle.createOn(subdiv);
+  }
+
+  private Geometry computeHull(List tris) {
+    return null;
+    
+  }
+
+  private QuadEdgeSubdivision buildDelaunay() {
+    DelaunayTriangulationBuilder builder = new DelaunayTriangulationBuilder();
+    builder.setSites(geom);
+    return builder.getSubdivision();
+    
+  }
+}
---
+++ jts-lab/src/test/java/com/vividsolutions/jts/hull/ConcaveHullTest.java
@@ -0,0 +1,28 @@
+package com.vividsolutions.jts.hull;
+
+import test.jts.GeometryTestCase;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+public class ConcaveHullTest extends GeometryTestCase {
+
+  public ConcaveHullTest(String name) {
+    super(name);
+  }
+
+  public void testSimple() {
+    checkHull(
+        "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))",
+        150,
+        "POLYGON ((100 200, 200 180, 300 200, 200 190, 100 200))"
+        );
+  }
+  
+  private void checkHull(String inputWKT, double tolerance, String expectedWKT) {
+    Geometry input = read(inputWKT);
+    Geometry expected = read(expectedWKT);
+    ConcaveHull hull = new ConcaveHull(input, tolerance);
+    Geometry actual = hull.getResult();
+    checkEqual(expected, actual);
+  }
+}
