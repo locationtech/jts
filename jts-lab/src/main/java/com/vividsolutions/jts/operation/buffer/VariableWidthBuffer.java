3b9fcf0ea78de733d82dee7a583a95b5da3d7e5f
dr-jts <mtnclimb@gmail.com>
2015-08-17 19:52:42 +0000

    Add VariableWidthBuffer prototype

---
+++ jts-lab/src/main/java/com/vividsolutions/jts/operation/buffer/VariableWidthBuffer.java
@@ -0,0 +1,98 @@
+package com.vividsolutions.jts.operation.buffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryCollection;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineSegment;
+import com.vividsolutions.jts.geom.LineString;
+
+/**
+ * Creates a buffer with varying width.
+ * 
+ * @author Martin Davis
+ *
+ */
+public class VariableWidthBuffer {
+
+  public static Geometry buffer(Geometry line, double startWidth,
+      double endWidth) {
+    double[] width = VariableWidthBuffer.interpolate((LineString) line,
+        startWidth, endWidth);
+    VariableWidthBuffer vb = new VariableWidthBuffer(line, width);
+    return vb.getResult();
+  }
+
+  public static double[] interpolate(LineString line, double startDistance,
+      double endDistance) {
+    double[] widths = new double[line.getNumPoints()];
+    widths[0] = startDistance;
+    widths[widths.length - 1] = endDistance;
+
+    double totalLen = line.getLength();
+    Coordinate[] pts = line.getCoordinates();
+    double currLen = 0;
+    for (int i = 1; i < widths.length; i++) {
+      double segLen = pts[i].distance(pts[i - 1]);
+      currLen += segLen;
+      double lenFrac = currLen / totalLen;
+      double delta = lenFrac * (endDistance - startDistance);
+      widths[i] = startDistance + delta;
+    }
+    return widths;
+  }
+
+  private LineString line;
+  private double[] width;
+  private GeometryFactory geomFactory;
+
+  public VariableWidthBuffer(Geometry line, double[] width) {
+    this.line = (LineString) line;
+    this.width = width;
+    geomFactory = line.getFactory();
+  }
+
+  private Geometry getResult() {
+    List parts = new ArrayList();
+
+    Coordinate[] pts = line.getCoordinates();
+    for (int i = 0; i < line.getNumPoints(); i++) {
+      double dist = width[i] / 2;
+      Geometry ptBuf = line.getPointN(i).buffer(dist);
+      parts.add(ptBuf);
+
+      if (i >= 1) {
+        Coordinate[] curvePts = generateSegmentCurve(pts[i - 1], pts[i],
+            width[i - 1], width[i]);
+        Geometry segBuf = geomFactory.createPolygon(curvePts);
+        parts.add(segBuf);
+      }
+    }
+
+    GeometryCollection partsGeom = geomFactory
+        .createGeometryCollection(GeometryFactory.toGeometryArray(parts));
+    Geometry buffer = partsGeom.union();
+    return buffer;
+  }
+
+  private Coordinate[] generateSegmentCurve(Coordinate p0, Coordinate p1,
+      double width0, double width1) {
+    LineSegment seg = new LineSegment(p0, p1);
+
+    double dist0 = width0 / 2;
+    double dist1 = width1 / 2;
+    Coordinate s0 = seg.pointAlongOffset(0, dist0);
+    Coordinate s1 = seg.pointAlongOffset(1, dist1);
+    Coordinate s2 = seg.pointAlongOffset(1, -dist1);
+    Coordinate s3 = seg.pointAlongOffset(0, -dist0);
+
+    Coordinate[] pts = new Coordinate[] { s0, s1, s2, s3, s0 };
+
+    return pts;
+
+  }
+
+}
