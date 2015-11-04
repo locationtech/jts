812facedd87e5aab39d4f38bb21304457fc5d91d
dr-jts <mtnclimb@gmail.com>
2015-11-04 22:02:59 +0000

    Add AffineTransformationFunctions.fitToEnvelope

--- jtsapp/src/main/java/com/vividsolutions/jtstest/function/AffineTransformationFunctions.java
+++ jtsapp/src/main/java/com/vividsolutions/jtstest/function/AffineTransformationFunctions.java
@@ -45,6 +45,23 @@ public class AffineTransformationFunctions
     return new Coordinate(env.getMinX(), env.getMinY());
   }
   
+  private static double diagonalLength(Envelope env) {
+    return Math.sqrt(env.getWidth() * env.getWidth() + env.getHeight() * env.getHeight());
+  }
+
+  public static Geometry fitToEnvelope(Geometry g, Geometry box)
+  {
+    Envelope boxEnv = box.getEnvelopeInternal();
+    Envelope env = g.getEnvelopeInternal();
+    
+    double scale = diagonalLength(boxEnv) / diagonalLength(env); 
+    Coordinate centre = envelopeCentre(g);
+    AffineTransformation trans = AffineTransformation.scaleInstance(scale, scale, centre.x, centre.y);
+    Coordinate boxCentre = envelopeCentre(box);
+    trans.translate(boxCentre.x - centre.x, boxCentre.y - centre.y);
+    return trans.transform(g);
+  }
+  
   public static Geometry scale(Geometry g, double scale)
   {
     Coordinate centre = envelopeCentre(g);
