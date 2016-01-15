09f4e2df3c728e3ac4b15e08516803e15efe535f
dr-jts <mtnclimb@gmail.com>
2016-01-15 23:19:03 +0000

    Add StarCross test to ValidStressTest

--- jts/java/test/test/jts/perf/operation/valid/ValidStressTest.java
+++ jts/java/test/test/jts/perf/operation/valid/ValidStressTest.java
@@ -2,11 +2,13 @@ package test.jts.perf.operation.valid;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.util.AffineTransformation;
 import com.vividsolutions.jts.operation.valid.IsValidOp;
+import com.vividsolutions.jts.util.Stopwatch;
 
 /**
  * Stress-tests {@link IsValidOp} 
@@ -14,7 +16,7 @@ import com.vividsolutions.jts.operation.valid.IsValidOp;
  * In JTS 1.14 and earlier this takes a very long time to run, 
  * since all intersections are computed before the invalid result is returned. 
  * In fact it is only necessary to detect a single intersection in order
- * to determine invalidity.
+ * to determine invalidity, and this provides much faster performance.
  * 
  * @author mdavis
  *
@@ -22,7 +24,8 @@ import com.vividsolutions.jts.operation.valid.IsValidOp;
 public class ValidStressTest  
 {
   public static void main(String args[]) {
-	  (new ValidStressTest()).run();
+    (new ValidStressTest()).runComb();
+    (new ValidStressTest()).runStarCross();
   }
 
   public ValidStressTest() {
@@ -32,25 +35,78 @@ public class ValidStressTest
   
   static GeometryFactory geomFact = new GeometryFactory();
   
-  public void run()
+  public void runComb()
   {
-	Envelope env =  new Envelope(0,100,0,100);
-	MultiPolygon mp = crossingCombs(env);
-	//System.out.println(mp);
-	boolean isValid = mp.isValid();
-	System.out.println("Is Valid = " + isValid);
+    int size = 100;
+    Envelope env =  new Envelope(0,100,0,100);
+    Geometry geom = Comb.crossedComb(env, size, geomFact);
+    System.out.println(geom);
+    checkValid("Crossed combs (size = " + size + " )", geom);
   }
 
-  private MultiPolygon crossingCombs(Envelope env) {
-	Polygon comb1 = comb(env, SIZE);
-	Coordinate centre = env.centre();
-	AffineTransformation trans = AffineTransformation.rotationInstance(0.5 * Math.PI, centre.x, centre.y);
-	Polygon comb2 = (Polygon) trans.transform(comb1);    
-	MultiPolygon mp = geomFact.createMultiPolygon(new Polygon[] { comb1, comb2 } );
-	return mp;
+  public void runStarCross()
+  {
+    int size = 1000;
+    Envelope env =  new Envelope(0,100,0,100);
+    Polygon poly = StarCross.star(env, size, geomFact);
+    Geometry geom = poly.getBoundary();
+    //System.out.println(geom);
+    checkValid("StarCross " + geom.getGeometryType() + "   (size = " + size + " )", geom);
   }
 
-  static Polygon comb(Envelope env, int nArms)
+  public void checkValid(String name, Geometry g)
+  {
+    System.out.println("Running " + name);
+    Stopwatch sw = new Stopwatch();
+    boolean isValid = g.isValid();
+    System.out.println("Is Valid = " + isValid 
+        + "           Time: " + sw.getTimeString() );
+  }
+  
+  
+}
+
+class StarCross 
+{
+  public static Polygon star(Envelope env, int nSeg, GeometryFactory geomFact)
+  {     
+    Coordinate[] pts = new Coordinate[nSeg + 1];
+    Coordinate centre = env.centre();
+    double len = 0.5 * Math.min(env.getHeight(), env.getWidth());
+    double angInc = Math.PI + 2 * Math.PI / nSeg;
+    
+    double ang = 0;
+    for (int i = 0; i < nSeg; i++) {
+      double x = centre.x + len * Math.cos(ang);
+      double y = centre.x + len * Math.sin(ang);
+      pts[i] = new Coordinate(x, y);
+      ang += angInc;
+    }
+    pts[nSeg] = new Coordinate(pts[0]);
+    return geomFact.createPolygon(pts);
+  }
+}
+
+/**
+ * Creates comb-like geometries.
+ * Crossed combs provide a geometry with a very high ratio of intersections to edges.
+ * 
+ * @author Martin Davis
+ *
+ */
+class Comb
+{
+  
+  public static MultiPolygon crossedComb(Envelope env, int size, GeometryFactory geomFact) {
+    Polygon comb1 = comb(env, size, geomFact);
+    Coordinate centre = env.centre();
+    AffineTransformation trans = AffineTransformation.rotationInstance(0.5 * Math.PI, centre.x, centre.y);
+    Polygon comb2 = (Polygon) trans.transform(comb1);    
+    MultiPolygon mp = geomFact.createMultiPolygon(new Polygon[] { comb1, comb2 } );
+    return mp;
+  }
+
+  public static Polygon comb(Envelope env, int nArms, GeometryFactory geomFact)
   {	
 	int npts = 4 * (nArms - 1) + 2 + 2 + 1;
 	Coordinate[] pts = new Coordinate[npts];
