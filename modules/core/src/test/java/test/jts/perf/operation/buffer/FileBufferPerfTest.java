5858559c546d1b657b1c140cf6da1307a18f9183
lossyrob <rdemanuele@gmail.com>
2016-01-27 14:55:54 -0800

    Fixed TestFiles, made test pass.

--- modules/core/src/test/java/test/jts/perf/operation/buffer/FileBufferPerfTest.java
+++ modules/core/src/test/java/test/jts/perf/operation/buffer/FileBufferPerfTest.java
@@ -2,14 +2,14 @@
  * The JTS Topology Suite is a collection of Java classes that
  * implement the fundamental operations required to validate a given
  * geo-spatial data set to a known topological specification.
- * 
+ *
  * Copyright (C) 2016 Vivid Solutions
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * (http://www.eclipse.org/legal/epl-v10.html), and the Vivid Solutions BSD
  * License v1.0 (found at the root of the repository).
- * 
+ *
  */
 
 package test.jts.perf.operation.buffer;
@@ -22,7 +22,7 @@ import com.vividsolutions.jts.io.WKTReader;
 import com.vividsolutions.jts.util.*;
 import test.jts.*;
 
-public class FileBufferPerfTest 
+public class FileBufferPerfTest
 {
   static final int MAX_ITER = 1;
 
@@ -31,7 +31,7 @@ public class FileBufferPerfTest
   static WKTReader wktRdr = new WKTReader(fact);
 
   GeometryFactory factory = new GeometryFactory();
-  
+
   public static void main(String[] args) {
     FileBufferPerfTest test = new FileBufferPerfTest();
     try {
@@ -46,24 +46,22 @@ public class FileBufferPerfTest
 
   public FileBufferPerfTest() {
   }
-  
+
   public void test()
   throws Exception
   {
-    test(TestFiles.DATA_DIR + "africa.wkt");
-//    test(TestFiles.DATA_DIR + "world.wkt");
-//    test(TestFiles.DATA_DIR + "bc-250k.wkt");
-//    test(TestFiles.DATA_DIR + "bc_20K.wkt");
-    
-//    test("C:\\data\\martin\\proj\\jts\\data\\veg.wkt");
+    test(TestFiles.getResourceFilePath("africa.wkt"));
+    // test(TestFiles.getResourceFilePath("world.wkt"));
+    // test(TestFiles.getResourceFilePath("bc-250k.wkt"));
+    // test(TestFiles.getResourceFilePath("bc_20K.wkt"));
   }
-  
+
   public void test(String filename)
     throws Exception
   {
     WKTFileReader fileRdr = new WKTFileReader(filename, wktRdr);
     List polys = fileRdr.read();
-    
+
     runAll(polys, 0.01);
     runAll(polys, 0.1);
     runAll(polys, 1.0);
@@ -71,7 +69,7 @@ public class FileBufferPerfTest
     runAll(polys, 100.0);
     runAll(polys, 1000.0);
   }
-     
+
   void runAll(List polys, double distance)
   {
     System.out.println("Geom count = " + polys.size() + "   distance = " + distance);
--- modules/core/src/test/java/test/jts/perf/operation/buffer/FileBufferResultValidatorTest.java
+++ modules/core/src/test/java/test/jts/perf/operation/buffer/FileBufferResultValidatorTest.java
@@ -3,14 +3,14 @@
  * The JTS Topology Suite is a collection of Java classes that
  * implement the fundamental operations required to validate a given
  * geo-spatial data set to a known topological specification.
- * 
+ *
  * Copyright (C) 2016 Martin Davis
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * (http://www.eclipse.org/legal/epl-v10.html), and the Martin Davis BSD
  * License v1.0 (found at the root of the repository).
- * 
+ *
  */
 package test.jts.perf.operation.buffer;
 
@@ -37,7 +37,7 @@ import test.jts.junit.*;
 public class FileBufferResultValidatorTest extends TestCase {
 
 	WKTReader rdr = new WKTReader();
-	
+
   public FileBufferResultValidatorTest(String name) {
     super(name);
   }
@@ -46,13 +46,13 @@ public class FileBufferResultValidatorTest extends TestCase {
     junit.textui.TestRunner.run(FileBufferResultValidatorTest.class);
   }
 
-  public void testAfrica() throws Exception 
+  public void testAfrica() throws Exception
   {
-//    runTest(TestFiles.DATA_DIR + "world.wkt");
-    runTest("../../../../../data/africa.wkt");
+    //    runTest(TestFiles.getResourceFilePath("world.wkt"));
+    runTest("/testdata/africa.wkt");
   }
-  
-  void runTest(String resource) 
+
+  void runTest(String resource)
   throws Exception
   {
     InputStream is = this.getClass().getResourceAsStream(resource);
@@ -63,16 +63,16 @@ public class FileBufferResultValidatorTest extends TestCase {
   throws Exception
   {
     List polys = fileRdr.read();
-    
+
     runAll(polys, 0.01);
     runAll(polys, 0.1);
     runAll(polys, 1.0);
     runAll(polys, 10.0);
     runAll(polys, 100.0);
     runAll(polys, 1000.0);
-    
+
   }
-  
+
   void runAll(List geoms, double dist)
   {
   	Stopwatch sw = new Stopwatch();
@@ -90,7 +90,7 @@ public class FileBufferResultValidatorTest extends TestCase {
   {
   	Geometry buf = g.buffer(dist);
     BufferResultValidator validator = new BufferResultValidator(g, dist, buf);
-    
+
     if (! validator.isValid()) {
       String msg = validator.getErrorMessage();
 
