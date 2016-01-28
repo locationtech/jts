508ac3f9f31982255916f3277151812425690a73
Martin Davis <mtnclimb@gmail.com>
2016-01-27 16:44:08 -0800

    rename packages to org.locationtech.jtsexample

---
+++ modules/example/src/main/java/org/locationtech/jtsexample/geom/ExtendedCoordinate.java
@@ -0,0 +1,74 @@
+
+/*
+ * The JTS Topology Suite is a collection of Java classes that
+ * implement the fundamental operations required to validate a given
+ * geo-spatial data set to a known topological specification.
+ * 
+ * Copyright (C) 2016 Vivid Solutions
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Vivid Solutions BSD
+ * License v1.0 (found at the root of the repository).
+ * 
+ */
+package org.locationtech.jtsexample.geom;
+
+import org.locationtech.jts.geom.*;
+
+
+/**
+ * @version 1.7
+ */
+public class ExtendedCoordinate
+    extends Coordinate
+{
+    private static final long serialVersionUID = 8527484784733305576L;
+  // A Coordinate subclass should provide all of these methods
+
+  /**
+   * Default constructor
+   */
+  public ExtendedCoordinate()
+  {
+    super();
+    this.m = 0.0;
+  }
+
+  public ExtendedCoordinate(double x, double y, double z, double m)
+  {
+    super(x, y, z);
+    this.m = m;
+  }
+
+  public ExtendedCoordinate(Coordinate coord)
+  {
+    super(coord);
+    if (coord instanceof ExtendedCoordinate)
+      m = ((ExtendedCoordinate) coord).m;
+    else
+      m = Double.NaN;
+  }
+
+  public ExtendedCoordinate(ExtendedCoordinate coord)
+  {
+    super(coord);
+    m = coord.m;
+  }
+
+  /**
+   * An example of extended data.
+   * The m variable holds a measure value for linear referencing
+   */
+
+  private double m;
+
+  public double getM() { return m; }
+  public void setM(double m) { this.m = m; }
+
+  public String toString()
+  {
+    String stringRep = x + " " + y + " m=" + m;
+    return stringRep;
+  }
+}
---
+++ modules/example/src/main/java/org/locationtech/jtsexample/operation/linemerge/LineMergeExample.java
@@ -0,0 +1,88 @@
+
+/*
+ * The JTS Topology Suite is a collection of Java classes that
+ * implement the fundamental operations required to validate a given
+ * geo-spatial data set to a known topological specification.
+ * 
+ * Copyright (C) 2016 Vivid Solutions
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Vivid Solutions BSD
+ * License v1.0 (found at the root of the repository).
+ * 
+ */
+package org.locationtech.jtsexample.operation.linemerge;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.WKTReader;
+import org.locationtech.jts.operation.linemerge.LineMerger;
+
+
+/**
+ * Example of using the LineMerger class to sew together a set of fully noded 
+ * linestrings.
+ *
+ * @version 1.7
+ */
+public class LineMergeExample {
+  private WKTReader reader = new WKTReader();
+
+  public LineMergeExample() {
+  }
+
+  public static void main(String[] args) throws Exception {
+    LineMergeExample test = new LineMergeExample();
+    try {
+      test.run();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  void run() throws Exception {
+    Collection lineStrings = getData();
+    
+    LineMerger lineMerger = new LineMerger();
+    lineMerger.add(lineStrings);
+    Collection mergedLineStrings = lineMerger.getMergedLineStrings();
+    
+    System.out.println("Lines formed (" + mergedLineStrings.size() + "):");
+    System.out.println(mergedLineStrings);
+  }
+
+  Collection getData() {
+    Collection lines = new ArrayList();
+    lines.add(read("LINESTRING (220 160, 240 150, 270 150, 290 170)"));
+    lines.add(read("LINESTRING (60 210, 30 190, 30 160)"));
+    lines.add(read("LINESTRING (70 430, 100 430, 120 420, 140 400)"));
+    lines.add(read("LINESTRING (160 310, 160 280, 160 250, 170 230)"));
+    lines.add(read("LINESTRING (170 230, 180 210, 200 180, 220 160)"));
+    lines.add(read("LINESTRING (30 160, 40 150, 70 150)"));
+    lines.add(read("LINESTRING (160 310, 200 330, 220 340, 240 360)"));
+    lines.add(read("LINESTRING (140 400, 150 370, 160 340, 160 310)"));
+    lines.add(read("LINESTRING (160 310, 130 300, 100 290, 70 270)"));
+    lines.add(read("LINESTRING (240 360, 260 390, 260 410, 250 430)"));
+    lines.add(read("LINESTRING (70 150, 100 180, 100 200)"));
+    lines.add(read("LINESTRING (70 270, 60 260, 50 240, 50 220, 60 210)"));
+    lines.add(read("LINESTRING (100 200, 90 210, 60 210)"));
+
+    return lines;
+  }
+
+  Geometry read(String lineWKT) {
+    try {
+      Geometry geom = reader.read(lineWKT);
+
+      return geom;
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return null;
+  }
+}
