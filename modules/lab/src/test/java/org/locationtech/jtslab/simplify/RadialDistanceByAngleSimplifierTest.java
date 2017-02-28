45eb03280f734ba57b43ec19b12e34b7e569c010
FObermaier <felix.obermaier@netcologne.de>
2017-02-28 12:29:08 +0100

    Added RadialDistanceByAngleSimplifier (#69) to lab module Added RadialDistanceByAngleSimplifierTest

---
+++ modules/lab/src/test/java/org/locationtech/jtslab/simplify/RadialDistanceByAngleSimplifierTest.java
@@ -0,0 +1,123 @@
+package org.locationtech.jtslab.simplify;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateList;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jtslab.simplify.RadialDistanceByAngleSimplifier;
+
+import junit.framework.TestCase;
+
+public class RadialDistanceByAngleSimplifierTest extends TestCase 
+{
+  GeometryFactory _factory;
+  
+  public RadialDistanceByAngleSimplifierTest(String name) {
+    super(name);
+    _factory = new GeometryFactory();
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(RadialDistanceByAngleSimplifierTest.class);
+  }
+  
+  public void testSimplifyEmptyLineShouldReturnNull() {
+    Geometry input = _factory.createLineString(new Coordinate[0]);
+    Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(input, 0, 0);
+
+    assertNull(simplified);
+  }
+
+  public void testSimplifyVeryShortLineShouldReturnNull()
+  {
+      Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(10, 0)
+      });
+
+      Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+      assertNull(simplified);
+  }
+
+  public void testSimplifyLineAlreadySimplifiedShouldReturnItself()
+  {
+    Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(100, 0)
+      });
+
+    Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+    assertNotNull(simplified);
+    assertTrue(line.equalsExact(simplified));
+    assertTrue(line == simplified);
+  } 
+
+  public void testSimplifyLineZigZagShouldReturnRemoveIt()
+  {
+    Coordinate coordinateThatShouldBeRemoved = new Coordinate(1, 0);
+    Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(10, 0),
+          coordinateThatShouldBeRemoved,
+          new Coordinate(11, 0),
+          new Coordinate(0, 0)
+      });
+
+      Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+      CoordinateList coordinates = new CoordinateList(simplified.getCoordinates());
+      assertTrue(!coordinates.contains(coordinateThatShouldBeRemoved));
+  }
+
+  public void testSimplifyLineNoSimplificationNeededShouldReturnSameLine()
+  {
+      Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(100, 0),
+          new Coordinate(200, 0),
+          new Coordinate(300, 0),
+      });
+
+      Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+      assertNotNull(simplified);
+      assertTrue(line.equalsExact(simplified));
+  }
+
+  public void testSimplifyLineShouldSimplifyByAngleShouldReturnSimplifiedLine()
+  {
+    Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(100, 0),
+          new Coordinate(100, 20),
+          new Coordinate(100, 200)
+      });
+
+    Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+    assertEquals(line.getCoordinates().length - 1, simplified.getCoordinates().length);
+  }
+
+  public void testSimplifyLineShouldNotSimplifyByAngleDueToDistanceShouldReturnSimplifiedLine()
+  {
+    Geometry line = _factory.createLineString(new Coordinate[]
+      {
+          new Coordinate(0,0),
+          new Coordinate(100, 0),
+          new Coordinate(0, 1),
+          new Coordinate(100, 1)
+      });
+
+    Geometry simplified = RadialDistanceByAngleSimplifier.Simplify(line, 30, Math.PI / 2);
+
+    assertTrue(line.equalsExact(simplified));
+  }
+
+}
