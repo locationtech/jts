c7582632ddce030bb43481aa57995a5b5771660c
Martin Davis <mtnclimb@gmail.com>
2017-08-07 11:24:57 -0700

    Make far jar name and script version-independent
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- bin/testrunner.bat
+++ bin/testrunner.bat
@@ -1,6 +1,6 @@
 @echo off
 REM A batch file to run the JTS Test Runner.  
-REM Requirements: run the Maven build  (mvn clean install)
+REM Requirements: Maven build must be run to create fat jar  (mvn clean install)
 
-java -jar %~dp0\..\modules\tests\target\jts-tests-1.15.0-SNAPSHOT-jar-with-dependencies.jar %*
+java -jar %~dp0\..\modules\tests\target\JTSTestRunner-jar-with-dependencies.jar %*
 
