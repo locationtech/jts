c685b6582d5606af91532a211d1cdf3f72652963
Martin Davis <mtnclimb@gmail.com>
2017-08-06 19:56:42 -0700

    Add build of jts-tests jar with dependencies Update testrunner.bat script to run it

--- bin/testrunner.bat
+++ bin/testrunner.bat
@@ -1,24 +1,6 @@
 @echo off
-rem A batch file to run the JTS test runner.
+REM A batch file to run the JTS Test Runner.  
+REM Requirements: run the Maven build  (mvn clean install)
 
-set CLASSPATH=
-for %%i in (..\lib\*.*) do (
- set jarfile=%%i
+java -jar %~dp0\..\modules\tests\target\jts-tests-1.15.0-SNAPSHOT-jar-with-dependencies.jar %*
 
- rem If we append to a variable inside the for, only the last entry will
- rem be kept. So append to the variable outside the for.
- rem See http://www.experts-exchange.com/Operating_Systems/MSDOS/Q_20561701.html.
- rem [Jon Aquino]
-
- call :setclass
-)  
-
-start javaw com.vividsolutions.jtstest.testrunner.TopologyTestApp -Properties testrunner.properties -GUI
-
-goto :eof
-
-:setclass
-set CLASSPATH=%jarfile%;%CLASSPATH%
-set jarfile=
-
-:eof
