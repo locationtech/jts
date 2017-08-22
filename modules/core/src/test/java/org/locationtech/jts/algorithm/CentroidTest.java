9e71b7d173137dd5dfc88bdae97eb43b46f47363
Daniel Baston <dbaston@gmail.com>
2017-08-22 05:53:51 -0400

    Fix formatting of tolerance constant

--- modules/core/src/test/java/org/locationtech/jts/algorithm/CentroidTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/CentroidTest.java
@@ -7,7 +7,7 @@ import org.locationtech.jts.io.WKTReader;
 
 public class CentroidTest extends TestCase {
 
-    private static final double tolerance = 1e-10;
+    private static final double TOLERANCE = 1e-10;
 
     public CentroidTest(String name) {
         super(name);
@@ -44,7 +44,7 @@ public class CentroidTest extends TestCase {
                 "MULTIPOLYGON ((( -92.661322 36.58994900000003, -92.66132199999993 36.58994900000005, -92.66132199999993 36.589949000000004, -92.661322 36.589949, -92.661322 36.58994900000003)), (( -92.65560500000008 36.58708800000005, -92.65560499999992 36.58708800000005, -92.65560499998745 36.587087999992576, -92.655605 36.587088, -92.65560500000008 36.58708800000005 )), (( -92.65512450000065 36.586800000000466, -92.65512449999994 36.58680000000004, -92.65512449998666 36.5867999999905, -92.65512450000065 36.586800000000466 )))"
         );
 
-        assertTrue(areaWeightedCentroid(g).equals2D(g.getCentroid().getCoordinate(), tolerance));
+        assertTrue(areaWeightedCentroid(g).equals2D(g.getCentroid().getCoordinate(), TOLERANCE));
     }
 
 }
