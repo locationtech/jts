41d634ef30265ef857e961f2a2c5f01140b696f4
Martin Davis <mtnclimb@gmail.com>
2017-08-22 16:33:32 -0700

    Rename TestBuilder fatjar and update bat script (#126)
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- bin/testbuilder.bat
+++ bin/testbuilder.bat
@@ -1,26 +1,13 @@
 @echo off
-rem A batch file to run the JTS Test Builder
+rem Windows batch file to run the JTS Test Builder from the Maven target directory
 
-set CLASSPATH=
-for %%i in (..\lib\*.*) do (
- set jarfile=%%i
+set JAVA_OPTS=-Xms256M -Xmx1024M
 
- rem If we append to a variable inside the for, only the last entry will
- rem be kept. So append to the variable outside the for.
- rem See http://www.experts-exchange.com/Operating_Systems/MSDOS/Q_20561701.html.
- rem [Jon Aquino]
+REM to change L&F if desired.  Blank is default
+REM JAVA_LOOKANDFEEL="-Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel"
+set JAVA_LOOKANDFEEL=
 
- call :setclass
-)  
+REM Default app options can be added here
+set APP_OPTS=%*
 
-set MEMSIZE=256
-
-start javaw -Xmx%MEMSIZE%M com.vividsolutions.jtstest.testbuilder.JTSTestBuilder
-
-goto :eof
-
-:setclass
-set CLASSPATH=%jarfile%;%CLASSPATH%
-set jarfile=
-
-:eof
+javaw %JAVA_OPTS% %JAVA_LOOKANDFEEL% -jar %~dp0\..\modules\app\target\JTSTestBuilder-jar-with-dependencies.jar %APP_OPTS%
