d9f5231b8b0501c0ac48d063a067950db63e7f42
Martin Davis <mtnclimb@gmail.com>
2017-08-27 10:50:48 -0700

    Add WindingNumber implementation Better code organization
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/test/jts/perf/algorithm/RayCrossingCounterStressTest.java
+++ modules/core/src/test/java/test/jts/perf/algorithm/RayCrossingCounterStressTest.java
@@ -21,28 +21,29 @@ import test.jts.GeometryTestCase;
  * determining the orientation of the point relative to the side.
  * The test fails if these are not consistent.
  * <p>
- * The stress test reveals that the original RayCrossingCounter
- * has inconsistencies with the DD orientation index computation
- * (which is now the standard in JTS, due to improved robustness).
+ * The stress test reveals that the original {@link RayCrossingCounter}
+ * has inconsistencies with the 
+ * {@link CGAlgorithmsDD#orientationIndex(Coordinate, Coordinate, Coordinate)}
+ * orientation index computation
+ * (which is now the standard in JTS, due to its improved robustness).
  * The {@link RayCrossingCounterDD} implementation is consistent,
  * as expected.
  * <p>
  * Note that the inconsistency does not indicate which algorithm is 
  * "more correct" - just that they produce different results.
  * However, it is highly likely that the original RCC algorithm
- * is not robust, since it involves significant arithmetic calcuation.
+ * is <b>not</b> as robust as RCCDD, since it uses a 
+ * series of double-precision arithmetic operations.
  * 
  * @author Martin Davis
  *
  */
 public class RayCrossingCounterStressTest extends GeometryTestCase {
   
-
   public static void main(String args[]) {
     TestRunner.run(RayCrossingCounterStressTest.class);
   }
 
-  PrecisionModel pmFixed_1 = new PrecisionModel(1.0);
   private boolean isAllConsistent = true;
   private int testCount;
   private int failureCount;
@@ -86,6 +87,7 @@ public class RayCrossingCounterStressTest extends GeometryTestCase {
   boolean checkTriangleConsistent(Coordinate[] triPts, Coordinate pt) {
     testCount++;
     //boolean isPointInRing = CGAlgorithms.isPointInRing(pt, triPts);
+    //boolean isPointInRing = pointInRingWindingNumber(pt, triPts);
     boolean isPointInRing = Location.INTERIOR == RayCrossingCounter.locatePointInRing(pt, triPts);
     //boolean isPointInRing = Location.INTERIOR == RayCrossingCounterDD.locatePointInRing(pt, triPts);
     
@@ -94,8 +96,8 @@ public class RayCrossingCounterStressTest extends GeometryTestCase {
     // if collinear can't determine a failure
     if (orientation == CGAlgorithms.COLLINEAR) return true;
     
-    boolean bothOutside = ! isPointInRing && orientation == CGAlgorithms.COUNTERCLOCKWISE;
-    boolean bothInside = isPointInRing && orientation == CGAlgorithms.CLOCKWISE;
+    boolean bothOutside = ! isPointInRing && orientation == CGAlgorithms.LEFT;
+    boolean bothInside = isPointInRing && orientation == CGAlgorithms.RIGHT;
     boolean isConsistent = bothOutside || bothInside;
     
     if (! isConsistent) {
@@ -108,7 +110,6 @@ public class RayCrossingCounterStressTest extends GeometryTestCase {
           + "  tri: " + toPolygon(triPts) );
     }
     return isConsistent;
-    
   }
   
   public static String toPolygon(Coordinate[] coord)
@@ -129,4 +130,37 @@ public class RayCrossingCounterStressTest extends GeometryTestCase {
     return buf.toString();
   }
 
+  /**
+   * Winding Number algorithm included for comparison purposes.
+   * This is almost equivalent to the RayCrossing algorithm,
+   * except that it checks all crossing segments 
+   * instead of just the ones to the right of the point.
+   * <p>
+   * This produces the same results as CGAlgorithmsDD
+   * 
+   * @param p point to test
+   * @param ring coordinates of ring (with same first and last point)
+   * @return true if the point lies inside the ring
+   */
+  private static boolean pointInRingWindingNumber(Coordinate p, Coordinate[] ring) {
+    int winding = 0; // the winding number counter
+
+    int n = ring.length;
+
+    for (int i = 0; i < n-1; i++) { 
+      if (ring[i].y <= p.y) { 
+        // detect an upward crossing
+        if (ring[i + 1].y > p.y) {
+          if (CGAlgorithms.LEFT == CGAlgorithms.orientationIndex(ring[i], ring[i + 1], p)) 
+            winding++; 
+        }
+      } else { 
+        // detect a downward crossing
+        if (ring[i + 1].y <= p.y) 
+          if (CGAlgorithms.RIGHT == CGAlgorithms.orientationIndex(ring[i], ring[i + 1], p)) 
+            winding--; 
+      }
+    }
+    return winding != 0;
+  }
 }
