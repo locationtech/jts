6fac55a95727fab0989be39eb8491c1bb27666dd
Martin Davis <mtnclimb@gmail.com>
2017-08-31 15:30:49 -0700

    Improvements to PointInArea classes: support Rings, full Location information, addd unit tests
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

---
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/locate/IndexedPointInAreaLocatorTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Vivid Solutions.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+package org.locationtech.jts.algorithm.locate;
+
+import org.locationtech.jts.algorithm.AbstractPointInRingTest;
+import org.locationtech.jts.algorithm.MCPointInRing;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.geom.Location;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.io.WKTReader;
+
+import junit.textui.TestRunner;
+
+/**
+ * Tests IndexedPointInAreaLocator algorithms
+ *
+ * @version 1.7
+ */
+public class IndexedPointInAreaLocatorTest extends AbstractPointInRingTest {
+
+  private WKTReader reader = new WKTReader();
+
+  public static void main(String args[]) {
+    TestRunner.run(IndexedPointInAreaLocatorTest.class);
+  }
+
+  public IndexedPointInAreaLocatorTest(String name) { super(name); }
+
+
+   protected void runPtInRing(int expectedLoc, Coordinate pt, String wkt)
+      throws Exception
+  {
+    Geometry geom = reader.read(wkt);
+    IndexedPointInAreaLocator loc = new IndexedPointInAreaLocator(geom);
+    int result = loc.locate(pt);
+    assertEquals(expectedLoc, result);
+  }
+
+}
