b4f673a4c2d006bdeb9efdc65ebd6d3532e11df2
Martin Davis <mtnclimb@gmail.com>
2017-09-02 08:47:35 -0700

    Shorten JTSTestRunner jar name
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- bin/testrunner.bat
+++ bin/testrunner.bat
@@ -2,5 +2,5 @@
 REM A batch file to run the JTS Test Runner.  
 REM Requirements: Maven build must be run to create fat jar  (mvn clean install)
 
-java -jar %~dp0\..\modules\tests\target\JTSTestRunner-jar-with-dependencies.jar %*
+java -jar %~dp0\..\modules\tests\target\JTSTestRunner.jar %*
 
