1b7bb75fcdeb1cbde1fe6c6deb2cde18929868d7
Martin Davis <mtnclimb@gmail.com>
2017-09-04 22:49:57 -0700

    Move length functions to Length class
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

---
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/LengthTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016 Vivid Solutions.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+package org.locationtech.jts.algorithm;
+
+import org.locationtech.jts.geom.CoordinateSequence;
+import org.locationtech.jts.geom.LineString;
+
+import junit.textui.TestRunner;
+import test.jts.GeometryTestCase;
+
+public class LengthTest extends GeometryTestCase {
+  public static void main(String args[]) {
+    TestRunner.run(LengthTest.class);
+  }
+
+  public LengthTest(String name) { super(name); }
+
+  public void testArea() {
+    checkLengthOfLine("LINESTRING (100 200, 200 200, 200 100, 100 100, 100 200)", 400.0);
+  }
+  
+  void checkLengthOfLine(String wkt, double expectedLen) {
+    LineString ring = (LineString) read(wkt);
+
+    CoordinateSequence pts = ring.getCoordinateSequence();
+    double actual = Length.ofLine(pts);
+    assertEquals(actual, expectedLen);
+  }
+}
