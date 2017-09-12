4ca04942c35c148c3a1e54cdc77b104970acd4ff
Martin Davis <mtnclimb@gmail.com>
2017-09-12 09:15:06 -0700

    Move unused NestedRing classes out of main codebase
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/test/jts/perf/operation/valid/QuadtreeNestedRingTester.java
+++ modules/core/src/test/java/test/jts/perf/operation/valid/QuadtreeNestedRingTester.java
@@ -10,7 +10,7 @@
  *
  * http://www.eclipse.org/org/documents/edl-v10.php.
  */
-package org.locationtech.jts.operation.valid;
+package test.jts.perf.operation.valid;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +21,7 @@ import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geom.LinearRing;
 import org.locationtech.jts.geomgraph.GeometryGraph;
 import org.locationtech.jts.index.quadtree.Quadtree;
+import org.locationtech.jts.operation.valid.IsValidOp;
 import org.locationtech.jts.util.Assert;
 
 /**
