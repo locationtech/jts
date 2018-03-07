714ce389c4ce50373bcc63548083b56ca4deaed6
Martin Davis <mtnclimb@gmail.com>
2018-03-06 16:55:19 -0800

    Fix SVG output for Points
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/util/io/SVGWriter.java
+++ modules/app/src/main/java/org/locationtech/jtstest/util/io/SVGWriter.java
@@ -345,9 +345,7 @@ public class SVGWriter
       PrecisionModel precisionModel)
     throws IOException
   {
-      writer.write("(");
-      appendCoordinate(coordinate, writer);
-      writer.write(")");
+    writer.write("<circle cx='" + coordinate.x + "' cy='" + coordinate.y + "' r='1' />\n");
   }
 
   /**
@@ -505,20 +503,16 @@ public class SVGWriter
     throws IOException
   {
     if (multiPoint.isEmpty()) {
-      writer.write("EMPTY");
+      writer.write(" ");
     }
     else {
-      writer.write("(");
+      int level2 = level;
       for (int i = 0; i < multiPoint.getNumGeometries(); i++) {
         if (i > 0) {
-          writer.write(", ");
-          indentCoords(i, level + 1, writer);
+          level2 = level + 1;
         }
-        writer.write("(");
-        appendCoordinate(((Point) multiPoint.getGeometryN(i)).getCoordinate(), writer);
-        writer.write(")");
-     }
-      writer.write(")");
+        appendPoint(multiPoint.getGeometryN(i).getCoordinate(), level2, writer, multiPoint.getPrecisionModel());
+      }
     }
   }
 
