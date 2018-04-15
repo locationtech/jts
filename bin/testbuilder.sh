eca80edf196bfbb5613466a3c98d4c69b614917b
Dan Baston <dbaston@gmail.com>
2018-04-15 16:59:57 -0400

    Fix Unix test scripts (#275)
    Marked files as executable, updated references to current JAR paths, and
    removed pre-LocationTech class references.
    
    Signed-off-by: Daniel Baston <dbaston@gmail.com>

--- bin/testbuilder.sh
+++ bin/testbuilder.sh
@@ -9,20 +9,10 @@ JAVA_OPTS="-Xms256M -Xmx1024M"
 APP_OPTS=""
 
 if test "x$JTS_LIB_DIR" = "x"; then
-        JTS_LIB_DIR=`dirname $0`/../lib/
+        JTS_LIB_DIR=`dirname $0`/../modules/app/target
 fi
-
-#---------------------------------#
-# dynamically build the classpath #
-#---------------------------------#
-CP=
-for i in `ls ${JTS_LIB_DIR}/*.jar`
-do
-  CP=${CP}:${i}
-done
 
 #---------------------------#
 # run the program           #
 #---------------------------#
-MAIN=com.vividsolutions.jtstest.testbuilder.JTSTestBuilder
-java -cp ".:${CP}" $JAVA_OPTS  $JAVA_LOOKANDFEEL $MAIN $APP_OPTS
+java -jar ${JAVA_OPTS} ${JAVA_LOOKANDFEEL} -jar ${JTS_LIB_DIR}/JTSTestBuilder.jar ${APP_OPTS}
