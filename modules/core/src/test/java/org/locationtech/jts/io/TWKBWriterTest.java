24d4926468c3153172c6c262b05c22d60dd5e800
Jim Hughes <jnh5y@ccri.com>
2018-05-10 22:46:18 -0400

    Added tests for all geometry types.
    Signed-off-by: Jim Hughes <jnh5y@ccri.com>

--- modules/core/src/test/java/org/locationtech/jts/io/TWKBWriterTest.java
+++ modules/core/src/test/java/org/locationtech/jts/io/TWKBWriterTest.java
@@ -115,6 +115,94 @@ public class TWKBWriterTest extends TestCase {
         check("POINT(1234.567891234 98765.54321)", 5, 5, 0, false, false, "a100aab4de75a28982cb49");
     }
 
+    public void testAllTypes() throws ParseException {
+        check("POINT EMPTY", -1, 0, 0, false, false, "1110");
+        check("POINT EMPTY", 0, 0, 0, false, false, "0110");
+        check("POINT EMPTY", 1, 0, 0, false, false, "2110");
+        check("POINT EMPTY", 5, 0, 0, false, false, "a110");
+        check("POINT(1 2)", -1, 0, 0, false, false, "11000000");
+        check("POINT(1 2)", 0, 0, 0, false, false, "01000204");
+        check("POINT(1 2)", 1, 0, 0, false, false, "21001428");
+        check("POINT(1 2)", 5, 0, 0, false, false, "a100c09a0c80b518");
+        check("LINESTRING EMPTY", -1, 0, 0, false, false, "1210");
+        check("LINESTRING EMPTY", 0, 0, 0, false, false, "0210");
+        check("LINESTRING EMPTY", 1, 0, 0, false, false, "2210");
+        check("LINESTRING EMPTY", 5, 0, 0, false, false, "a210");
+        check("LINESTRING(1 2, 3 4)", -1, 0, 0, false, false, "12000200000000");
+        check("LINESTRING(1 2, 3 4)", 0, 0, 0, false, false, "02000202040404");
+        check("LINESTRING(1 2, 3 4)", 1, 0, 0, false, false, "22000214282828");
+        check("LINESTRING(1 2, 3 4)", 5, 0, 0, false, false, "a20002c09a0c80b51880b51880b518");
+        check("POLYGON EMPTY", -1, 0, 0, false, false, "1310");
+        check("POLYGON EMPTY", 0, 0, 0, false, false, "0310");
+        check("POLYGON EMPTY", 1, 0, 0, false, false, "2310");
+        check("POLYGON EMPTY", 5, 0, 0, false, false, "a310");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2))", -1, 0, 0, false, false, "130001040000000002020101");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2))", 0, 0, 0, false, false, "030001040204040404040707");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2))", 1, 0, 0, false, false, "230001041428282828284f4f");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2))", 5, 0, 0, false, false, "a3000104c09a0c80b51880b51880b51880b51880b518ffe930ffe930");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12))", -1, 0, 0, false, false, "130002040000000002020101040202000002020101");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12))", 0, 0, 0, false, false, "030002040204040404040707041414040404040707");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12))", 1, 0, 0, false, false, "230002041428282828284f4f04c801c801282828284f4f");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12))", 5, 0, 0, false, false, "a3000204c09a0c80b51880b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe930");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22))", -1, 0, 0, false, false, "130003040000000002020101040202000002020101040202000002020101");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22))", 0, 0, 0, false, false, "030003040204040404040707041414040404040707041414040404040707");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22))", 1, 0, 0, false, false, "230003041428282828284f4f04c801c801282828284f4f04c801c801282828284f4f");
+        check("POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22))", 5, 0, 0, false, false, "a3000304c09a0c80b51880b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe930");
+        check("MULTIPOINT EMPTY", -1, 0, 0, false, false, "1410");
+        check("MULTIPOINT EMPTY", 0, 0, 0, false, false, "0410");
+        check("MULTIPOINT EMPTY", 1, 0, 0, false, false, "2410");
+        check("MULTIPOINT EMPTY", 5, 0, 0, false, false, "a410");
+        check("MULTIPOINT(1 2)", -1, 0, 0, false, false, "1400010000");
+        check("MULTIPOINT(1 2)", 0, 0, 0, false, false, "0400010204");
+        check("MULTIPOINT(1 2)", 1, 0, 0, false, false, "2400011428");
+        check("MULTIPOINT(1 2)", 5, 0, 0, false, false, "a40001c09a0c80b518");
+        check("MULTIPOINT(1 2, 3 4)", -1, 0, 0, false, false, "14000200000000");
+        check("MULTIPOINT(1 2, 3 4)", 0, 0, 0, false, false, "04000202040404");
+        check("MULTIPOINT(1 2, 3 4)", 1, 0, 0, false, false, "24000214282828");
+        check("MULTIPOINT(1 2, 3 4)", 5, 0, 0, false, false, "a40002c09a0c80b51880b51880b518");
+        check("MULTILINESTRING EMPTY", -1, 0, 0, false, false, "1510");
+        check("MULTILINESTRING EMPTY", 0, 0, 0, false, false, "0510");
+        check("MULTILINESTRING EMPTY", 1, 0, 0, false, false, "2510");
+        check("MULTILINESTRING EMPTY", 5, 0, 0, false, false, "a510");
+        check("MULTILINESTRING((1 2, 3 4))", -1, 0, 0, false, false, "1500010200000000");
+        check("MULTILINESTRING((1 2, 3 4))", 0, 0, 0, false, false, "0500010202040404");
+        check("MULTILINESTRING((1 2, 3 4))", 1, 0, 0, false, false, "2500010214282828");
+        check("MULTILINESTRING((1 2, 3 4))", 5, 0, 0, false, false, "a5000102c09a0c80b51880b51880b518");
+        check("MULTILINESTRING((1 2, 3 4), (5 6, 7 8))", -1, 0, 0, false, false, "15000202000000000202020000");
+        check("MULTILINESTRING((1 2, 3 4), (5 6, 7 8))", 0, 0, 0, false, false, "05000202020404040204040404");
+        check("MULTILINESTRING((1 2, 3 4), (5 6, 7 8))", 1, 0, 0, false, false, "25000202142828280228282828");
+        check("MULTILINESTRING((1 2, 3 4), (5 6, 7 8))", 5, 0, 0, false, false, "a5000202c09a0c80b51880b51880b5180280b51880b51880b51880b518");
+        check("MULTIPOLYGON EMPTY", -1, 0, 0, false, false, "1610");
+        check("MULTIPOLYGON EMPTY", 0, 0, 0, false, false, "0610");
+        check("MULTIPOLYGON EMPTY", 1, 0, 0, false, false, "2610");
+        check("MULTIPOLYGON EMPTY", 5, 0, 0, false, false, "a610");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)))", -1, 0, 0, false, false, "16000101040000000002020101");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)))", 0, 0, 0, false, false, "06000101040204040404040707");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)))", 1, 0, 0, false, false, "26000101041428282828284f4f");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)))", 5, 0, 0, false, false, "a600010104c09a0c80b51880b51880b51880b51880b518ffe930ffe930");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)), ((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", -1, 0, 0, false, false, "1600020104000000000202010103040000000002020101040202000002020101040202000002020101");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)), ((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 0, 0, 0, false, false, "0600020104020404040404070703040000040404040707041414040404040707041414040404040707");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)), ((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 1, 0, 0, false, false, "26000201041428282828284f4f03040000282828284f4f04c801c801282828284f4f04c801c801282828284f4f");
+        check("MULTIPOLYGON(((1 2, 3 4, 5 6, 1 2)), ((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 5, 0, 0, false, false, "a600020104c09a0c80b51880b51880b51880b51880b518ffe930ffe9300304000080b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe930");
+        check("GEOMETRYCOLLECTION EMPTY", -1, 0, 0, false, false, "1710");
+        check("GEOMETRYCOLLECTION EMPTY", 0, 0, 0, false, false, "0710");
+        check("GEOMETRYCOLLECTION EMPTY", 1, 0, 0, false, false, "2710");
+        check("GEOMETRYCOLLECTION EMPTY", 5, 0, 0, false, false, "a710");
+        check("GEOMETRYCOLLECTION(POINT(1 2))", -1, 0, 0, false, false, "17000111000000");
+        check("GEOMETRYCOLLECTION(POINT(1 2))", 0, 0, 0, false, false, "07000101000204");
+        check("GEOMETRYCOLLECTION(POINT(1 2))", 1, 0, 0, false, false, "27000121001428");
+        check("GEOMETRYCOLLECTION(POINT(1 2))", 5, 0, 0, false, false, "a70001a100c09a0c80b518");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4))", -1, 0, 0, false, false, "1700021100000012000200000000");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4))", 0, 0, 0, false, false, "0700020100020402000202040404");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4))", 1, 0, 0, false, false, "2700022100142822000214282828");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4))", 5, 0, 0, false, false, "a70002a100c09a0c80b518a20002c09a0c80b51880b51880b518");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4), POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", -1, 0, 0, false, false, "1700031100000012000200000000130003040000000002020101040202000002020101040202000002020101");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4), POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 0, 0, 0, false, false, "0700030100020402000202040404030003040204040404040707041414040404040707041414040404040707");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4), POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 1, 0, 0, false, false, "2700032100142822000214282828230003041428282828284f4f04c801c801282828284f4f04c801c801282828284f4f");
+        check("GEOMETRYCOLLECTION(POINT(1 2), LINESTRING(1 2, 3 4), POLYGON((1 2, 3 4, 5 6, 1 2), (11 12, 13 14, 15 16, 11 12), (21 22, 23 24, 25 26, 21 22)))", 5, 0, 0, false, false, "a70003a100c09a0c80b518a20002c09a0c80b51880b51880b518a3000304c09a0c80b51880b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe9300480897a80897a80b51880b51880b51880b518ffe930ffe930");
+
+    }
+
     public void testPointsZ() throws ParseException {
         check(reader3,"POINT Z(1 2 3)", -1, 0, 0, false, true, "110901000000000600000006");
         check(reader3,"POINT Z(1 2 3)", -1, 0, 0, false, false, "110801000006");
@@ -142,34 +230,34 @@ public class TWKBWriterTest extends TestCase {
         check(reader3,"POINT Z(1 2 3)", 5, 5, 0, false, false, "a10815c09a0c80b518c0cf24");
     }
 
-        public void testPointsZandM() throws ParseException {
-
-            check("POINT Z(1 2 3)", -1, 0, 0, false, false, "110801000006");
-            check("POINT Z(1 2 3)", -1, 1, 0, false, false, "11080500003c");
-            check("POINT Z(1 2 3)", -1, 5, 0, false, false, "1108150000c0cf24");
-            check("POINT Z(1 2 3)", 0, 0, 0, false, false, "010801020406");
-            check("POINT Z(1 2 3)", 0, 1, 0, false, false, "01080502043c");
-            check("POINT Z(1 2 3)", 0, 5, 0, false, false, "0108150204c0cf24");
-            check("POINT Z(1 2 3)", 1, 0, 0, false, false, "210801142806");
-            check("POINT Z(1 2 3)", 1, 1, 0, false, false, "21080514283c");
-            check("POINT Z(1 2 3)", 1, 5, 0, false, false, "2108151428c0cf24");
-            check("POINT Z(1 2 3)", 5, 0, 0, false, false, "a10801c09a0c80b51806");
-            check("POINT Z(1 2 3)", 5, 1, 0, false, false, "a10805c09a0c80b5183c");
-            check("POINT Z(1 2 3)", 5, 5, 0, false, false, "a10815c09a0c80b518c0cf24");
-
-            check("POINT M(1 2 3)", -1, 0, 0, false, false, "110802000006");
-            check("POINT M(1 2 3)", -1, 1, 0, false, false, "110806000006");
-            check("POINT M(1 2 3)", -1, 5, 0, false, false, "110816000006");
-            check("POINT M(1 2 3)", 0, 0, 0, false, false, "010802020406");
-            check("POINT M(1 2 3)", 0, 1, 0, false, false, "010806020406");
-            check("POINT M(1 2 3)", 0, 5, 0, false, false, "010816020406");
-            check("POINT M(1 2 3)", 1, 0, 0, false, false, "210802142806");
-            check("POINT M(1 2 3)", 1, 1, 0, false, false, "210806142806");
-            check("POINT M(1 2 3)", 1, 5, 0, false, false, "210816142806");
-            check("POINT M(1 2 3)", 5, 0, 0, false, false, "a10802c09a0c80b51806");
-            check("POINT M(1 2 3)", 5, 1, 0, false, false, "a10806c09a0c80b51806");
-            check("POINT M(1 2 3)", 5, 5, 0, false, false, "a10816c09a0c80b51806");
-        }
+    public void testPointsZandM() throws ParseException {
+/*
+            check(reader3,"POINT Z(1 2 3)", -1, 0, 0, false, false, "110801000006");
+            check(reader3,"POINT Z(1 2 3)", -1, 1, 0, false, false, "11080500003c");
+            check(reader3,"POINT Z(1 2 3)", -1, 5, 0, false, false, "1108150000c0cf24");
+            check(reader3,"POINT Z(1 2 3)", 0, 0, 0, false, false, "010801020406");
+            check(reader3,"POINT Z(1 2 3)", 0, 1, 0, false, false, "01080502043c");
+            check(reader3,"POINT Z(1 2 3)", 0, 5, 0, false, false, "0108150204c0cf24");
+            check(reader3,"POINT Z(1 2 3)", 1, 0, 0, false, false, "210801142806");
+            check(reader3,"POINT Z(1 2 3)", 1, 1, 0, false, false, "21080514283c");
+            check(reader3,"POINT Z(1 2 3)", 1, 5, 0, false, false, "2108151428c0cf24");
+            check(reader3,"POINT Z(1 2 3)", 5, 0, 0, false, false, "a10801c09a0c80b51806");
+            check(reader3,"POINT Z(1 2 3)", 5, 1, 0, false, false, "a10805c09a0c80b5183c");
+            check(reader3,"POINT Z(1 2 3)", 5, 5, 0, false, false, "a10815c09a0c80b518c0cf24");
+*/
+        check(reader3,"POINT M(1 2 3)", -1, 0, 0, false, false, "110802000006");
+        check(reader3,"POINT M(1 2 3)", -1, 1, 0, false, false, "110806000006");
+        check(reader3,"POINT M(1 2 3)", -1, 5, 0, false, false, "110816000006");
+        check(reader3,"POINT M(1 2 3)", 0, 0, 0, false, false, "010802020406");
+        check(reader3,"POINT M(1 2 3)", 0, 1, 0, false, false, "010806020406");
+        check(reader3,"POINT M(1 2 3)", 0, 5, 0, false, false, "010816020406");
+        check(reader3,"POINT M(1 2 3)", 1, 0, 0, false, false, "210802142806");
+        check(reader3,"POINT M(1 2 3)", 1, 1, 0, false, false, "210806142806");
+        check(reader3,"POINT M(1 2 3)", 1, 5, 0, false, false, "210816142806");
+        check(reader3,"POINT M(1 2 3)", 5, 0, 0, false, false, "a10802c09a0c80b51806");
+        check(reader3,"POINT M(1 2 3)", 5, 1, 0, false, false, "a10806c09a0c80b51806");
+        check(reader3,"POINT M(1 2 3)", 5, 5, 0, false, false, "a10816c09a0c80b51806");
+    }
 
     private CoordinateSequenceFactory factory = new PackedCoordinateSequenceFactory(PackedCoordinateSequenceFactory.DOUBLE, 2);
     private GeometryFactory geometryFactory = new GeometryFactory(factory);
