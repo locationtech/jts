34178b1277a0319f7127523b532b6be07c552c45
Jody Garnett <jody.garnett@gmail.com>
2018-08-07 09:49:32 -0700

    address typos and header feedback

--- modules/core/src/main/java/org/locationtech/jts/geom/Coordinates.java
+++ modules/core/src/main/java/org/locationtech/jts/geom/Coordinates.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 Vivid Solutions
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
