68cc804fc8cac499ae1143ecbdb1cd8c37302ae8
Jody Garnett <jody.garnett@gmail.com>
2018-08-19 19:25:04 -0700

    Version 1.16.0-RC

--- modules/core/src/main/java/org/locationtech/jts/JTSVersion.java
+++ modules/core/src/main/java/org/locationtech/jts/JTSVersion.java
@@ -46,7 +46,7 @@ public class JTSVersion {
   /**
    * An optional string providing further release info (such as "alpha 1");
    */
-  private static final String releaseInfo = "SNAPSHOT";
+  private static final String releaseInfo = "RC1";
 
   /**
    * Prints the current JTS version to stdout.
