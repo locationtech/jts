17e98e50be4eb9f4bc9e67ddd69073e7e0006169
Martin Davis <mtnclimb@gmail.com>
2018-10-17 15:13:11 -0700

    Improve TestBuilder segment labelling
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/testbuilder/ui/style/SegmentIndexStyle.java
+++ modules/app/src/main/java/org/locationtech/jtstest/testbuilder/ui/style/SegmentIndexStyle.java
@@ -30,9 +30,13 @@ public class SegmentIndexStyle
   private final static int VERTEX_OFFSET = 15;
   private final static int BOX_PAD = 1;
   private final static Font FONT = new Font(FontGlyphReader.FONT_SANSSERIF, Font.PLAIN, 10);
+  private static final int MIN_LABEL_DIST = 300;
   
   private Color color = Color.RED;
 
+  private double lastX = 0;
+  private double lastY = 0;
+  
   public SegmentIndexStyle(Color color) {
     this.color = color;
   }
@@ -41,6 +45,7 @@ public class SegmentIndexStyle
   throws Exception
   {
     double len = p0.distance(p1);
+    // don't try and label very short segments
     // can't compute label location for zero-length segments
     if (len < MIN_LEN) {
       return;
@@ -50,13 +55,11 @@ public class SegmentIndexStyle
     if (vertexOffset > VERTEX_OFFSET)
       vertexOffset = VERTEX_OFFSET;
     
-    String indexStr = Integer.toString(index);
-
     double dx = p1.getX() - p0.getX();
     double dy = p1.getY() - p0.getY();
-    
     int quadrant = Quadrant.quadrant(dx, -dy);
     
+    String indexStr = Integer.toString(index);
     double boxMaxX = /*2 * BOX_PAD +*/ indexStr.length() * CHAR_WIDTH_APPROX;
     double boxMaxY = 2 * BOX_PAD + CHAR_HEIGHT_APPROX;
     
@@ -80,9 +83,21 @@ public class SegmentIndexStyle
     double x = p0.getX() + dx/len*vertexOffset + strOffsetX;
     double y = p0.getY() + dy/len*vertexOffset + strOffsetY;
     
-    gr.setColor(color);
-    gr.setFont(FONT);
-    gr.drawString(indexStr, (int) x, (int) y);  
+    // cache last point drawn for simple label deconfliction
+    boolean doDraw = index == 0 || distToLast(x, y) > MIN_LABEL_DIST; 
+    
+    if (doDraw) {
+      gr.setColor(color);
+      gr.setFont(FONT);
+      gr.drawString(indexStr, (int) x, (int) y);
+      
+      lastX = x;
+      lastY = y;
+    }
+  }
+
+  private double distToLast(double x, double y) {
+    return (x - lastX)*(x - lastX) + (y - lastY)*(y - lastY);
   }
   
 
