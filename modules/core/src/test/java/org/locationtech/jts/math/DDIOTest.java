921f24fd9accbec18c1414525111c34b90c801ec
Kurt Schwehr <schwehr@google.com>
2018-11-06 16:15:11 -0800

    Fix method spelling: tesWritetRepeatedSqrt -> testWriteRepeatedSqrt
    Signed-off-by: Kurt Schwehr <schwehr@google.com>

--- modules/core/src/test/java/org/locationtech/jts/math/DDIOTest.java
+++ modules/core/src/test/java/org/locationtech/jts/math/DDIOTest.java
@@ -175,7 +175,7 @@ public class DDIOTest extends TestCase {
 		assertTrue(foundParseError);
 	}
 
-	public void tesWritetRepeatedSqrt()
+	public void testWriteRepeatedSqrt()
 	{
 		writeRepeatedSqrt(DD.valueOf(1.0));
 		writeRepeatedSqrt(DD.valueOf(.999999999999));
