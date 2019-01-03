502ad85bd9e3d3c292166ea7326eb4ffa11d5f3d
dr-jts <mtnclimb@gmail.com>
2019-01-03 14:29:05 -0800

    Add Variable Width Buffer functions in Lab
    Signed-off-by: dr-jts <mtnclimb@gmail.com>

---
+++ modules/lab/src/main/java/org/locationtech/jtslab/BufferFunctions.java
@@ -0,0 +1,17 @@
+package org.locationtech.jtslab;
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.operation.buffer.VariableWidthBuffer;
+import org.locationtech.jtstest.geomfunction.Metadata;
+
+
+public class BufferFunctions {
+  @Metadata(description="Buffer a line by a width varying along the line")
+  public static Geometry bufferVariableWidth(Geometry line,
+      @Metadata(title="Start width")
+      double startWidth,
+      @Metadata(title="End width")
+      double endWidth) {
+    return VariableWidthBuffer.buffer(line, startWidth, endWidth);
+  }
+}
