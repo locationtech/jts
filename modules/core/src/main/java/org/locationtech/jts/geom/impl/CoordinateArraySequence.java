534cbb15675be95854c3c6018ee63a1d0c05c533
Jody Garnett <jody.garnett@gmail.com>
2019-02-18 14:25:38 -0800

    Ask enforceArrayConsistency to copy provided array on modification

--- modules/core/src/main/java/org/locationtech/jts/geom/impl/CoordinateArraySequence.java
+++ modules/core/src/main/java/org/locationtech/jts/geom/impl/CoordinateArraySequence.java
@@ -12,6 +12,7 @@
 package org.locationtech.jts.geom.impl;
 
 import java.io.Serializable;
+import java.lang.reflect.Array;
 
 import org.locationtech.jts.geom.*;
 
@@ -82,13 +83,14 @@ public class CoordinateArraySequence
    */
   public CoordinateArraySequence(Coordinate[] coordinates, int dimension, int measures)
   {
-    this.coordinates = coordinates;
     this.dimension = dimension;
     this.measures = measures;
     if (coordinates == null) {
       this.coordinates = new Coordinate[0];
     }
-    enforceArrayConsistency( this.coordinates );
+    else {
+      this.coordinates = enforceArrayConsistency(coordinates);
+    }
   }
 
   /**
@@ -158,23 +160,42 @@ public class CoordinateArraySequence
 
   /**
    * Ensure array contents of the same type, making use of {@link #createCoordinate()} as needed.
+   * <p>
+   * A new array will be created if needed to return a consistent result.
+   * </p>
    * 
-   * @param array array is modified in place as needed
+   * @param array array containing consistent coordinate instances
    */
-  protected void enforceArrayConsistency(Coordinate[] array)
+  protected Coordinate[] enforceArrayConsistency(Coordinate[] array)
   {
      Coordinate sample = createCoordinate();
      Class<?> type = sample.getClass();
+     boolean isConsistent=true;
      for( int i = 0; i < array.length; i++) {
        Coordinate coordinate = array[i];
-       if( coordinate == null ) {
-         array[i] = createCoordinate();
+       if( coordinate != null && !coordinate.getClass().equals(type)) {
+         isConsistent = false;
+         break;
        }
-       else if(!coordinate.getClass().equals(type)) {
-         Coordinate duplicate = createCoordinate();
-         duplicate.setCoordinate(coordinate);
-         array[i] = duplicate;         
+     }
+     if( isConsistent ){
+       return array;
+     }
+     else {
+       Class<? extends Coordinate> coordinateType = sample.getClass();
+       Coordinate copy[] = (Coordinate[]) Array.newInstance(coordinateType, array.length);
+       for ( int i = 0; i < copy.length; i++){
+          Coordinate coordinate = array[i];
+          if( coordinate != null && !coordinate.getClass().equals(type)){
+            Coordinate duplicate = createCoordinate();
+            duplicate.setCoordinate(coordinate);
+            copy[i] = duplicate;
+          }
+          else {
+            copy[i] = coordinate;
+          }
        }
+       return copy;
      }
   }
 
