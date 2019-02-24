dd1e133e9990829fb5871a1a49ee6766389b03e9
Martin Davis <mtnclimb@gmail.com>
2019-02-23 22:00:20 -0800

    PackedCoordinateSequenceFactory Javadoc, renaming
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/geom/impl/PackedCoordinateSequenceFactory.java
+++ modules/core/src/main/java/org/locationtech/jts/geom/impl/PackedCoordinateSequenceFactory.java
@@ -19,22 +19,41 @@ import org.locationtech.jts.geom.CoordinateSequenceFactory;
 import org.locationtech.jts.geom.Coordinates;
 
 /**
- * Builds packed array coordinate sequences. The array data type can be either
- * double or float, and defaults to float.
+ * Builds packed array coordinate sequences. 
+ * The array data type can be either
+ * <code>double</code> or <code>float</code>, 
+ * and defaults to <code>double</code>.
  */
 public class PackedCoordinateSequenceFactory implements
     CoordinateSequenceFactory, Serializable
 {
   private static final long serialVersionUID = -3558264771905224525L;
   
+  /**
+   * Type code for arrays of type <code>double</code>.
+   */
   public static final int DOUBLE = 0;
+  
+  /**
+   * Type code for arrays of type <code>float</code>.
+   */
   public static final int FLOAT = 1;
 
+  /**
+   * A factory using array type {@link DOUBLE}
+   */
   public static final PackedCoordinateSequenceFactory DOUBLE_FACTORY =
       new PackedCoordinateSequenceFactory(DOUBLE);
   
+  /**
+   * A factory using array type {@link FLOAT}
+   */
   public static final PackedCoordinateSequenceFactory FLOAT_FACTORY =
       new PackedCoordinateSequenceFactory(FLOAT);
+
+  private static final int DEFAULT_MEASURES = 0;
+
+  private static final int DEFAULT_DIMENSION = 3;
 
   private int type = DOUBLE;
 
@@ -50,17 +69,19 @@ public class PackedCoordinateSequenceFactory implements
    * Creates a new PackedCoordinateSequenceFactory
    * of the given type.
    * Acceptable type values are
-   * {@linkplain PackedCoordinateSequenceFactory#Float}or
-   * {@linkplain PackedCoordinateSequenceFactory#Double}
+   * {@linkplain PackedCoordinateSequenceFactory#FLOAT}or
+   * {@linkplain PackedCoordinateSequenceFactory#DOUBLE}
    */
   public PackedCoordinateSequenceFactory(int type){
     this.type = type;
   }
 
   /**
-   * Returns the type of packed coordinate sequences this factory builds, either
-   * {@linkplain PackedCoordinateSequenceFactory#Float} or
-   * {@linkplain PackedCoordinateSequenceFactory#Double}
+   * Gets the type of packed coordinate sequence this factory builds, either
+   * {@linkplain PackedCoordinateSequenceFactory#FLOAT} or
+   * {@linkplain PackedCoordinateSequenceFactory#DOUBLE}
+   * 
+   * @return the type of packed array built
    */
   public int getType() {
     return type;
@@ -70,8 +91,8 @@ public class PackedCoordinateSequenceFactory implements
    * @see org.locationtech.jts.geom.CoordinateSequenceFactory#create(org.locationtech.jts.geom.Coordinate[])
    */
   public CoordinateSequence create(Coordinate[] coordinates) {
-    int dimension = 3;
-    int measures = 0;
+    int dimension = DEFAULT_DIMENSION;
+    int measures = DEFAULT_MEASURES;
     if (coordinates != null && coordinates.length > 0 && coordinates[0] != null) {
       Coordinate first = coordinates[0];
       dimension = Coordinates.dimension(first);
@@ -98,23 +119,28 @@ public class PackedCoordinateSequenceFactory implements
   }
 
   /**
-   * Create a packed coordinate sequence from the provided array. 
+   * Creates a packed coordinate sequence of type {@link #DOUBLE}
+   * from the provided array
+   * using the given coordinate dimension and a measure count of 0. 
    * 
-   * @param packedCoordinates 
-   * @param dimension
-   * @return Packaged coordinate seqeunce of the requested type
+   * @param packedCoordinates the array containing coordinate values
+   * @param dimension the coordinate dimension
+   * @param measures the coordinate measure count
+   * @return a packed coordinate sequence of type {@link #DOUBLE}
    */
   public CoordinateSequence create(double[] packedCoordinates, int dimension) {
-    return create( packedCoordinates, dimension, 0 );
+    return create( packedCoordinates, dimension, DEFAULT_MEASURES );
   }
   
   /**
-   * Create a packed coordinate sequence from the provided array. 
+   * Creates a packed coordinate sequence of type {@link #DOUBLE}
+   * from the provided array
+   * using the given coordinate dimension and measure count. 
    * 
-   * @param packedCoordinates 
-   * @param dimension
-   * @param measures
-   * @return Packaged coordinate seqeunce of the requested type
+   * @param packedCoordinates the array containing coordinate values
+   * @param dimension the coordinate dimension
+   * @param measures the coordinate measure count
+   * @return a packed coordinate sequence of type {@link #DOUBLE}
    */
   public CoordinateSequence create(double[] packedCoordinates, int dimension, int measures) {
     if (type == DOUBLE) {
@@ -124,21 +150,25 @@ public class PackedCoordinateSequenceFactory implements
     }
   }
   /**
-   * Create a packed coordinate sequence from the provided array. 
+   * Creates a packed coordinate sequence of type {@link #FLOAT}
+   * from the provided array. 
    * 
-   * @param packedCoordinates 
-   * @param dimension
-   * @return Packaged coordinate seqeunce of the requested type
+   * @param packedCoordinates the array containing coordinate values
+   * @param dimension the coordinate dimension
+   * @return a packed coordinate sequence of type {@link #FLOAT}
    */
   public CoordinateSequence create(float[] packedCoordinates, int dimension) {
     return create( packedCoordinates, dimension, 0 );
   }
   
   /**
-   * @param packedCoordinates
-   * @param dimension
-   * @param measures
-   * @return Packaged coordinate seqeunce of the requested type
+   * Creates a packed coordinate sequence of type {@link #FLOAT}
+   * from the provided array. 
+   * 
+   * @param packedCoordinates the array containing coordinate values
+   * @param dimension the coordinate dimension
+   * @param measures the coordinate measure count
+   * @return a packed coordinate sequence of type {@link #FLOAT}
    */
   public CoordinateSequence create(float[] packedCoordinates, int dimension, int measures) {
     if (type == DOUBLE) {
