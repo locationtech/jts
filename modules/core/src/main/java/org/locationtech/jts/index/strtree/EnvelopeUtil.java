3985aabadb18138df53f9ee23639c3bd3219fef5
Martin Davis <mtnclimb@gmail.com>
2019-03-04 11:03:56 -0800

    Add Envelope MinMaxDistance
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/index/strtree/EnvelopeUtil.java
+++ modules/core/src/main/java/org/locationtech/jts/index/strtree/EnvelopeUtil.java
@@ -21,7 +21,7 @@ public class EnvelopeUtil
    * @param env2 an envelope
    * @return the maximum distance between the points defining the envelopes
    */
-  static double maximumDistance(Envelope env1, Envelope env2)
+  public static double maximumDistance(Envelope env1, Envelope env2)
   {
     double minx = Math.min(env1.getMinX(), env2.getMinX());
     double miny = Math.min(env1.getMinY(), env2.getMinY());
@@ -34,5 +34,48 @@ public class EnvelopeUtil
     double dx = x2 - x1;
     double dy = y2 - y1;
     return Math.sqrt(dx * dx + dy * dy);    
+  }
+  
+  public static double minMaxDistance(Envelope a, Envelope b)
+  {
+    double aminx = a.getMinX();
+    double aminy = a.getMinY();
+    double amaxx = a.getMaxX();
+    double amaxy = a.getMaxY();
+    double bminx = b.getMinX();
+    double bminy = b.getMinY();
+    double bmaxx = b.getMaxX();
+    double bmaxy = b.getMaxY();
+    
+    double dist =         maxDistance(aminx, aminy, aminx, amaxy, bminx, bminy, bminx, bmaxy);
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, aminx, amaxy, bmaxx, bmaxy, bmaxx, bminy));
+  
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(aminx, aminy, amaxx, aminy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, aminx, amaxy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bminx, bminy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bminx, bminy, bmaxx, bminy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bmaxx, bmaxy, bminx, bmaxy));
+    dist = Math.min(dist, maxDistance(amaxx, amaxy, amaxx, aminy, bmaxx, bmaxy, bmaxx, bminy));
+    
+    return dist;
+}
+
+  private static double maxDistance(double ax1, double ay1, double ax2, double ay2, 
+      double bx1, double by1, double bx2, double by2) {
+    double dist = distance(ax1, ay1, bx1, by1);
+    dist = Math.max(dist, distance(ax1, ay1, bx2, by2));
+    dist = Math.max(dist, distance(ax2, ay2, bx1, by1));
+    dist = Math.max(dist, distance(ax2, ay2, bx2, by2));
+    return dist;
   }
 }
---
+++ modules/core/src/test/java/test/jts/perf/operation/distance/TestStressEnvelopeMinMaxDistance.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016 Martin Davis.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package test.jts.perf.operation.distance;
+
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.MultiPoint;
+import org.locationtech.jts.index.strtree.EnvelopeUtil;
+
+public class TestStressEnvelopeMinMaxDistance 
+{
+  static GeometryFactory factory = new GeometryFactory();
+  
+  public static void main(String[] args) {
+    TestStressEnvelopeMinMaxDistance test = new TestStressEnvelopeMinMaxDistance();
+    test.test();
+  }
+
+  boolean testFailed = false;
+  boolean verbose = true;
+
+  public TestStressEnvelopeMinMaxDistance() {
+  }
+
+  public void test()
+  {
+    int sizeX = 6;
+    int sizeY = 6;
+    
+    Coordinate[] pts = createPoints(sizeX, sizeY);
+    
+    MultiPoint[] boxes = createPointPairs(pts);
+    
+    run(boxes);
+  }
+
+  private void run(MultiPoint[] boxes) {
+    int n = boxes.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        run(boxes[i], boxes[j]);
+      }
+    }
+  }
+
+  private void run(MultiPoint a, MultiPoint b) {
+    double distance = a.distance(b);
+    double minMaxDistance = EnvelopeUtil.minMaxDistance(
+        a.getEnvelopeInternal(), b.getEnvelopeInternal());
+    
+    System.out.println("distance: " + distance
+        + "   minMaxDist: " + minMaxDistance);
+
+    if (distance > minMaxDistance) {
+      System.out.println("ERROR - distance: " + distance
+          + "   minMaxDist: " + minMaxDistance);
+    }
+  }
+
+  private MultiPoint[] createPointPairs(Coordinate[] pts) {
+    int npts = pts.length;
+    MultiPoint[] pairs = new MultiPoint[npts * npts];
+    
+    for (int i = 0; i < npts; i++) {
+      for (int j = 0; j < npts; j++) {
+        int index = i * npts + j;
+        MultiPoint pair = factory.createMultiPointFromCoords(new Coordinate[] { pts[i], pts[j] } );
+        pairs[index] = pair;
+      }
+    }
+    
+    return pairs;
+  }
+
+  private Coordinate[] createPoints(int sizeX, int sizeY) {
+    int npts = sizeX * sizeY;
+    Coordinate[] pts = new Coordinate[npts];
+    for (int x = 0; x < sizeX; x++) {
+      for (int y = 0; y < sizeY; y++) {
+        pts[x * sizeX + y] = new Coordinate(x, y);
+      }
+    }
+    return pts;
+  }
+
+  
+
+}
+  
+  
