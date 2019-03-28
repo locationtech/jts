b85ac7843cbc52f259d718c0fbe94e2f18cae106
Martin Davis <mtnclimb@gmail.com>
2019-03-28 10:38:41 -0700

    Uncomment the SegmentNode fix!
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/noding/SegmentNode.java
+++ modules/core/src/main/java/org/locationtech/jts/noding/SegmentNode.java
@@ -72,8 +72,8 @@ public class SegmentNode
 
     // an exterior node is the segment start point, so always sorts first
     // this guards against a robustness problem where the octants are not reliable
-    //if (! isInterior) return -1;
-    //if (! other.isInterior) return 1;
+    if (! isInterior) return -1;
+    if (! other.isInterior) return 1;
     
     return SegmentPointComparator.compare(segmentOctant, coord, other.coord);
     //return segment.compareNodePosition(this, other);
