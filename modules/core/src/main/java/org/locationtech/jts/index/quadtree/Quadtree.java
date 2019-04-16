51e326da1a0ecbaa2db667f37f2eaf377cb53b8d
Martin Davis <mtnclimb@gmail.com>
2019-04-15 22:22:50 -0700

    Fix Quadtree ensureextent method
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/index/quadtree/Quadtree.java
+++ modules/core/src/main/java/org/locationtech/jts/index/quadtree/Quadtree.java
@@ -75,11 +75,11 @@ public class Quadtree
     // pad one or both extents
     if (minx == maxx) {
       minx = minx - minExtent / 2.0;
-      maxx = minx + minExtent / 2.0;
+      maxx = maxx + minExtent / 2.0;
     }
     if (miny == maxy) {
       miny = miny - minExtent / 2.0;
-      maxy = miny + minExtent / 2.0;
+      maxy = maxy + minExtent / 2.0;
     }
     return new Envelope(minx, maxx, miny, maxy);
   }
