9c8e9730b28fb1aa6f695f1768af80464dcdab6e
Martin Davis <mtnclimb@gmail.com>
2019-07-10 12:01:38 -0700

    Javadoc
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/noding/ValidatingNoder.java
+++ modules/core/src/main/java/org/locationtech/jts/noding/ValidatingNoder.java
@@ -5,11 +5,23 @@ import java.util.Collection;
 import org.locationtech.jts.algorithm.LineIntersector;
 import org.locationtech.jts.algorithm.RobustLineIntersector;
 
+/**
+ * A wrapper for {@link Noder}s which validates
+ * the noding is correct.
+ * 
+ * @author mdavis
+ *
+ */
 public class ValidatingNoder implements Noder {
 
   private Noder noder;
   private Collection<SegmentString> nodedSS;
   
+  /**
+   * Creates a noding validator wrapping the given Noder
+   * 
+   * @param noder the Noder to validate
+   */
   public ValidatingNoder(Noder noder) {
     this.noder = noder;
   }
