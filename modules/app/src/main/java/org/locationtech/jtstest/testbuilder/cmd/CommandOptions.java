58c7901277ddb5cc1c57d9f98916ed8190046f65
Martin Davis <mtnclimb@gmail.com>
2019-07-18 11:28:33 -0700

    Add TestBuilderOpCmd
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

---
+++ modules/app/src/main/java/org/locationtech/jtstest/testbuilder/cmd/CommandOptions.java
@@ -0,0 +1,13 @@
+package org.locationtech.jtstest.testbuilder.cmd;
+
+public class CommandOptions {
+  public static final String GEOMFUNC = "geomfunc";
+  public static final String VERBOSE = "verbose";
+  
+  public static final String OP = "op";
+  public static final String GEOMAFILE = "afile";
+  public static final String GEOMBFILE = "bfile";
+  public static final String ARG1 = "arg1";
+  public static final String OUTPUT = "output";
+
+}
