bb561a088d1d9af4ec8a2e775a488a678f23e262
Martin Davis <mtnclimb@gmail.com>
2019-09-25 10:52:54 -0700

    Add some functions
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/function/OverlayNGTestFunctions.java
+++ modules/app/src/main/java/org/locationtech/jtstest/function/OverlayNGTestFunctions.java
@@ -39,6 +39,29 @@ public class OverlayNGTestFunctions {
     return ovr.getResultGeometry();
   }
 
+  public static Geometry edgesNodedIntersection(Geometry a, Geometry b, double scaleFactor) {
+    PrecisionModel pm = new PrecisionModel(scaleFactor);
+    // force non-null inputs
+    a = OverlayNGFunctions.sameOrEmpty(a, b);
+    b = OverlayNGFunctions.sameOrEmpty(b, a);
+    // op should not matter, since edges are captured pre-result
+    OverlayNG ovr = new OverlayNG(a, b, pm, OverlayOp.INTERSECTION);
+    ovr.setOutputNodedEdges(true);
+    return ovr.getResultGeometry();
+  }
+
+  public static Geometry edgesNodedIntNoOpt(Geometry a, Geometry b, double scaleFactor) {
+    PrecisionModel pm = new PrecisionModel(scaleFactor);
+    // force non-null inputs
+    a = OverlayNGFunctions.sameOrEmpty(a, b);
+    b = OverlayNGFunctions.sameOrEmpty(b, a);
+    // op should not matter, since edges are captured pre-result
+    OverlayNG ovr = new OverlayNG(a, b, pm, OverlayOp.INTERSECTION);
+    ovr.setOutputNodedEdges(true);
+    ovr.setOptimized(false);
+    return ovr.getResultGeometry();
+  }
+
   private static Geometry extractPoly(Geometry g) {
     if (g instanceof Polygon) return g;
     if (g instanceof MultiPolygon) return g;
