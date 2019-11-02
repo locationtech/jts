50e8d55a8d6e53b1db46c267dc28bba7b00dfb06
Martin Davis <mtnclimb@gmail.com>
2019-11-01 21:23:46 -0700

    Reorder method params
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/org/locationtech/jts/operation/overlayng/PrecisionReducerTest.java
+++ modules/core/src/test/java/org/locationtech/jts/operation/overlayng/PrecisionReducerTest.java
@@ -29,45 +29,45 @@ public class PrecisionReducerTest extends GeometryTestCase
 
   public void testPolygonGore( ) {
     checkReduce("POLYGON ((2 1, 9 1, 9 5, 3 5, 9 5.3, 9 9, 2 9, 2 1))",
-        "POLYGON ((9 1, 2 1, 2 9, 9 9, 9 5, 9 1))", 1);
+        1, "POLYGON ((9 1, 2 1, 2 9, 9 9, 9 5, 9 1))");
   }
 
   public void testPolygonGore2( ) {
     checkReduce("POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 5 5.1, 5 4.9, 9 4.9, 9 1))",
-        "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 9 1))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 9 1))");
   }
 
   public void testPolygonGoreToHole( ) {
     checkReduce("POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 5 5.9, 5 4.9, 9 4.9, 9 1))",
-        "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 9 1), (9 5, 5 6, 5 5, 9 5))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 5, 9 1), (9 5, 5 6, 5 5, 9 5))");
   }
 
   public void testPolygonSpike( ) {
     checkReduce("POLYGON ((1 1, 9 1, 5 1.4, 5 5, 1 5, 1 1))",
-        "POLYGON ((5 5, 5 1, 1 1, 1 5, 5 5))", 1);
+        1, "POLYGON ((5 5, 5 1, 1 1, 1 5, 5 5))");
   }
 
   public void testPolygonNarrowHole( ) {
     checkReduce("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (2 5, 8 5, 8 5.3, 2 5))",
-        "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 1))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 1))");
   }
 
   public void testPolygonWideHole( ) {
     checkReduce("POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9), (2 5, 8 5, 8 5.8, 2 5))",
-        "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 1), (8 5, 8 6, 2 5, 8 5))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 9, 9 9, 9 1), (8 5, 8 6, 2 5, 8 5))");
   }
 
   public void testMultiPolygonGap( ) {
     checkReduce("MULTIPOLYGON (((1 9, 9.1 9.1, 9 9, 9 4, 1 4.3, 1 9)), ((1 1, 1 4, 9 3.6, 9 1, 1 1)))",
-        "POLYGON ((9 1, 1 1, 1 4, 1 9, 9 9, 9 4, 9 1))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 4, 1 9, 9 9, 9 4, 9 1))");
   }
 
   public void testMultiPolygonGapToHole( ) {
     checkReduce("MULTIPOLYGON (((1 9, 9 9, 9.05 4.35, 6 4.35, 4 6, 2.6 4.25, 1 4, 1 9)), ((1 1, 1 4, 9 4, 9 1, 1 1)))",
-        "POLYGON ((9 1, 1 1, 1 4, 1 9, 9 9, 9 4, 9 1), (6 4, 4 6, 3 4, 6 4))", 1);
+        1, "POLYGON ((9 1, 1 1, 1 4, 1 9, 9 9, 9 4, 9 1), (6 4, 4 6, 3 4, 6 4))");
   }
 
-  private void checkReduce(String wkt, String wktExpected, double scaleFactor) {
+  private void checkReduce(String wkt, double scaleFactor, String wktExpected) {
     Geometry geom = read(wkt);
     Geometry expected = read(wktExpected);
     PrecisionModel pm = new PrecisionModel(scaleFactor);
