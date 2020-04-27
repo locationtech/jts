7216ac9c5206455e33704dcf1d6aff2634de7026
tink-expo <whhan8485@gmail.com>
2020-04-27 09:44:45 +0900

    Fixed farthestPoints() to handle case where extremalPts are acute triangle

--- modules/core/src/test/java/org/locationtech/jts/algorithm/MinimumBoundingCircleTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/MinimumBoundingCircleTest.java
@@ -82,6 +82,7 @@ public class MinimumBoundingCircleTest extends GeometryTestCase {
   public void testMinDiameterPolygon() {
     doMinDiameterTest("POLYGON ((100 200, 300 150, 110 100, 100 200))", "LINESTRING (300 150, 100 200)");
     doMinDiameterTest("POLYGON ((110 200, 300 150, 100 100, 110 200))", "LINESTRING (300 150, 100 100)");
+    doMinDiameterTest("POLYGON ((0 0, 6 0, 5 5, 0 0))", "LINESTRING (5 5, 0 0)");
   }
   
   static final double TOLERANCE = 1.0e-5;
