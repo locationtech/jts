dc32f995e7705a680eb144b6d566da2c0624d494
Martin Davis <mtnclimb@gmail.com>
2020-06-09 11:51:19 -0700

    Add OverlaySnapIfNeeded areatest function
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/function/OverlayNGSnapIfNeededFunctions.java
+++ modules/app/src/main/java/org/locationtech/jtstest/function/OverlayNGSnapIfNeededFunctions.java
@@ -14,39 +14,49 @@ package org.locationtech.jtstest.function;
 import static org.locationtech.jts.operation.overlayng.OverlayNG.DIFFERENCE;
 import static org.locationtech.jts.operation.overlayng.OverlayNG.INTERSECTION;
 import static org.locationtech.jts.operation.overlayng.OverlayNG.UNION;
+import static org.locationtech.jts.operation.overlayng.OverlayNG.SYMDIFFERENCE;
 
-import org.locationtech.jts.algorithm.LineIntersector;
-import org.locationtech.jts.algorithm.RobustLineIntersector;
 import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.PrecisionModel;
-import org.locationtech.jts.noding.IntersectionAdder;
-import org.locationtech.jts.noding.MCIndexNoder;
-import org.locationtech.jts.noding.Noder;
-import org.locationtech.jts.noding.ValidatingNoder;
-import org.locationtech.jts.operation.overlayng.OverlayNG;
 import org.locationtech.jts.operation.overlayng.OverlayNGSnapIfNeeded;
 import org.locationtech.jts.operation.union.UnaryUnionOp;
 import org.locationtech.jts.operation.union.UnionFunction;
 
 public class OverlayNGSnapIfNeededFunctions {
   
+  private static Geometry overlay(Geometry a, Geometry b, int opcode) {
+    return OverlayNGSnapIfNeeded.overlay(a, b, opcode );
+  }
+  
   public static Geometry difference(Geometry a, Geometry b) {
-    return OverlayNGSnapIfNeeded.overlay(a, b, DIFFERENCE );
+    return overlay(a, b, DIFFERENCE );
+  }
+
+  public static Geometry differenceBA(Geometry a, Geometry b) {
+    return overlay(b, a, DIFFERENCE );
   }
 
   public static Geometry intersection(Geometry a, Geometry b) {
-    return OverlayNGSnapIfNeeded.overlay(a, b, INTERSECTION );
+    areatest(a, b);
+    System.out.println(areaDelta(a, b));
+    return overlay(a, b, INTERSECTION );
   }
 
   public static Geometry union(Geometry a, Geometry b) {
-    return OverlayNGSnapIfNeeded.overlay(a, b, UNION );
+    areatest(a, b);
+    System.out.println(areaDelta(a, b));
+    return overlay(a, b, UNION );
   }
 
+  public static Geometry symDifference(Geometry a, Geometry b) {
+    System.out.println(areaDelta(a, b));
+    return overlay(a, b, SYMDIFFERENCE );
+  }
+  
   public static Geometry unaryUnion(Geometry a) {
     UnionFunction unionSRFun = new UnionFunction() {
 
       public Geometry union(Geometry g0, Geometry g1) {
-         return OverlayNGSnapIfNeeded.overlay(g0, g1, UNION );
+         return overlay(g0, g1, UNION );
       }
       
     };
@@ -55,4 +65,79 @@ public class OverlayNGSnapIfNeededFunctions {
     return op.union();
   }
   
+  public static double areatest(Geometry a, Geometry b) {
+    double areaDelta = areaDelta(a, b);
+    if (areaDelta > 1e-6) 
+      throw new RuntimeException("areatest delta too large: "+ areaDelta);
+    
+    return areaDelta;
+  }
+  
+  /**
+   * Computes the maximum area delta value
+   * resulting from identity equations over the overlay operations.
+   * The delta value is normalized to the total area of the geometries.
+   * If the overlay operations are computed correctly 
+   * the area delta is expected to be very small (e.g. < 1e-6).
+   *  
+   * @param a a geometry
+   * @param b a geometry
+   * @return the computed maximum area delta
+   */
+  public static double areaDelta(Geometry a, Geometry b) {
+    
+    double areaA = a.getArea();
+    double areaB = b.getArea();
+    
+    // if an input is non-polygonal delta is 0
+    if (areaA == 0 || areaB == 0)
+      return 0;
+    
+    double areaU   = overlay( a, b, UNION ).getArea();    
+    double areaI   = overlay( a, b, INTERSECTION ).getArea();
+    double areaDab = overlay( a, b, DIFFERENCE ).getArea();
+    double areaDba = overlay( b, a, DIFFERENCE ).getArea();
+    double areaSD  = overlay( a, b, SYMDIFFERENCE ).getArea();
+    
+    double maxDelta = 0;
+
+    // & : intersection
+    // - : difference
+    // + : union
+    // ^ : symdifference
+
+
+    // A = ( A & B ) + ( A - B )
+    double delta = Math.abs( areaA - areaI - areaDab );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    // B = ( A & B ) + ( B - A )
+    delta = Math.abs( areaB - areaI - areaDba );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A ^ B ) = ( A - B ) + ( B - A )
+    delta = Math.abs( areaDab + areaDba - areaSD );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A + B ) = ( A & B ) + ( A ^ B )
+    delta = Math.abs( areaI + areaSD - areaU );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A + B ) = ( A & B ) + ( A - B ) + ( A - B )
+    delta = Math.abs( areaU - areaI - areaDab - areaDba );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+    
+    // normalize the area delta value
+    return maxDelta / (areaA + areaB);
+  }
 }
