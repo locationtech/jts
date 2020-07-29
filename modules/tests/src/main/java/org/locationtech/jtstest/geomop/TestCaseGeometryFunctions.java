57b1100aa40fe8af4bdcfc43d686a64b7a660884
Martin Davis <mtnclimb@gmail.com>
2020-07-28 17:29:34 -0700

    Add TestRunner overlay robustness functions
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/tests/src/main/java/org/locationtech/jtstest/geomop/TestCaseGeometryFunctions.java
+++ modules/tests/src/main/java/org/locationtech/jtstest/geomop/TestCaseGeometryFunctions.java
@@ -11,6 +11,11 @@
  */
 package org.locationtech.jtstest.geomop;
 
+import static org.locationtech.jts.operation.overlayng.OverlayNG.DIFFERENCE;
+import static org.locationtech.jts.operation.overlayng.OverlayNG.INTERSECTION;
+import static org.locationtech.jts.operation.overlayng.OverlayNG.SYMDIFFERENCE;
+import static org.locationtech.jts.operation.overlayng.OverlayNG.UNION;
+
 import java.util.Collection;
 
 import org.locationtech.jts.densify.Densifier;
@@ -108,5 +113,86 @@ public class TestCaseGeometryFunctions
     return OverlayNG.overlay(geom0, geom1, OverlayNG.SYMDIFFERENCE, pm);
   }
 
+  public static double unionArea(Geometry geom) {
+    return geom.union().getArea();
+  }
+  
+  public static double unionLength(Geometry geom) {
+    return geom.union().getLength();
+  }
+  
+  public static boolean overlayAreaTest(Geometry a, Geometry b) {
+    double areaDelta = areaDelta(a, b);
+    return areaDelta < 1e-6; 
+  }
+  
+  /**
+   * Computes the maximum area delta value
+   * resulting from identity equations over the overlay operations.
+   * The delta value is normalized to the total area of the geometries.
+   * If the overlay operations are computed correctly 
+   * the area delta is expected to be very small (e.g. < 1e-6).
+   *  
+   * @param a a geometry
+   * @param b a geometry
+   * @return the computed maximum area delta
+   */
+  private static double areaDelta(Geometry a, Geometry b) {
+    
+    double areaA = a == null ? 0 : a.getArea();
+    double areaB = b == null ? 0 : b.getArea();
+    
+    // if an input is non-polygonal delta is 0
+    if (areaA == 0 || areaB == 0)
+      return 0;
+    
+    double areaU   = a.union( b ).getArea();    
+    double areaI   = a.intersection( b ).getArea();
+    double areaDab = a.difference( b ).getArea();
+    double areaDba = b.difference( a ).getArea();
+    double areaSD  = a.symDifference( b ).getArea();
+    
+    double maxDelta = 0;
+
+    // & : intersection
+    // - : difference
+    // + : union
+    // ^ : symdifference
+
+
+    // A = ( A & B ) + ( A - B )
+    double delta = Math.abs( areaA - areaI - areaDab );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    // B = ( A & B ) + ( B - A )
+    delta = Math.abs( areaB - areaI - areaDba );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A ^ B ) = ( A - B ) + ( B - A )
+    delta = Math.abs( areaDab + areaDba - areaSD );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A + B ) = ( A & B ) + ( A ^ B )
+    delta = Math.abs( areaI + areaSD - areaU );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+
+    //  ( A + B ) = ( A & B ) + ( A - B ) + ( A - B )
+    delta = Math.abs( areaU - areaI - areaDab - areaDba );
+    if (delta > maxDelta) {
+        maxDelta = delta;
+    }
+    
+    // normalize the area delta value
+    return maxDelta / (areaA + areaB);
+  }
+  
 }
 ;
