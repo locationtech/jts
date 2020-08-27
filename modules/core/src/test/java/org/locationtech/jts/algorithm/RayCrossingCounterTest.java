c398a542fa0f3efa3c5d61fc8d163595b6d07c6b
mukoki <m.michael.michaud@orange.fr>
2020-08-27 20:33:28 +0200

    unit test for bug #581

--- modules/core/src/test/java/org/locationtech/jts/algorithm/RayCrossingCounterTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/RayCrossingCounterTest.java
@@ -12,7 +12,10 @@
 package org.locationtech.jts.algorithm;
 
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateSequence;
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Location;
+import org.locationtech.jts.geom.impl.PackedCoordinateSequenceFactory;
 import org.locationtech.jts.io.WKTReader;
 
 import junit.textui.TestRunner;
@@ -28,15 +31,28 @@ public class RayCrossingCounterTest extends AbstractPointInRingTest {
 
   public static void main(String args[]) {
     TestRunner.run(RayCrossingCounterTest.class);
+    new RayCrossingCounterTest("RayCrossingCounterTest").testRunPtInRing4d();
   }
 
   public RayCrossingCounterTest(String name) { super(name); }
 
   protected void runPtInRing(int expectedLoc, Coordinate pt, String wkt)
-      throws Exception
+          throws Exception
   {
     Geometry geom = reader.read(wkt);
     assertEquals(expectedLoc, RayCrossingCounter.locatePointInRing(pt, geom.getCoordinates()));
+  }
+
+  public void testRunPtInRing4d()
+  {
+    CoordinateSequence cs = new PackedCoordinateSequenceFactory(PackedCoordinateSequenceFactory.DOUBLE)
+            .create(new double[]{
+                    0.0, 0.0, 0.0, 0.0,
+                    10.0, 0.0, 0.0, 0.0,
+                    5.0, 10.0, 0.0, 0.0,
+                    0.0, 0.0, 0.0, 0.0
+            }, 4, 1);
+    assertEquals(Location.INTERIOR, RayCrossingCounter.locatePointInRing(new Coordinate(5.0, 2.0), cs));
   }
 
 }
