bb820b848e6b9593ea10c8b328a4c580a49ee616
Martin Davis <mtnclimb@gmail.com>
2020-10-26 10:29:56 -0700

    Add PolygonOverlay overlaySR function
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/function/PolygonOverlayFunctions.java
+++ modules/app/src/main/java/org/locationtech/jtstest/function/PolygonOverlayFunctions.java
@@ -20,24 +20,34 @@ import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.geom.PrecisionModel;
 import org.locationtech.jts.geom.util.LinearComponentExtracter;
-import org.locationtech.jts.noding.snapround.GeometryNoder;
+import org.locationtech.jts.operation.overlayng.OverlayNG;
+import org.locationtech.jts.operation.overlayng.OverlayNGRobust;
 import org.locationtech.jts.operation.polygonize.Polygonizer;
 
 public class PolygonOverlayFunctions 
 {
 
-  public static Geometry overlaySnapRounded(Geometry g1, Geometry g2, double precisionTol)
+  public static Geometry overlaySR(Geometry g1, Geometry g2, double precisionTol)
   {
     PrecisionModel pm = new PrecisionModel(precisionTol);
+    return overlay(g1, g2, pm);
+  }
+  public static Geometry overlay(Geometry g1, Geometry g2)
+  {
+    return overlay(g1, g2, null);
+  }
+  
+  private static Geometry overlay(Geometry g1, Geometry g2, PrecisionModel pm)
+  {
     GeometryFactory geomFact = g1.getFactory();
 
     List lines = LinearComponentExtracter.getLines(g1);
     // add second input's linework, if any
     if (g2 != null)
       LinearComponentExtracter.getLines(g2, lines);
-    List nodedLinework = new GeometryNoder(pm).node(lines);
-    // union the noded linework to remove duplicates
-    Geometry nodedDedupedLinework = geomFact.buildGeometry(nodedLinework).union();
+    Geometry inputLines = g1.getFactory().buildGeometry(lines);
+    
+    Geometry nodedDedupedLinework = node(inputLines, pm);
 
     // polygonize the result
     Polygonizer polygonizer = new Polygonizer();
@@ -49,6 +59,13 @@ public class PolygonOverlayFunctions
     // convert to collection for return
     Polygon[] polyArray = GeometryFactory.toPolygonArray(polys);
     return geomFact.createGeometryCollection(polyArray);
+  }
+  
+  private static Geometry node(Geometry inputLines, PrecisionModel pm) {
+    if (pm == null) {
+      return OverlayNGRobust.overlay(inputLines, null, OverlayNG.UNION);
+    }
+    return OverlayNG.overlay(inputLines, null, OverlayNG.UNION, pm);
   }
 
 }
