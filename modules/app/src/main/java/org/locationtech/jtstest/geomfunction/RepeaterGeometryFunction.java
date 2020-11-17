e3ee769908382c3b73832367b101fee01c2782af
Martin Davis <mtnclimb@gmail.com>
2020-11-16 22:47:32 -0800

    Allow TestBuilder to repeat functions with no numeric arg
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/geomfunction/RepeaterGeometryFunction.java
+++ modules/app/src/main/java/org/locationtech/jtstest/geomfunction/RepeaterGeometryFunction.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2020 Martin Davis
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
 package org.locationtech.jtstest.geomfunction;
 
 import java.util.ArrayList;
@@ -9,14 +20,26 @@ import org.locationtech.jts.geom.util.GeometryMapper.MapOp;
 import org.locationtech.jtstest.function.FunctionsUtil;
 import org.locationtech.jtstest.util.ClassUtil;
 
+/**
+ * Repeats a function a given number of times.
+ * If the function has a single numeric argument, 
+ * the argument will be multiplied by the repeat counter for every call, 
+ * and the function results will be accumulated 
+ * into a collection to provide the final result.
+ * 
+ * @author Martin Davis
+ *
+ */
 public class RepeaterGeometryFunction implements GeometryFunction {
 
   private GeometryFunction fun;
   private int count;
+  private boolean hasRepeatableArg;
 
   public RepeaterGeometryFunction(GeometryFunction fun, int count) {
     this.fun = fun;
     this.count = count;
+    hasRepeatableArg = hasRepeatableArg(fun);
   }
   
   public String getCategory() {
@@ -67,7 +90,9 @@ public class RepeaterGeometryFunction implements GeometryFunction {
     
     //TODO: handle repeating methods with integer arg
     int repeatArgIndex = repeatableArgIndex(fun);
-    Double argStart = ClassUtil.toDouble(args[repeatArgIndex]);
+    double argStart = 0;
+    if (repeatArgIndex < args.length)
+      argStart = ClassUtil.toDouble(args[repeatArgIndex]);
     return invokeRepeated(geom, args, argStart);
   }
 
@@ -76,23 +101,28 @@ public class RepeaterGeometryFunction implements GeometryFunction {
     
     Class[] paramType = fun.getParameterTypes();
     int repeatArgIndex = repeatableArgIndex(fun);
-    // abort if no repeatable parameter
-    if (paramType.length < repeatArgIndex + 1) return false;
+    
+    // allow no repeatable arg
+    if (paramType.length < repeatArgIndex + 1) return true;
+    
     Class type = paramType[repeatArgIndex];
     if (! ClassUtil.isDouble(type)) return false;
-    
-    /*
-    Double argBase = ClassUtil.toDouble(args[0]);
-    if (argBase == null) return false;
-    */
     
     return true;
   }
 
+  public static boolean hasRepeatableArg(GeometryFunction fun) {
+    Class[] paramType = fun.getParameterTypes();
+    int numParam = paramType.length;
+    int numGeomParam = fun.isBinary() ? 1 : 0;
+    return numParam > numGeomParam;
+  }
+  
   public static int repeatableArgIndex(GeometryFunction fun) {
     if (fun.isBinary()) return 1;
     return 0;
   }
+  
   private Object invokeRepeated(Geometry geom, Object[] args, double argStart) {
     List results = new ArrayList();
     int repeatArgIndex = repeatableArgIndex(fun);
@@ -100,16 +130,20 @@ public class RepeaterGeometryFunction implements GeometryFunction {
       double val = argStart * i;
       Geometry result = (Geometry) fun.invoke(geom, copyArgs(args, repeatArgIndex, val));
       if (result == null) continue;
-      
-      FunctionsUtil.showIndicator(result);
-      results.add(result);
+      //System.out.println("Repeat: " + i);
+      if (hasRepeatableArg || i == 1) {
+        FunctionsUtil.showIndicator(result);
+        results.add(result);
+      }
     }
     return geom.getFactory().buildGeometry(results);
   }
 
   private Object[] copyArgs(Object[] args, int replaceIndex, double val) {
     Object[] newArgs = args.clone();
-    newArgs[replaceIndex] = val;
+    // only copy arg if there is a repeatable arg
+    if (newArgs.length > replaceIndex)
+      newArgs[replaceIndex] = val;
     return newArgs;
   }
 
