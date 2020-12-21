cd70d42e2ccfd53f91c9c6445891095666d83f65
Martin Davis <mtnclimb@gmail.com>
2020-12-21 13:39:34 -0800

    Fix buffer(0) to return largest area
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/main/java/org/locationtech/jts/operation/buffer/OffsetCurveSetBuilder.java
+++ modules/core/src/main/java/org/locationtech/jts/operation/buffer/OffsetCurveSetBuilder.java
@@ -241,8 +241,18 @@ public class OffsetCurveSetBuilder {
     
     int leftLoc  = cwLeftLoc;
     int rightLoc = cwRightLoc;
+    /*
+     * Use area-based orientation test, 
+     * to ensure that for invalid rings the largest enclosed area
+     * is used to determine orientation.
+     * This produces a more sensible result especially when
+     * used for validifying polygonal geometry via buffer-by-zero.
+     * For buffering use, the lower robustness of ccw-by-area
+     * doesn't matter, since very narrow or flat rings
+     * produce an acceptable offset curve for either orientation.
+     */
     if (coord.length >= LinearRing.MINIMUM_VALID_SIZE 
-        && Orientation.isCCW(coord)) {
+      && Orientation.isCCWArea(coord)) {
       leftLoc = cwRightLoc;
       rightLoc = cwLeftLoc;
       side = Position.opposite(side);
--- modules/core/src/test/java/org/locationtech/jts/algorithm/OrientationIsCCWTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/OrientationIsCCWTest.java
@@ -15,24 +15,21 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.CoordinateSequence;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.Polygon;
-import org.locationtech.jts.io.ParseException;
-import org.locationtech.jts.io.WKTReader;
 
-import junit.framework.TestCase;
 import junit.textui.TestRunner;
 import test.jts.GeometryTestCase;
 
 /**
- * Tests CGAlgorithms.isCCW
+ * Tests Orientation isCCW
  * @version 1.7
  */
-public class IsCCWTest extends GeometryTestCase {
+public class OrientationIsCCWTest extends GeometryTestCase {
 
   public static void main(String args[]) {
-    TestRunner.run(IsCCWTest.class);
+    TestRunner.run(OrientationIsCCWTest.class);
   }
 
-  public IsCCWTest(String name) { super(name); }
+  public OrientationIsCCWTest(String name) { super(name); }
 
   public void testTooFewPoints() {
     Coordinate[] pts = new Coordinate[] {
@@ -51,71 +48,83 @@ public class IsCCWTest extends GeometryTestCase {
   }
   
   public void testCCW() {
-    checkOrientationCCW(true, "POLYGON ((60 180, 140 120, 100 180, 140 240, 60 180))");
+    checkCCW(true, "POLYGON ((60 180, 140 120, 100 180, 140 240, 60 180))");
   }
   
   public void testRingCW() {
-    checkOrientationCCW(false, "POLYGON ((60 180, 140 240, 100 180, 140 120, 60 180))");
+    checkCCW(false, "POLYGON ((60 180, 140 240, 100 180, 140 120, 60 180))");
   }
   
   public void testCCWSmall() {
-    checkOrientationCCW(true, "POLYGON ((1 1, 9 1, 5 9, 1 1))");
+    checkCCW(true, "POLYGON ((1 1, 9 1, 5 9, 1 1))");
   }
   
   public void testDuplicateTopPoint() {
-    checkOrientationCCW(true, "POLYGON ((60 180, 140 120, 100 180, 140 240, 140 240, 60 180))");
+    checkCCW(true, "POLYGON ((60 180, 140 120, 100 180, 140 240, 140 240, 60 180))");
   }
   
   public void testFlatTopSegment() {
-    checkOrientationCCW(false, "POLYGON ((100 200, 200 200, 200 100, 100 100, 100 200))");
+    checkCCW(false, "POLYGON ((100 200, 200 200, 200 100, 100 100, 100 200))");
   }
   
   public void testFlatMultipleTopSegment() {
-    checkOrientationCCW(false, "POLYGON ((100 200, 127 200, 151 200, 173 200, 200 200, 100 100, 100 200))");
+    checkCCW(false, "POLYGON ((100 200, 127 200, 151 200, 173 200, 200 200, 100 100, 100 200))");
   }
   
   public void testDegenerateRingHorizontal() {
-    checkOrientationCCW(false, "POLYGON ((100 200, 100 200, 200 200, 100 200))");
+    checkCCW(false, "POLYGON ((100 200, 100 200, 200 200, 100 200))");
   }
   
   public void testDegenerateRingAngled() {
-    checkOrientationCCW(false, "POLYGON ((100 100, 100 100, 200 200, 100 100))");
+    checkCCW(false, "POLYGON ((100 100, 100 100, 200 200, 100 100))");
   }
   
   public void testDegenerateRingVertical() {
-    checkOrientationCCW(false, "POLYGON ((200 100, 200 100, 200 200, 200 100))");
+    checkCCW(false, "POLYGON ((200 100, 200 100, 200 200, 200 100))");
   }
   
   /**
    * This case is an invalid ring, so answer is a default value
    */
   public void testTopAngledSegmentCollapse() {
-    checkOrientationCCW(false, "POLYGON ((10 20, 61 20, 20 30, 50 60, 10 20))");
+    checkCCW(false, "POLYGON ((10 20, 61 20, 20 30, 50 60, 10 20))");
   }
   
   public void testABATopFlatSegmentCollapse() {
-    checkOrientationCCW(true, "POLYGON ((71 0, 40 40, 70 40, 40 40, 20 0, 71 0))");
+    checkCCW(true, "POLYGON ((71 0, 40 40, 70 40, 40 40, 20 0, 71 0))");
   }
   
   public void testABATopFlatSegmentCollapseMiddleStart() {
-    checkOrientationCCW(true, "POLYGON ((90 90, 50 90, 10 10, 90 10, 50 90, 90 90))");
+    checkCCW(true, "POLYGON ((90 90, 50 90, 10 10, 90 10, 50 90, 90 90))");
   }
   
   public void testMultipleTopFlatSegmentCollapseSinglePoint() {
-    checkOrientationCCW(true, "POLYGON ((100 100, 200 100, 150 200, 170 200, 200 200, 100 200, 150 200, 100 100))");
+    checkCCW(true, "POLYGON ((100 100, 200 100, 150 200, 170 200, 200 200, 100 200, 150 200, 100 100))");
   }
   
   public void testMultipleTopFlatSegmentCollapseFlatTop() {
-    checkOrientationCCW(true, "POLYGON ((10 10, 90 10, 70 70, 90 70, 10 70, 30 70, 50 70, 10 10))");
+    checkCCW(true, "POLYGON ((10 10, 90 10, 70 70, 90 70, 10 70, 30 70, 50 70, 10 10))");
   }
   
-  private void checkOrientationCCW(boolean expectedCCW, String wkt) {
+  /**
+   * Signed-area orientation returns orientation of largest enclosed area
+   */
+  public void testBowTieByArea() {
+    checkCCWArea(false, "POLYGON ((10 10, 50 10, 25 35, 35 35, 10 10))");
+  }
+  
+  private void checkCCW(boolean expectedCCW, String wkt) {
     Coordinate[] pts2x = getCoordinates(wkt);
     assertEquals("Coordinate array isCCW: ", expectedCCW, Orientation.isCCW(pts2x) );
     CoordinateSequence seq2x = getCoordinateSequence(wkt);
     assertEquals("CoordinateSequence isCCW: ", expectedCCW, Orientation.isCCW(seq2x) );
   }
 
+  private void checkCCWArea(boolean expectedCCW, String wkt) {
+    Coordinate[] pts = getCoordinates(wkt);
+    assertEquals("Coordinate array isCCW: ", expectedCCW, Orientation.isCCW(pts) );
+  }
+  
   private Coordinate[] getCoordinates(String wkt)
   {
     Geometry geom = read(wkt);
