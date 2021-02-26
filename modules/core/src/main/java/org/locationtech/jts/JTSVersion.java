b41cecd65a4e81eb3c97536f0f23cfc824bb97a7
Jim Hughes <jhughes@ccri.com>
2021-02-26 15:03:48 -0500

    Preparing JTSVersion.java and JTS_Version_History.md for 1.18.2-SNAPSHOT development.
    Signed-off-by: Jim Hughes <jhughes@ccri.com>

--- modules/core/src/main/java/org/locationtech/jts/JTSVersion.java
+++ modules/core/src/main/java/org/locationtech/jts/JTSVersion.java
@@ -41,12 +41,12 @@ public class JTSVersion {
   /**
    * The patch version number.
    */
-  public static final int PATCH = 1;
+  public static final int PATCH = 2;
 
   /**
    * An optional string providing further release info (such as "alpha 1");
    */
-  private static final String RELEASE_INFO = "";
+  private static final String RELEASE_INFO = "SNAPSHOT";
 
   /**
    * Prints the current JTS version to stdout.
