48800a4c32520bf91816598fa6a34c91384cd290
Martin Davis <mtnclimb@gmail.com>
2021-07-13 12:17:32 -0700

    Fix IsValidOp to return RING_SELF_INTERSECTION
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/org/locationtech/jts/operation/valid/IsValidTest.java
+++ modules/core/src/test/java/org/locationtech/jts/operation/valid/IsValidTest.java
@@ -50,111 +50,105 @@ public class IsValidTest extends GeometryTestCase {
     assertEquals(false, valid);
   }
 
-  public void testZeroAreaPolygon() throws Exception {
-    Geometry g = reader.read(
-          "POLYGON((0 0, 0 0, 0 0, 0 0, 0 0))");
-    g.isValid();
-    assertTrue(true); //No exception thrown [Jon Aquino]
+  public void testZeroAreaPolygon() {
+    checkInvalid( "POLYGON((0 0, 0 0, 0 0, 0 0, 0 0))");
   }
 
-  public void testValidSimplePolygon() throws Exception {
-    checkValid(
-          "POLYGON ((10 89, 90 89, 90 10, 10 10, 10 89))");
+  public void testValidSimplePolygon() {
+    checkValid( "POLYGON ((10 89, 90 89, 90 10, 10 10, 10 89))");
   }
 
-  public void testInvalidSimplePolygonRingSelfIntersection() throws Exception {
-    checkInvalid( TopologyValidationError.SELF_INTERSECTION,
+  public void testInvalidSimplePolygonRingSelfIntersection() {
+    checkInvalid( TopologyValidationError.RING_SELF_INTERSECTION,
         "POLYGON ((10 90, 90 10, 90 90, 10 10, 10 90))");
   }
 
-  public void testSimplePolygonHole() throws Exception {
+  public void testInvalidPolygonInverted() {
+    checkInvalid( TopologyValidationError.RING_SELF_INTERSECTION,
+        "POLYGON ((70 250, 40 500, 100 400, 70 250, 80 350, 60 350, 70 250))");
+  }
+
+  public void testSimplePolygonHole() {
     checkValid(
           "POLYGON ((10 90, 90 90, 90 10, 10 10, 10 90), (60 20, 20 70, 90 90, 60 20))");
   }
 
-  public void testPolygonTouchingHoleAtVertex() throws Exception {
+  public void testPolygonTouchingHoleAtVertex() {
     checkValid(
           "POLYGON ((240 260, 40 260, 40 80, 240 80, 240 260), (140 180, 40 260, 140 240, 140 180))");
   }
 
-  public void testPolygonMultipleHolesTouchAtSamePoint() throws Exception {
+  public void testPolygonMultipleHolesTouchAtSamePoint() {
     checkValid(
           "POLYGON ((10 90, 90 90, 90 10, 10 10, 10 90), (40 80, 60 80, 50 50, 40 80), (20 60, 20 40, 50 50, 20 60), (40 20, 60 20, 50 50, 40 20))");
   }
 
-  public void testPolygonHoleOutsideShellAllTouch() throws Exception {
+  public void testPolygonHoleOutsideShellAllTouch() {
     checkInvalid(TopologyValidationError.HOLE_OUTSIDE_SHELL,
           "POLYGON ((10 10, 30 10, 30 50, 70 50, 70 10, 90 10, 90 90, 10 90, 10 10), (50 50, 30 10, 70 10, 50 50))");
   }
 
-  public void testPolygonHoleOutsideShellDoubleTouch() throws Exception {
+  public void testPolygonHoleOutsideShellDoubleTouch() {
     checkInvalid(TopologyValidationError.HOLE_OUTSIDE_SHELL,
           "POLYGON ((10 90, 90 90, 90 10, 10 10, 10 90), (20 80, 80 80, 80 20, 20 20, 20 80), (90 70, 150 50, 90 20, 110 40, 90 70))");
   }
 
-  public void testPolygonNestedHolesAllTouch() throws Exception {
+  public void testPolygonNestedHolesAllTouch() {
     checkInvalid(TopologyValidationError.NESTED_HOLES,
           "POLYGON ((10 90, 90 90, 90 10, 10 10, 10 90), (20 80, 80 80, 80 20, 20 20, 20 80), (50 80, 80 50, 50 20, 20 50, 50 80))");
   }
 
-  public void testInvalidPolygonHoleProperIntersection() throws Exception {
+  public void testInvalidPolygonHoleProperIntersection() {
     checkInvalid( TopologyValidationError.SELF_INTERSECTION, 
         "POLYGON ((10 90, 50 50, 10 10, 10 90), (20 50, 60 70, 60 30, 20 50))");
   }
 
-  public void testInvalidPolygonDisconnectedInterior() throws Exception {
+  public void testInvalidPolygonDisconnectedInterior() {
     checkInvalid( TopologyValidationError.DISCONNECTED_INTERIOR,
           "POLYGON ((10 90, 90 90, 90 10, 10 10, 10 90), (20 80, 30 80, 20 20, 20 80), (80 30, 20 20, 80 20, 80 30), (80 80, 30 80, 80 30, 80 80))");
   }
 
-  public void testValidMultiPolygonTouchAtVertices() throws Exception {
+  public void testValidMultiPolygonTouchAtVertices() {
     checkValid(
           "MULTIPOLYGON (((10 10, 10 90, 90 90, 90 10, 80 80, 50 20, 20 80, 10 10)), ((90 10, 10 10, 50 20, 90 10)))");
   }
 
-  public void testInvalidMultiPolygonHoleOverlapCrossing() throws Exception {
+  public void testInvalidMultiPolygonHoleOverlapCrossing() {
     checkInvalid( TopologyValidationError.SELF_INTERSECTION,
         "MULTIPOLYGON (((20 380, 420 380, 420 20, 20 20, 20 380), (220 340, 180 240, 60 200, 140 100, 340 60, 300 240, 220 340)), ((60 200, 340 60, 220 340, 60 200)))");
   }
 
-  public void testValidMultiPolygonTouchAtVerticesSegments() throws Exception {
+  public void testValidMultiPolygonTouchAtVerticesSegments() {
     checkValid(
           "MULTIPOLYGON (((60 40, 90 10, 90 90, 10 90, 10 10, 40 40, 60 40)), ((50 40, 20 20, 80 20, 50 40)))");
   }
 
-  public void testInvalidMultiPolygonNestedAllTouchAtVertices() throws Exception {
+  public void testInvalidMultiPolygonNestedAllTouchAtVertices() {
     checkInvalid( TopologyValidationError.NESTED_SHELLS,
           "MULTIPOLYGON (((10 10, 20 30, 10 90, 90 90, 80 30, 90 10, 50 20, 10 10)), ((80 30, 20 30, 50 20, 80 30)))");
   }
 
-  public void testValidMultiPolygonHoleTouchVertices() throws Exception {
+  public void testValidMultiPolygonHoleTouchVertices()  {
     checkValid(
           "MULTIPOLYGON (((20 380, 420 380, 420 20, 20 20, 20 380), (220 340, 80 320, 60 200, 140 100, 340 60, 300 240, 220 340)), ((60 200, 340 60, 220 340, 60 200)))");
   }
   
   public void testLineString() {
-    Geometry g = read(
-          "LINESTRING(0 0, 0 0)");
-    g.isValid();
-    assertTrue(true); //No exception thrown [Jon Aquino]
+    checkInvalid( "LINESTRING(0 0, 0 0)");
   }
   
-  public void testLinearRingTriangle() throws Exception {
-    Geometry g = reader.read(
-          "LINEARRING (100 100, 150 200, 200 100, 100 100)");
-    assertTrue(g.isValid());
+  public void testLinearRingTriangle() {
+    checkValid( "LINEARRING (100 100, 150 200, 200 100, 100 100)");
   }
 
-  public void testLinearRingSelfCrossing() throws Exception {
-    Geometry g = reader.read(
+  public void testLinearRingSelfCrossing() {
+    checkInvalid( TopologyValidationError.RING_SELF_INTERSECTION,
           "LINEARRING (150 100, 300 300, 100 300, 350 100, 150 100)");
-    assertTrue(! g.isValid());
   }
 
-  public void testLinearRingSelfCrossing2() throws Exception {
-    Geometry g = reader.read(
+  public void testLinearRingSelfCrossing2() {
+    checkInvalid( TopologyValidationError.RING_SELF_INTERSECTION,
           "LINEARRING (0 0, 100 100, 100 0, 0 100, 0 0)");
-    assertTrue(! g.isValid());
   }
 
   //=============================================
