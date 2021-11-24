41362c73c372f1efa8d9d8246f6bab18d3cf7926
Martin Davis <mtnclimb@gmail.com>
2021-11-24 11:55:02 -0800

    Refactor TestBuilder AppIcons.load
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/app/src/main/java/org/locationtech/jtstest/testbuilder/AppCursors.java
+++ modules/app/src/main/java/org/locationtech/jtstest/testbuilder/AppCursors.java
@@ -19,18 +19,18 @@ import java.awt.Toolkit;
 public class AppCursors
 {
   public static Cursor DRAW_GEOM = Toolkit.getDefaultToolkit().createCustomCursor(
-      IconLoader.icon("DrawCursor.png").getImage(), new java.awt.Point(4, 26),
+      AppIcons.load("DrawCursor.png").getImage(), new java.awt.Point(4, 26),
       "Draw");
 
   public static Cursor EDIT_VERTEX = Toolkit.getDefaultToolkit().createCustomCursor(
-      IconLoader.icon("MoveVertexCursor.gif").getImage(),
+      AppIcons.load("MoveVertexCursor.gif").getImage(),
       new java.awt.Point(16, 16), "MoveVertex");
 
   public static Cursor HAND = Toolkit.getDefaultToolkit().createCustomCursor(
-      IconLoader.icon("Hand.gif").getImage(), new java.awt.Point(7, 7), "Pan");
+      AppIcons.load("Hand.gif").getImage(), new java.awt.Point(7, 7), "Pan");
 
   public static Cursor ZOOM = Toolkit.getDefaultToolkit().createCustomCursor(
-      IconLoader.icon("MagnifyCursor.gif").getImage(),
+      AppIcons.load("MagnifyCursor.gif").getImage(),
       new java.awt.Point(16, 16), "Zoom In");
 
 }
