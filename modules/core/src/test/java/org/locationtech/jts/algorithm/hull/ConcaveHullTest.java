051e6a1cca930c8b98f9c0e663e573dcd164e34f
Martin Davis <mtnclimb@gmail.com>
2021-12-20 13:56:35 -0800

    Add Area tests
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/org/locationtech/jts/algorithm/hull/ConcaveHullTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/hull/ConcaveHullTest.java
@@ -29,9 +29,14 @@ public class ConcaveHullTest extends GeometryTestCase {
        70, "POLYGON ((30 70, 70 70, 90 10, 50 60, 10 10, 30 70))" );
   }
 
-  public void testLengthZeroConnected() {
+  public void testLengthZero() {
     checkHullByLength("MULTIPOINT ((10 10), (90 10), (70 70), (50 60), (50 90), (40 70), (30 30))", 
        0, "POLYGON ((10 10, 40 70, 50 90, 70 70, 90 10, 50 60, 30 30, 10 10))" );
+  }
+
+  public void testLengthConvex() {
+    checkHullByLength("MULTIPOINT ((10 10), (90 10), (70 70), (50 60), (50 90), (40 70), (30 30))", 
+       100, "POLYGON ((10 10, 40 70, 50 90, 70 70, 90 10, 10 10))" );
   }
 
   public void testLengthCShape() {
@@ -39,9 +44,35 @@ public class ConcaveHullTest extends GeometryTestCase {
        50, "POLYGON ((30 70, 50 80, 80 90, 90 70, 70 80, 40 50, 40 30, 90 20, 50 10, 30 20, 20 40, 30 70))" );
   }
 
+  //------------------------------------------------
+  
+  public void testAreaSimple() {
+    checkHullByArea("MULTIPOINT ((10 10), (90 10), (30 70), (70 70), (50 60))", 
+       .5, "POLYGON ((30 70, 70 70, 90 10, 50 60, 10 10, 30 70))" );
+  }
+
+  public void testAreaZero() {
+    checkHullByArea("MULTIPOINT ((10 10), (90 10), (70 70), (50 60), (50 90), (40 70), (30 30))", 
+       0, "POLYGON ((10 10, 40 70, 50 90, 70 70, 90 10, 50 60, 30 30, 10 10))" );
+  }
+
+  public void testAreaConvex() {
+    checkHullByArea("MULTIPOINT ((10 10), (90 10), (70 70), (50 60), (50 90), (40 70), (30 30))", 
+       1, "POLYGON ((10 10, 40 70, 50 90, 70 70, 90 10, 10 10))" );
+  }
+
+  //==========================================================================
+  
   private void checkHullByLength(String wkt, double threshold, String wktExpected) {
     Geometry geom = read(wkt);
     Geometry actual = ConcaveHull.concaveHullByLength(geom, threshold);
+    Geometry expected = read(wktExpected);
+    checkEqual(expected, actual);
+  }
+  
+  private void checkHullByArea(String wkt, double threshold, String wktExpected) {
+    Geometry geom = read(wkt);
+    Geometry actual = ConcaveHull.concaveHullByArea(geom, threshold);
     Geometry expected = read(wktExpected);
     checkEqual(expected, actual);
   }
