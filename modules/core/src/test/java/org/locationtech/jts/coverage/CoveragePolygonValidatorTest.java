5ece01a26cfa9843846fa658b9ba3c6a18795438
Martin Davis <mtnclimb@gmail.com>
2022-07-01 23:00:52 -0700

    Add CoveragePolygon tests
    Signed-off-by: Martin Davis <mtnclimb@gmail.com>

--- modules/core/src/test/java/org/locationtech/jts/coverage/CoveragePolygonValidatorTest.java
+++ modules/core/src/test/java/org/locationtech/jts/coverage/CoveragePolygonValidatorTest.java
@@ -26,25 +26,54 @@ public class CoveragePolygonValidatorTest extends GeometryTestCase {
     super(name);
   }
   
-  public void testSingleEdgeAligned() {
-    checkResult("POLYGON ((100 200, 200 200, 200 100, 100 100, 100 200))",
+  //========  Invalid cases   =============================
+
+  public void testCollinearUnmatchedEdge() {
+    checkInvalid("POLYGON ((100 200, 200 200, 200 100, 100 100, 100 200))",
         "POLYGON ((100 300, 180 300, 180 200, 100 200, 100 300))",
         "LINESTRING (100 200, 200 200)");
   }
 
-  public void testExactDuplicate() {
-    checkResult("POLYGON ((1 3, 3 3, 3 1, 1 1, 1 3))",
+  public void testDuplicateGeometry() {
+    checkInvalid("POLYGON ((1 3, 3 3, 3 1, 1 1, 1 3))",
         "MULTIPOLYGON (((1 3, 3 3, 3 1, 1 1, 1 3)), ((5 3, 5 1, 3 1, 3 3, 5 3)))",
         "LINEARRING (1 3, 1 1, 3 1, 3 3, 1 3)");
   }
 
-  public void testCrossingSegments() {
-    checkResult("POLYGON ((1 9, 9 9, 9 3, 1 3, 1 9))",
+  public void testCrossingSegment() {
+    checkInvalid("POLYGON ((1 9, 9 9, 9 3, 1 3, 1 9))",
         "POLYGON ((1 1, 5 6, 9 1, 1 1))",
         "LINESTRING (1 3, 9 3)");
   }
 
-  private void checkResult(String wktTarget, String wktAdj, String wktExpected) {
+  public void testCrossingAndInteriorSegments() {
+    checkInvalid("POLYGON ((1 1, 3 4, 7 4, 9 1, 1 1))",
+        "POLYGON ((1 9, 9 9, 9 3, 1 3, 1 9))",
+        "LINESTRING (1 1, 3 4, 7 4, 9 1)");
+  }
+
+  //========  Valid cases   =============================
+  
+  public void testMatchedEdges() {
+    checkValid("POLYGON ((3 7, 7 7, 7 3, 3 3, 3 7))",
+        "MULTIPOLYGON (((1 7, 3 7, 3 3, 1 3, 1 7)), ((3 9, 7 9, 7 7, 3 7, 3 9)), ((9 7, 9 3, 7 3, 7 7, 9 7)), ((3 1, 3 3, 7 3, 7 1, 3 1)))");
+  }
+
+  //-- confirms zero-length segments are skipped in processing
+  public void testRepeatedCommonVertexInTarget() {
+    checkValid("POLYGON ((1 1, 1 3, 5 3, 5 3, 9 1, 1 1))",
+        "POLYGON ((1 9, 9 9, 9 5, 5 3, 1 3, 1 9))");
+  }
+
+  //-- confirms zero-length segments are skipped in processing
+  public void testRepeatedCommonVertexInAdjacent() {
+    checkValid("POLYGON ((1 1, 1 3, 5 3, 9 1, 1 1))",
+        "POLYGON ((1 9, 9 9, 9 5, 5 3, 5 3, 1 3, 1 9))");
+  }
+
+  //----------------------------------------------------------------------
+  
+  private void checkInvalid(String wktTarget, String wktAdj, String wktExpected) {
     checkResult(wktTarget, wktAdj, 0, wktExpected);
   }
   
@@ -54,6 +83,14 @@ public class CoveragePolygonValidatorTest extends GeometryTestCase {
     Geometry actual = CoveragePolygonValidator.validate(target, adj, tolerance);
     //System.out.println(actual);
     Geometry expected = read(wktExpected);
+    checkEqual(expected, actual);
+  }
+  
+  private void checkValid(String wktTarget, String wktAdj) {
+    Geometry target = read(wktTarget);
+    Geometry adj = read(wktAdj);
+    Geometry actual = CoveragePolygonValidator.validate(target, adj);
+    Geometry expected = read("LINESTRING EMPTY");    //TODO: check equals LINESTRING EMPTY
     checkEqual(expected, actual);
   }
 }
