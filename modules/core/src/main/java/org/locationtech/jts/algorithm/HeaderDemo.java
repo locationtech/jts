56069b279121dc026a576a68e004edbc3b8eca35
Felix Obermaier <felix.obermaier@netcologne.de>
2022-12-09 13:42:11 +0100

    Prove sample_java_header.txt not conform
    DO NOT MERGE, fix sample_java_header.txt

---
+++ modules/core/src/main/java/org/locationtech/jts/algorithm/HeaderDemo.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2016 Felix Obermaier.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
+
+package org.locationtech.jts.algorithm;
+
+/**
+ * Class with header from <i>docs/sample_java_header.txt</i>
+ */
+public class HeaderDemo {
+
+  /**
+   * Does nothing but return {@code true}.
+   * @return {@code true}
+   */
+  public boolean compute() {
+    return true;
+  }
+}
