15ef53578a99b3f7a165e44ecedbd8b74b5fcb84
Martin Davis <mtnclimb@gmail.com>
2023-05-23 17:55:59 -0700

    Fix imports

--- modules/core/src/main/java/org/locationtech/jts/coverage/CoverageEdge.java
+++ modules/core/src/main/java/org/locationtech/jts/coverage/CoverageEdge.java
@@ -17,7 +17,6 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LineSegment;
 import org.locationtech.jts.geom.LineString;
-import org.locationtech.jts.geom.LinearRing;
 import org.locationtech.jts.geom.MultiLineString;
 import org.locationtech.jts.io.WKTWriter;
 
