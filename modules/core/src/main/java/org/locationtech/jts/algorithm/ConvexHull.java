e4f8510ae972a4cc8d6b1870fab96b8ea2bab85f
Martin Davis <mtnclimb@gmail.com>
2023-06-20 18:03:01 -0700

    Add List import

--- modules/core/src/main/java/org/locationtech/jts/algorithm/ConvexHull.java
+++ modules/core/src/main/java/org/locationtech/jts/algorithm/ConvexHull.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
