7ce18de357388aa275f159779f1776623adb96f3
Martin Davis <mtnclimb@gmail.com>
2024-09-26 15:11:09 -0700

    Fix imports

--- modules/core/src/test/java/org/locationtech/jts/algorithm/ConvexHullTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/ConvexHullTest.java
@@ -19,7 +19,6 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.PrecisionModel;
-import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 
 import junit.framework.Test;
