4a557a127771ca96341438a51dd82a3c7acdbf24
Michael Carleton <micycle1@hotmail.co.uk>
2025-05-18 16:59:21 +0100

    KdPerfTest - time using the framework

--- modules/core/src/test/java/test/jts/perf/index/KdtreePerfTest.java
+++ modules/core/src/test/java/test/jts/perf/index/KdtreePerfTest.java
@@ -1,11 +1,9 @@
 package test.jts.perf.index;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.index.kdtree.KdNode;
@@ -16,7 +14,12 @@ import test.jts.perf.PerformanceTestRunner;
 
 public class KdtreePerfTest extends PerformanceTestCase {
 
-	public static void main(String args[]) {
+	private List<Coordinate> points;
+	private KdTree tree;
+	private Coordinate query;
+	private int k;
+
+	public static void main(String[] args) {
 		PerformanceTestRunner.run(KdtreePerfTest.class);
 	}
 
@@ -27,63 +30,42 @@ public class KdtreePerfTest extends PerformanceTestCase {
 	}
 
 	@Override
-	public void setUp() throws Exception {
-		System.out.println("KDtree nearest neighbors query perf test");
-	}
-
-	@Override
-	public void startRun(int size) {
-		testNearestNeighborsPerformance(size);
-	}
-
-	private void testNearestNeighborsPerformance(int n) {
-		int k = n / 100;
-		KdTree tree = new KdTree();
-		Random rand = new Random();
-
-		List<Coordinate> points = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			double x = rand.nextDouble();
-			double y = rand.nextDouble();
-			points.add(new Coordinate(x, y));
+	public void startRun(int size) throws Exception {
+		Random rnd = new Random(12345);
+		points = new ArrayList<>(size);
+		for (int i = 0; i < size; i++) {
+			points.add(new Coordinate(rnd.nextDouble(), rnd.nextDouble()));
 		}
+		tree = new KdTree(); // empty tree
+		query = new Coordinate(rnd.nextDouble(), rnd.nextDouble());
+		k = Math.max(1, size / 100);
+	}
 
-		long startTime = System.nanoTime();
-		for (Coordinate coordinate : points) {
-			tree.insert(coordinate);
+	/**
+	 * Insert all points into the tree. Framework will time this method.
+	 */
+	public void runInsertPoints() {
+		for (Coordinate pt : points) {
+			tree.insert(pt);
 		}
-		long insertTime = System.nanoTime() - startTime;
-		System.out.println("Kdtree: Time to insert " + n + " points: " + (insertTime / 1_000_000) + " ms");
-
-		Coordinate query = new Coordinate(rand.nextDouble(), rand.nextDouble());
-
-		// Time k-NN query using k-d tree
-		startTime = System.nanoTime();
-		List<KdNode> nearest = tree.nearestNeighbors(query, k);
-		long knnTime = System.nanoTime() - startTime;
-		System.out.println("Kdtree: Time to find " + k + " nearest neighbors: " + (knnTime / 1_000_000) + " ms");
-
-		// Time k-NN query using brute-force
-		startTime = System.nanoTime();
-		List<Coordinate> bruteForceNearest = bruteForceNearestNeighbors(tree, query, k);
-		long bruteForceTime = System.nanoTime() - startTime;
-		System.out.println("brute-force: time to find " + k + " nearest neighbors: " + (bruteForceTime / 1_000_000) + " ms");
-
-		System.out.println("");
 	}
 
-	private List<Coordinate> bruteForceNearestNeighbors(KdTree tree, Coordinate query, int k) {
-		List<Coordinate> allPoints = getAllPoints(tree);
-
-		// Sort all points by distance to the query point
-		allPoints.sort(Comparator.comparingDouble(point -> query.distance(point)));
-
-		// Return the first k points (ordered closest first)
-		return allPoints.subList(0, Math.min(k, allPoints.size()));
+	/**
+	 * Do a single k-NN query using the kd-tree. Framework will time this method.
+	 */
+	public void runKdTreeNearest() {
+		@SuppressWarnings("unused")
+		List<KdNode> result = tree.nearestNeighbors(query, k);
 	}
 
-	private List<Coordinate> getAllPoints(KdTree tree) {
-		return Arrays.stream(KdTree.toCoordinates(tree.getNodes())).collect(Collectors.toList());
+	/**
+	 * Do a single k-NN query by brute-force. Framework will time this method.
+	 */
+	public void runBruteForceNearest() {
+		// make a copy of the points list
+		List<Coordinate> copy = new ArrayList<>(points);
+		copy.sort(Comparator.comparingDouble(query::distance));
+		@SuppressWarnings("unused")
+		List<Coordinate> nearest = copy.subList(0, Math.min(k, copy.size()));
 	}
-
 }
