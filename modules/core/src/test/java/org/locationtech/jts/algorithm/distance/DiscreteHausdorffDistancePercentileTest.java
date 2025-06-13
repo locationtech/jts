8eb8381486fbebe2b6c317e4762dd20d5667e95c
Aleksander Rubelek <aleksander.rubelek@here.com>
2025-06-13 15:12:52 +0200

    Header correction
    Signed-off-by: Aleksander Rubelek <aleksander.rubelek@here.com>

--- modules/core/src/test/java/org/locationtech/jts/algorithm/distance/DiscreteHausdorffDistancePercentileTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/distance/DiscreteHausdorffDistancePercentileTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 Vivid Solutions.
  *
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
  * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
  * and the Eclipse Distribution License is available at
