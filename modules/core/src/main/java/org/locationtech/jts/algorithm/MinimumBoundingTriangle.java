f4f0eab7f40794378f80f47fa5d8c867f83364d4
Michael Carleton <micycle1@hotmail.co.uk>
2025-10-31 20:07:25 +0000

    MBT - add license

--- modules/core/src/main/java/org/locationtech/jts/algorithm/MinimumBoundingTriangle.java
+++ modules/core/src/main/java/org/locationtech/jts/algorithm/MinimumBoundingTriangle.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025 Michael Carleton
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
 package org.locationtech.jts.algorithm;
 
 import static java.lang.Math.floorMod;
--- modules/core/src/test/java/org/locationtech/jts/algorithm/MinimumBoundingTriangleTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/MinimumBoundingTriangleTest.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2025 Michael Carleton
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * and Eclipse Distribution License v. 1.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v20.html
+ * and the Eclipse Distribution License is available at
+ *
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ */
 package org.locationtech.jts.algorithm;
 
 import org.locationtech.jts.geom.Coordinate;
@@ -5,7 +16,6 @@ import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Polygon;
 import org.locationtech.jts.geom.PrecisionModel;
-import org.locationtech.jts.io.WKTReader;
 
 import test.jts.GeometryTestCase;
 
@@ -15,7 +25,6 @@ public class MinimumBoundingTriangleTest extends GeometryTestCase {
 
 	private final PrecisionModel precisionModel = new PrecisionModel(1);
 	private final GeometryFactory geometryFactory = new GeometryFactory(precisionModel, 0);
-	private final WKTReader reader = new WKTReader(geometryFactory);
 
 	public static void main(String[] args) {
 		junit.textui.TestRunner.run(MinimumBoundingTriangleTest.class);
