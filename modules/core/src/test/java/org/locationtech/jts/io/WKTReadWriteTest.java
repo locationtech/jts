45744222b0b8a861c44dbc2c03dcf4f1e85c27fd
Martin Davis <mtnclimb@gmail.com>
2025-12-04 17:19:09 -0800

    Refactor WKTReadWriteTest code

--- modules/core/src/test/java/org/locationtech/jts/io/WKTReadWriteTest.java
+++ modules/core/src/test/java/org/locationtech/jts/io/WKTReadWriteTest.java
@@ -32,70 +32,83 @@ public class WKTReadWriteTest extends TestCase {
 
   public void testReadNaN() throws Exception {
 
+    checkReadWrite("POINT (NaN NaN)");
+    
     final String pt = "POINT (10 10)";
-    assertEquals(pt, writer.write(reader.read("POINT (10 10 NaN)")));
-    assertEquals(pt, writer.write(reader.read("POINT (10 10 nan)")));
-    assertEquals(pt, writer.write(reader.read("POINT (10 10 NAN)")));
+    assertEquals(pt, readWrite("POINT (10 10 NaN)"));
+    assertEquals(pt, readWrite("POINT (10 10 nan)"));
+    assertEquals(pt, readWrite("POINT (10 10 NAN)"));
   }
 
-  public void testReadInf() throws Exception {
-    final String pt = "POINT (Inf -Inf)";
-    assertEquals(pt, writer.write(reader.read("POINT (Inf -Inf)")));
+  public void testReadInf() {
+    checkReadWrite("POINT (Inf -Inf)");
   }
 
-  public void testReadPoint() throws Exception {
-    assertEquals("POINT (10 10)", writer.write(reader.read("POINT (10 10)")));
-    assertEquals("POINT EMPTY", writer.write(reader.read("POINT EMPTY")));
+  public void testReadPoint() {
+    checkReadWrite("POINT (10 10)");
+    checkReadWrite("POINT EMPTY");
   }
 
-  public void testReadLineString() throws Exception {
-    assertEquals("LINESTRING (10 10, 20 20, 30 40)", writer.write(reader.read("LINESTRING (10 10, 20 20, 30 40)")));
-    assertEquals("LINESTRING EMPTY", writer.write(reader.read("LINESTRING EMPTY")));
+  public void testReadLineString() {
+    checkReadWrite("LINESTRING (10 10, 20 20, 30 40)");
+    checkReadWrite("LINESTRING EMPTY");
   }
 
-  public void testReadLinearRing() throws Exception {
-    try {
-      reader.read("LINEARRING (10 10, 20 20, 30 40, 10 99)");
-    }
-    catch (IllegalArgumentException e) {
-      assertTrue(e.getMessage().contains("not form a closed linestring"));
-    }
-    assertEquals("LINEARRING (10 10, 20 20, 30 40, 10 10)", writer.write(reader.read("LINEARRING (10 10, 20 20, 30 40, 10 10)")));
-    assertEquals("LINEARRING EMPTY", writer.write(reader.read("LINEARRING EMPTY")));
+  public void testReadLinearRing() {
+    checkReadWrite("LINEARRING (10 10, 20 20, 30 40, 10 10)");
+    checkReadWrite("LINEARRING EMPTY");
   }
 
-  public void testReadPolygon() throws Exception {
-    assertEquals("POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10))", writer.write(reader.read("POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10))")));
-    assertEquals("POLYGON EMPTY", writer.write(reader.read("POLYGON EMPTY")));
+  public void testReadPolygon() {
+    checkReadWrite("POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10))");
+    checkReadWrite("POLYGON EMPTY");
   }
 
-  public void testReadMultiPoint() throws Exception {
-    assertEquals("MULTIPOINT ((10 10), (20 20))", writer.write(reader.read("MULTIPOINT ((10 10), (20 20))")));
-    assertEquals("MULTIPOINT EMPTY", writer.write(reader.read("MULTIPOINT EMPTY")));
-    assertEquals("MULTIPOINT (EMPTY, EMPTY)", writer.write(reader.read("MULTIPOINT (EMPTY, EMPTY)")));
+  public void testReadMultiPoint() {
+    checkReadWrite("MULTIPOINT ((10 10), (20 20))");
+    checkReadWrite("MULTIPOINT EMPTY");
+    checkReadWrite("MULTIPOINT (EMPTY, EMPTY)");
   }
 
-  public void testReadMultiLineString() throws Exception {
-    assertEquals("MULTILINESTRING ((10 10, 20 20), (15 15, 30 15))", writer.write(reader.read("MULTILINESTRING ((10 10, 20 20), (15 15, 30 15))")));
-    assertEquals("MULTILINESTRING EMPTY", writer.write(reader.read("MULTILINESTRING EMPTY")));
-    assertEquals("MULTILINESTRING (EMPTY, EMPTY)", writer.write(reader.read("MULTILINESTRING (EMPTY, EMPTY)")));
+  public void testReadMultiLineString() {
+    checkReadWrite("MULTILINESTRING ((10 10, 20 20), (15 15, 30 15))");
+    checkReadWrite("MULTILINESTRING EMPTY");
+    checkReadWrite("MULTILINESTRING (EMPTY, EMPTY)");
   }
 
-  public void testReadMultiPolygon() throws Exception {
-    assertEquals("MULTIPOLYGON (((10 10, 10 20, 20 20, 20 15, 10 10)), ((60 60, 70 70, 80 60, 60 60)))", writer.write(reader.read("MULTIPOLYGON (((10 10, 10 20, 20 20, 20 15, 10 10)), ((60 60, 70 70, 80 60, 60 60)))")));
-    assertEquals("MULTIPOLYGON EMPTY", writer.write(reader.read("MULTIPOLYGON EMPTY")));
-    assertEquals("MULTIPOLYGON (EMPTY, EMPTY)", writer.write(reader.read("MULTIPOLYGON (EMPTY, EMPTY)")));
+  public void testReadMultiPolygon() {
+    checkReadWrite("MULTIPOLYGON (((10 10, 10 20, 20 20, 20 15, 10 10)), ((60 60, 70 70, 80 60, 60 60)))");
+    checkReadWrite("MULTIPOLYGON EMPTY");
+    checkReadWrite("MULTIPOLYGON (EMPTY, EMPTY)");
   }
 
-  public void testReadGeometryCollection() throws Exception {
-    assertEquals("GEOMETRYCOLLECTION (POINT (10 10), POINT (30 30), LINESTRING (15 15, 20 20))", writer.write(reader.read("GEOMETRYCOLLECTION (POINT (10 10), POINT (30 30), LINESTRING (15 15, 20 20))")));
-    assertEquals("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING EMPTY, LINESTRING (15 15, 20 20))", writer.write(reader.read("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING EMPTY, LINESTRING (15 15, 20 20))")));
-    assertEquals("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING (10 10, 20 20, 30 40, 10 10), LINESTRING (15 15, 20 20))", writer.write(reader.read("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING (10 10, 20 20, 30 40, 10 10), LINESTRING (15 15, 20 20))")));
-    assertEquals("GEOMETRYCOLLECTION EMPTY", writer.write(reader.read("GEOMETRYCOLLECTION EMPTY")));
+  public void testReadGeometryCollection() {
+    checkReadWrite("GEOMETRYCOLLECTION (POINT (10 10), POINT (30 30), LINESTRING (15 15, 20 20))");
+    checkReadWrite("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING EMPTY, LINESTRING (15 15, 20 20))");
+    checkReadWrite("GEOMETRYCOLLECTION (POINT (10 10), LINEARRING (10 10, 20 20, 30 40, 10 10), LINESTRING (15 15, 20 20))");
+    checkReadWrite("GEOMETRYCOLLECTION EMPTY");
   }
   
-  public void testReadGeometryCollectionEmptyWithElements() throws Exception {
-    assertEquals("GEOMETRYCOLLECTION (POINT EMPTY)", writer.write(reader.read("GEOMETRYCOLLECTION ( POINT EMPTY )")));
-    assertEquals("GEOMETRYCOLLECTION (POINT EMPTY, LINESTRING EMPTY)", writer.write(reader.read("GEOMETRYCOLLECTION ( POINT EMPTY, LINESTRING EMPTY )")));
+  public void testReadGeometryCollectionEmptyWithElements() {
+    checkReadWrite("GEOMETRYCOLLECTION (POINT EMPTY)");
+    checkReadWrite("GEOMETRYCOLLECTION (POINT EMPTY, LINESTRING EMPTY)");
+  }
+  
+  //===============================================
+  
+  private void checkReadWrite(final String wkt) {
+    String wktResult = readWrite(wkt);
+    assertEquals(wkt, wktResult);
+  }
+
+  private String readWrite(final String wkt) {
+    String wktResult = null;
+    try {
+      return writer.write(reader.read(wkt));
+    }
+    catch (ParseException ex) {
+      fail(ex.getMessage());
+    }
+    return wktResult;
   }
 }
