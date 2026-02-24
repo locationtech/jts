811e3205d3a242e90aa669a822f7c2b72a413985
Martin Davis <mtnclimb@gmail.com>
2026-02-23 21:57:53 -0800

    Remove tolerance from tests

--- modules/core/src/test/java/org/locationtech/jts/algorithm/distance/DirectedHausdorffDistanceTest.java
+++ modules/core/src/test/java/org/locationtech/jts/algorithm/distance/DirectedHausdorffDistanceTest.java
@@ -36,22 +36,20 @@ extends GeometryTestCase
   {
     String a = "MULTIPOINT ((0 1), (2 3), (4 5), (6 6))";
     String b = "MULTIPOINT ((0.1 0), (1 0), (2 0), (3 0), (4 0), (5 0))";
-    checkDistance(a, b, 0.01, "LINESTRING (6 6, 5 0)");
-    checkDistance(b, a, 0.01, "LINESTRING (5 0, 2 3)");
+    checkDistance(a, b, "LINESTRING (6 6, 5 0)");
+    checkDistance(b, a, "LINESTRING (5 0, 2 3)");
     checkHausdorff(a, b, "LINESTRING (6 6, 5 0)");
   }
   
   public void testPointPolygonInterior()
   {
     checkDistance("POINT (3 4)", "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))",
-        0.01,
         0);
   }
   
   public void testPointsPolygon()
   {
     checkDistance("MULTIPOINT ((4 3), (2 8), (8 5))", "POLYGON ((6 9, 6 4, 9 1, 1 1, 6 9))",
-        0.01,
         "LINESTRING (2 8, 4.426966292134832 6.48314606741573)");
   }
   
@@ -77,7 +75,6 @@ extends GeometryTestCase
   {
     checkDistance("MULTILINESTRING ((10 10, 10 90, 40 30), (40 30, 60 80, 90 30, 40 10))", 
         "LINESTRING (10 10, 10 90, 40 30, 60 80, 90 30, 40 10)", 
-        0.01,
         0.0);
   }
   
@@ -92,7 +89,7 @@ extends GeometryTestCase
   {
     String a = "MULTILINESTRING ((2 3, 2 7), (9 1, 9 8, 4 9))";
     String b = "POLYGON ((3 7, 6 8, 8 2, 3 4, 3 7))";
-    checkDistance(a, b, 0.01, "LINESTRING (9 8, 6.3 7.1)");
+    checkDistance(a, b, "LINESTRING (9 8, 6.3 7.1)");
     checkHausdorff(a, b, "LINESTRING (2 3, 5.5 3)");
   }
   
@@ -100,7 +97,6 @@ extends GeometryTestCase
   {
     checkDistance("POLYGON ((2 8, 8 2, 2 1, 2 8))", 
         "LINESTRING (6 5, 4 7, 0 0, 8 4)", 
-        0.001,
         "LINESTRING (2 8, 3.9384615384615387 6.892307692307693)");
   }
   
@@ -108,7 +104,6 @@ extends GeometryTestCase
   {
     checkDistanceStartPtLen("POLYGON ((2 8, 8 2, 2 1, 2 8))", 
         "LINESTRING (6 5, 4 7, 0 0, 9 1)", 
-        0.001,
         "LINESTRING (4.555 2.989, 4.828 0.536)", 0.01);
   }
   
@@ -116,8 +111,8 @@ extends GeometryTestCase
   {
     String a = "POLYGON ((2 18, 18 18, 17 3, 2 2, 2 18))";
     String b = "POLYGON ((1 19, 5 12, 5 3, 14 10, 11 19, 19 19, 20 0, 1 1, 1 19))";
-    checkDistance(b, a, 0.01, "LINESTRING (20 0, 17 3)");
-    checkDistance(a, b, 0.01, "LINESTRING (6.6796875 18, 11 19)");
+    checkDistance(b, a, "LINESTRING (20 0, 17 3)");
+    checkDistance(a, b, "LINESTRING (6.6796875 18, 11 19)");
     checkHausdorff(a, b, "LINESTRING (6.6796875 18, 11 19)");
   }
   
@@ -126,16 +121,16 @@ extends GeometryTestCase
     // B is contained in A
     String a = "POLYGON ((1 19, 19 19, 19 1, 1 1, 1 19), (6 8, 11 14, 15 7, 6 8))";
     String b = "POLYGON ((2 18, 18 18, 18 2, 2 2, 2 18), (10 17, 3 7, 17 5, 10 17))";
-    checkDistance(a, b, 0.01, "LINESTRING (9.8134765625 12.576171875, 7.8603515625 13.943359375)");
-    checkDistance(b, a, 0.01, 0.0);
+    checkDistance(a, b, "LINESTRING (9.817138671875 12.58056640625, 7.863620425230705 13.948029178901006)");
+    checkDistance(b, a, 0.0);
   }
   
   public void testMultiPolygons()
   {
     String a = "MULTIPOLYGON (((1 1, 1 10, 5 1, 1 1)), ((4 17, 9 15, 9 6, 4 17)))";
     String b = "MULTIPOLYGON (((1 12, 4 13, 8 10, 1 12)), ((3 8, 7 7, 6 2, 3 8)))";
-    checkDistance(a, b, 0.01, "LINESTRING (1 1, 5.4 3.2)");
-    checkDistanceStartPtLen(b, a, 0.001,
+    checkDistance(a, b, "LINESTRING (1 1, 5.4 3.2)");
+    checkDistanceStartPtLen(b, a, 
         "LINESTRING (2.669921875 12.556640625, 5.446115154109589 13.818546660958905)",
         0.01);
   }
@@ -145,7 +140,7 @@ extends GeometryTestCase
   {
     String wkt1 = "LINESTRING (2 5, 5 10, 6 4)";
     String wkt2 = "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))";
-    checkDistance(wkt1, wkt2, 0.01, "LINESTRING (5 10, 5 9)");
+    checkDistance(wkt1, wkt2, "LINESTRING (5 10, 5 9)");
   } 
   
   public void testNonVertexResult()
@@ -154,23 +149,23 @@ extends GeometryTestCase
     String wkt2 = "LINESTRING (0 10, 0 0, 10 0)";
     
     checkHausdorff(wkt1, wkt2, "LINESTRING (6.53857421875 6.5382080078125, 6.53857421875 0)");
-    checkDistance(wkt1, wkt2, 0.01, "LINESTRING (6.5390625 6.537109375, 6.5390625 0)");
+    checkDistance(wkt1, wkt2, "LINESTRING (6.53857421875 6.5382080078125, 6.53857421875 0)");
   }
   
   public void testDirectedLines()
   {
     String wkt1 = "LINESTRING (1 6, 3 5, 1 4)";
     String wkt2 = "LINESTRING (1 10, 9 5, 1 2)";
-    checkDistance(wkt1, wkt2, 0.01, "LINESTRING (1 6, 2.797752808988764 8.876404494382022)");
-    checkDistance(wkt2, wkt1, 0.01, "LINESTRING (9 5, 3 5)");
+    checkDistance(wkt1, wkt2, "LINESTRING (1 6, 2.797752808988764 8.876404494382022)");
+    checkDistance(wkt2, wkt1, "LINESTRING (9 5, 3 5)");
   }
 
   public void testDirectedLines2()
   {
     String wkt1 = "LINESTRING (1 6, 3 5, 1 4)";
     String wkt2 = "LINESTRING (1 3, 1 9, 9 5, 1 1)";
-    checkDistance(wkt1, wkt2, 0.01, "LINESTRING (3 5, 1 5)");
-    checkDistance(wkt2, wkt1, 0.01, "LINESTRING (9 5, 3 5)");
+    checkDistance(wkt1, wkt2, "LINESTRING (3 5, 1 5)");
+    checkDistance(wkt2, wkt1, "LINESTRING (9 5, 3 5)");
   }
 
   /**
@@ -191,7 +186,7 @@ extends GeometryTestCase
   {
     String a = "POLYGON ((1 9, 3 9, 4 5, 5.05 9, 9 9, 9 1, 1 1, 1 9))";
     String b = "POLYGON ((1 9, 9 9, 9 1, 1 1, 1 9))";
-    checkDistance(a, b, 1.0, 0.0);
+    checkDistance(a, b, 0.0);
   }
   
   //-----------------------------------------------------
@@ -266,21 +261,21 @@ extends GeometryTestCase
     checkEqualExact(expected, result, TOLERANCE);
   }
 
-  private void checkDistance(String wkt1, String wkt2, double tolerance, String wktExpected) {
+  private void checkDistance(String wkt1, String wkt2, String wktExpected) {
     Geometry g1 = read(wkt1);
     Geometry g2 = read(wkt2);
     
-    Geometry result = DirectedHausdorffDistance.distanceLine(g1, g2, tolerance);
+    Geometry result = DirectedHausdorffDistance.distanceLine(g1, g2);
     Geometry expected = read(wktExpected);
-    checkEqualExact(expected, result, 100 * tolerance);
+    checkEqualExact(expected, result, TOLERANCE);
   }
-
-  private void checkDistanceStartPtLen(String wkt1, String wkt2, double tolerance, 
+  
+  private void checkDistanceStartPtLen(String wkt1, String wkt2, 
       String wktExpected, double resultTolerance) {
     Geometry g1 = read(wkt1);
     Geometry g2 = read(wkt2);
     
-    Geometry result = DirectedHausdorffDistance.distanceLine(g1, g2, tolerance);
+    Geometry result = DirectedHausdorffDistance.distanceLine(g1, g2);
     Geometry expected = read(wktExpected);
     
     Coordinate resultPt = result.getCoordinates()[0];
@@ -303,7 +298,17 @@ extends GeometryTestCase
     assertEquals(expectedDistance, distResult, TOLERANCE);
   }
 
-  
+  private void checkDistance(String wkt1, String wkt2, 
+      double expectedDistance) {
+    Geometry g1 = read(wkt1);
+    Geometry g2 = read(wkt2);
+    
+    Geometry result = DirectedHausdorffDistance.distanceLine(g1, g2);
+    
+    double distResult = result.getLength();
+    assertEquals(expectedDistance, distResult, TOLERANCE);
+  }
+
   private void checkFullyWithinDistance(String a, String b, double distance, boolean expected) {
     Geometry g1 = read(a);
     Geometry g2 = read(b);
@@ -312,6 +317,5 @@ extends GeometryTestCase
     
     assertEquals(expected, result);
   }
-
   
 }
